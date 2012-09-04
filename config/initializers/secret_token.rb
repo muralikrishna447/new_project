14bcce7253c442c44a9b9db4893d2a45cdd82984
Aaron Jensen <aaronjensen@gmail.com>
2012-09-04 16:07:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delve::Application.config.secret_token = 'e03868af4e7d420bf230f01bfbd36862dab9bce5b0c6b66a27f677a24797fc3fcfd8f7a43c4af3fa27780b725f2295db1c6e7f48b5864cd449da9b09b5bff00c'
