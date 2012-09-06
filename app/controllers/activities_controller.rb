5fe73c7def7a88566aba9b0c02d7b17c62489892
Shaun Dern <shaun@substantial.com>
2012-09-06 16:07:43 -0700

    Check for page type for now in the controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,13 @@
 class ActivitiesController < ApplicationController
   def show
+    case params[:id]
+    when 'lecture'
+      render 'lecture'
+    when 'step-by-step'
+      render 'step_by_step'
+    else
+      redirect_to root_url
+    end
   end
 end
 
