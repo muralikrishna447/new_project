4d9409798e023f27da3d45f71942ab4529b3582f
Shaun Dern <shaun@substantial.com>
2012-09-07 14:04:21 -0700

    Create Activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906183601) do
+ActiveRecord::Schema.define(:version => 20120907204320) do
+
+  create_table "activities", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
