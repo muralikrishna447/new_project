d9b982aa4ae35f4fc4318bfbddc134c677bf5cca
Shaun Dern <shaun@substantial.com>
2012-09-07 14:05:02 -0700

    Use decent exposure and activities show view

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,4 @@
 class ActivitiesController < ApplicationController
-  def show
-    case params[:id]
-    when 'lecture'
-      render 'lecture'
-    when 'step-by-step'
-      render 'step_by_step'
-    else
-      redirect_to root_url
-    end
-  end
+  expose (:activity)
 end
 
