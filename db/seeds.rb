6f5a72103ea139ae794fd42cf84e425361412fba
Shaun Dern <shaun@substantial.com>
2012-09-07 15:19:52 -0700

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def create_activity(title, video_url='')
+  a = Activity.create
+  a.title = title
+  a.video_url = video_url
+  a.save!
+  a
+end
+
+def create_step(activity, title='', video_url='')
+  s = Step.create
+  s.activity = activity
+  s.title = title
+  s.video_url = video_url
+  s.save!
+  s
+end
+
+lecture = create_activity("Lecture", 'http://www.youtube.com/embed/ydOB-YNJ8Jw')
+create_step(lecture, 'Weeze the Juice')
+create_step(lecture, 'Do stuff', 'http://www.youtube.com/embed/ydOB-YNJ8Jw')
