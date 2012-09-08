5e5608bcb1c0f791303606c5911dd15d1fb8f76a
Shaun Dern <shaun@substantial.com>
2012-09-07 17:16:19 -0700

    create the admin in the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,25 @@ def create_activity(title, video_url='')
   a = Activity.create
   a.title = title
   a.video_url = video_url
-  a.save!
+  a.save
   a
 end
 
 def create_step(activity, title='', video_url='')
   s = Step.create
-  s.activity = activity
-  s.title = title
   s.video_url = video_url
-  s.save!
+  s.save
   s
 end
+
+def create_admin
+  u = User.create
+  u.email = "admin@admin.org"
+  u.password = 'asdfasdf'
+  u.save
+end
+
+create_admin
 
 lecture = create_activity("Lecture", 'http://www.youtube.com/embed/ydOB-YNJ8Jw')
 create_step(lecture, 'Weeze the Juice')
