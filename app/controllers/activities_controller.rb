6e729cb7e52f5bd8c57d094a8980248bb09628c3
Shaun Dern <shaun@substantial.com>
2012-09-10 15:03:44 -0700

    No need to expose

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,4 @@
 class ActivitiesController < ApplicationController
   expose (:activity)
-  expose (:required_equipment) { activity.required_equipment }
-  expose (:optional_equipment) { activity.optional_equipment }
-  expose (:ingredients) { activity.ingredients }
 end
 
