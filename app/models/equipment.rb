7437bc293b50c6f5cf018eed4dc80ceede78a66d
Shaun Dern <shaun@substantial.com>
2012-09-10 13:06:40 -0700

    remove unused activity_id attr

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,7 +1,10 @@
 class Equipment < ActiveRecord::Base
   has_many :activities, :through => :activity_equipment
 
-  attr_accessible :title, :product_url, :activity_id, as: :admin
+  attr_accessible :title, :product_url, :optional, as: :admin
+
+
+  scope :optional, where(optional: true)
 
   def product_url?
     product_url.present?
