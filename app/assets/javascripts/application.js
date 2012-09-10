4f1807dc28c7861265d3e3295aeadf9b62f3560b
Aaron Jensen <aaronjensen@gmail.com>
2012-09-10 16:06:05 -0700

    got carousel working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 //= require jquery_ujs
 //= require jquery.fitvids
 //= require fit-videos
+//= require carousel
 
 //= require twitter/bootstrap/dropdown
 //= require twitter/bootstrap/transition
 //= require twitter/bootstrap/collapse
+//= require twitter/bootstrap/carousel
