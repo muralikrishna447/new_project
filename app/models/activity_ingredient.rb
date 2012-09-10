323f6635f41c2ef3192865d8a5012223687e600d
Shaun Dern <shaun@substantial.com>
2012-09-10 15:03:35 -0700

    delegate title

--- app/models/activity_ingredient.rb
+++ app/models/activity_ingredient.rb
@@ -3,5 +3,7 @@ class ActivityIngredient < ActiveRecord::Base
   belongs_to :ingredient
 
   attr_accessible :activity_id, :ingredient_id, :quantity, as: :admin
+
+  delegate :title, to: :ingredient
 end
 
