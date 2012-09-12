90148f313f1f6b2885762b96fad3f98cabea2137
Shaun Dern <shaun@substantial.com>
2012-09-12 14:26:28 -0700

    Add submit button and spinner

--- app/controllers/mailing_list_controller.rb
+++ app/controllers/mailing_list_controller.rb
@@ -1,12 +1,14 @@
 class MailingListController < ActionController::Base
   include MailingListHelper
+  expose(:placeholder_text) { "Thanks for subscribing"}
 
   def subscribe
     email = params[:email]
     @chimp = MailChimpListManager.new(MAILCHIMP_LIST)
 
     if @chimp.subscribe_user(email)
-      render nothing: true, status: :ok
+      sleep(20)
+      render 'success', format: :js
     else
       render nothing: true, status: :internal_server_error
     end
