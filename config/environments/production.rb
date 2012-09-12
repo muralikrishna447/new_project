867268d36e0edb488601b196eb8f976344f902b7
Aaron Jensen <aaronjensen@gmail.com>
2012-09-12 13:21:53 -0700

    set rails env in staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Delve::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'delve-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'chefsteps.com' }
 
   # Enable threaded mode
   # config.threadsafe!
