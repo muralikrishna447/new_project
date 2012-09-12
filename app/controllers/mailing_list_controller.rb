3d3180584fc7cb1de98916e9dabf4da348025c0b
Shaun Dern <shaun@substantial.com>
2012-09-12 14:31:00 -0700

    add error view for subscribe

--- app/controllers/mailing_list_controller.rb
+++ app/controllers/mailing_list_controller.rb
@@ -1,16 +1,16 @@
 class MailingListController < ActionController::Base
   include MailingListHelper
-  expose(:placeholder_text) { "Thanks for subscribing"}
+  expose(:success_placeholder_text) { "Thanks for subscribing"}
+  expose(:error_placeholder_text) { "Something went wrong, please try again..."}
 
   def subscribe
     email = params[:email]
     @chimp = MailChimpListManager.new(MAILCHIMP_LIST)
 
     if @chimp.subscribe_user(email)
-      sleep(20)
       render 'success', format: :js
     else
-      render nothing: true, status: :internal_server_error
+      render 'error', format: :js
     end
   end
 end
