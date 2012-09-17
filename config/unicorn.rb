f3f6c205652b191d75e4d60229c548e85d261645
Aaron Jensen <aaronjensen@gmail.com>
2012-09-17 15:20:46 -0700

    newrelic stuff and app forking

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,18 @@
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  ActiveRecord::Base.connection.disconnect! if defined(ActiveRecord::Base)
+end
+
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+end
+
