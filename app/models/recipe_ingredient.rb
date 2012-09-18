3d271150ef0f704862fd322909efc4abf8527c95
Shaun Dern <shaun@substantial.com>
2012-09-18 14:08:41 -0700

    make touch true for recipes

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,5 +1,5 @@
 class RecipeIngredient < ActiveRecord::Base
-  belongs_to :recipe
+  belongs_to :recipe, touch: true
   belongs_to :ingredient
 
   attr_accessible :recipe_id, :ingredient_id, :quantity, :unit, as: :admin
