513626cf18ab88a72f9be2ea6114a99898390390
Shaun Dern <shaun@substantial.com>
2012-09-19 19:13:19 -0700

    add horseradish images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def s3_image_url(image_id)
+    "https://delve-staging.s3.amazonaws.com/#{image_id}"
+  end
+
   def quantity_number(quantity)
     number_with_precision(quantity, precision: 2, strip_insignificant_zeros: true)
   end
