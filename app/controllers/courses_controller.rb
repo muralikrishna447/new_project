c3607d936b4a8bb8fa3a8573948e97a717bf67df
Shaun Dern <shaun@substantial.com>
2012-09-21 14:06:41 -0700

    add coures syllabus to copy

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   include AggressiveCaching
   expose(:activities) { Activity.all }
+  expose(:syllabus_copy) { Copy.find_by_location('course-syllabus') }
 end
 
