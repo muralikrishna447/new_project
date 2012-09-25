9c13e3a9ee557d46b1598c8b836f7f94a89abe80
Shaun Dern <shaun@substantial.com>
2012-09-25 10:57:07 -0700

    add course description

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,5 +2,6 @@ class CoursesController < ApplicationController
   include AggressiveCaching
   expose(:activities) { Activity.all }
   expose(:syllabus_copy) { Copy.find_by_location('course-syllabus') }
+  expose(:course_description) { Copy.find_by_location('course-description') }
 end
 
