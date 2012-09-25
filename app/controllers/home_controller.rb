6745965a90794e2b6c4cf0d0f51916b2bae63e4f
Shaun Dern <shaun@substantial.com>
2012-09-25 12:05:58 -0700

    add carousel copy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   expose(:bio_chris) { Copy.find_by_location('creator-chris') }
   expose(:bio_grant) { Copy.find_by_location('creator-grant') }
   expose(:bio_ryan) { Copy.find_by_location('creator-ryan') }
+  expose(:carousel_images_copy) { Copy.find_by_location('carousel-images').copy }
 end
 
