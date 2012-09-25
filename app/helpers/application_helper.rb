3748ed61fa5750d83dee4bfc868a2fc69bf3d32d
Shaun Dern <shaun@substantial.com>
2012-09-25 10:47:54 -0700

    Use a single chefsteps s3 bucket

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
 module ApplicationHelper
   def s3_image_url(image_id)
-    case Rails.env
-    when 'development'
-      root = "https://chefsteps-staging.s3.amazonaws.com"
-    when 'staging'
-      root = "https://chefsteps-staging.s3.amazonaws.com"
-    when 'production'
-      root = "https://chefsteps-production.s3.amazonaws.com"
-    end
-    [root, image_id].compact.join('/')
+    "https://chefsteps-production.s3.amazonaws.com/#{image_id}"
   end
 
   def quantity_number(quantity)
