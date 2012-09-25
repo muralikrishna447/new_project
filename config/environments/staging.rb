0c7244bdd278bd434750e1869f8ad888729b0ada
Aaron Jensen <aaronjensen@gmail.com>
2012-09-25 11:35:28 -0700

    config updates for asset_sync

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -44,7 +44,7 @@ Delve::Application.configure do
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://d287o4ct1nmkd9.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( print.css styleguide.css )
