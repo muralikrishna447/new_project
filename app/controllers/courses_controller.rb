0f4e4b363bd40f3f6dc737a68d44ed5d7f119f07
Shaun Dern <shaun@substantial.com>
2012-09-25 11:09:41 -0700

    add instructor and creator bios copy

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,5 +3,7 @@ class CoursesController < ApplicationController
   expose(:activities) { Activity.all }
   expose(:syllabus_copy) { Copy.find_by_location('course-syllabus') }
   expose(:course_description) { Copy.find_by_location('course-description') }
+  expose(:bio_chris) { Copy.find_by_location('instructor-chris') }
+  expose(:bio_grant) { Copy.find_by_location('instructor-grant') }
 end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   include AggressiveCaching
   expose(:homepage_blurb) { Copy.find_by_location('homepage-blurb') }
+  expose(:bio_chris) { Copy.find_by_location('creator-chris') }
+  expose(:bio_grant) { Copy.find_by_location('creator-grant') }
+  expose(:bio_ryan) { Copy.find_by_location('creator-ryan') }
 end
+
