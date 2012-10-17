27169477d2adf0643374b53290ffdcc33c6302d7
Shaun Dern <shaun@substantial.com>
2012-10-17 14:59:32 -0700

    compile and load global nav css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Delve::Application.configure do
   config.action_controller.asset_host = "http://dpybg80nberao.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( print.css styleguide.css )
+  config.assets.precompile += %w( print.css styleguide.css header.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
