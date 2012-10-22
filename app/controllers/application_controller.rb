748dfc5b694c774f3f1b6c43cbc384a23593b2fc
Shaun Dern <shaun@substantial.com>
2012-10-22 14:01:24 -0700

    set server-side header response for CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :set_access_control_headers, only: :global_navigation
 
   def global_navigation
-    response.headers.add_header('Access-Controler-Allow-Origin: http://chefstepsblog.com')
     render partial: 'layouts/header'
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://chefstepsblog.com'
+    headers['Access-Control-Request-Method'] = 'GET'
   end
 end
 
