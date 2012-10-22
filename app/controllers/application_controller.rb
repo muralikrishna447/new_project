f43b8b8a640df1d8676ae4bae48b2ecad0dd67f8
Shaun Dern <shaun@substantial.com>
2012-10-22 13:42:31 -0700

    add allow access controller header to response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def global_navigation
+    response.headers.add_header('Access-Controler-Allow-Origin: http://chefstepsblog.com')
     render partial: 'layouts/header'
   end
 end
