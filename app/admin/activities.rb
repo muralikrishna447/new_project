15ef2c566a03238c5645165a0ca8d35b276fa560
Shaun Dern <shaun@substantial.com>
2012-10-23 13:54:37 -0700

    get activities up to parity

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -1,25 +1,25 @@
 ActiveAdmin.register Activity do
 
-  form partial: "form"
-  # form do |f|
-  #   f.inputs "Activity Details" do
-  #     f.input :title
-  #     f.input :description
-  #     f.input :youtube_id
-  #     f.input :yield
-  #     f.input :timing
-  #     f.input :difficulty
-  #     f.input :activity_order
-  #   end
-  #   f.inputs "Equipment" do
-  #     f.input :equipment
-  #   end
-  #   f.inputs "Steps" do
-  #     f.input :steps
-  #   end
-  #   f.inputs "Recipes" do
-  #     f.input :recipes
-  #   end
-  # end
+  form do |f|
+    f.inputs "Activity Details" do
+      f.input :title
+      f.input :description
+      f.input :youtube_id
+      f.input :yield
+      f.input :timing
+      f.input :difficulty
+      f.input :activity_order
+    end
+    f.inputs "Equipment" do
+      f.input :equipment
+    end
+    f.inputs "Steps" do
+      f.input :steps
+    end
+    f.inputs "Recipes" do
+      f.input :recipes
+    end
+    f.buttons
+  end
 end
 
