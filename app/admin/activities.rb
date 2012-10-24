9a0ceafd94b65a8c8a32709cb2773aa33a81e36b
Shaun Dern <shaun@substantial.com>
2012-10-24 13:47:10 -0700

    start adding activities form

--- app/admin/activities.rb
+++ app/admin/activities.rb
@@ -4,27 +4,27 @@ ActiveAdmin.register Activity do
     render "show", activity: activity
   end
 
-  form do |f|
-    f.inputs "Activity Details" do
-      f.input :title
-      f.input :description
-      f.input :youtube_id
-      f.input :yield
-      f.input :timing
-      f.input :difficulty
-      f.input :activity_order
-    end
-    f.inputs "Equipment" do
-      f.input :equipment
-    end
-    f.inputs "Steps" do
-      f.input :steps
-    end
-    f.inputs "Recipes" do
-      f.input :recipes
-    end
-    f.buttons
-  end
+  form partial: 'form'
+  #   f.inputs "Activity Details" do
+  #     f.input :title
+  #     f.input :description
+  #     f.input :youtube_id
+  #     f.input :yield
+  #     f.input :timing
+  #     f.input :difficulty
+  #     f.input :activity_order
+  #   end
+  #   f.inputs "Equipment" do
+  #     f.input :equipment
+  #   end
+  #   f.inputs "Steps" do
+  #     f.input :steps
+  #   end
+  #   f.inputs "Recipes" do
+  #     f.input :recipes
+  #   end
+  #   f.buttons
+  # end
 
   index do
     column "Order", :activity_order
