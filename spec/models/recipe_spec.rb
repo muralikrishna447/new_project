7984f0bad39737faa1cee05af928c994f88cdaf4
Shaun Dern <shaun@substantial.com>
2012-10-29 11:23:14 -0700

    check ingredient count

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe Recipe do
   describe "#update_ingredients" do
+    let(:soup) { {title: 'Soup', quantity: 2, unit: 'g'}  }
+    let(:pepper) { {title: 'Pepper', quantity: 1, unit: 'kg'}  }
     let(:recipe) { Recipe.create(title: 'foo') }
-    let(:ingredient_attrs) {[
-      { title: 'Soup', quantity: 2, unit: 'g' },
-      { title: 'Pepper', quantity: 1, unit: 'g' },
-      { title: 'Pepper', quantity: 1, unit: 'g' },
+    let(:ingredient_attrs) {[ soup, pepper, pepper,
       { title: '', quantity: 2, unit: '' }
     ]}
 
@@ -30,19 +29,20 @@ describe Recipe do
     describe "deletion" do
       before do
         recipe.update_ingredients(ingredient_attrs)
+        recipe.update_ingredients(ingredient_attrs[1..-1])
+        recipe.ingredients.reload
       end
 
       it "deletes ingredients not included in attribute set" do
-        recipe.update_ingredients(ingredient_attrs[1..-1])
-        recipe.ingredients.reload.should have(1).ingredients
-        recipe.ingredients.first.quantity.should == 1
-        recipe.ingredients.first.unit.should == 'g'
+        recipe.ingredients.should have(1).ingredients
         recipe.ingredients.first.title.should == 'Pepper'
+        recipe.ingredients.first.quantity.should == 1
+        recipe.ingredients.first.unit.should == 'kg'
       end
     end
 
     describe "update" do
-      let(:updated_ingredient_attrs) {[ { title: 'Soup', quantity: 15, unit: 'foobars' } ]}
+      let(:updated_ingredient_attrs) { ingredient_attrs << { title: 'Soup', quantity: 15, unit: 'foobars' } }
 
       before do
         recipe.update_ingredients(ingredient_attrs)
@@ -51,6 +51,7 @@ describe Recipe do
       end
 
       it "updates existing ingredients" do
+        recipe.ingredients.should have(2).ingredients
         recipe.ingredients.first.quantity.should == 15
         recipe.ingredients.first.unit.should == 'foobars'
       end
