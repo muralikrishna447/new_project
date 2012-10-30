5ae2144181de413dce616316ee3f20ed302ec023
Shaun Dern <shaun@substantial.com>
2012-10-30 14:15:46 -0700

    capitalize title on save and create

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Ingredient do
+  describe "capitalize title" do
+    let(:ingredient) { Ingredient.create(title: 'fOo Bar bAz') }
+
+    describe "create" do
+      it "capitalizes the first letter of the first word only" do
+        ingredient.title.should == 'Foo bar baz'
+      end
+    end
+
+    describe "update" do
+      before do
+        ingredient.update_attributes(title: 'NuCLeAR ISOtOpEs')
+      end
+
+      it "capitalizes the first letter of the first word only" do
+        ingredient.title.should == 'Nuclear isotopes'
+      end
+    end
+  end
+end
+
