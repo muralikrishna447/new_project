27c8abb697b4660bcf2ecd7b617e1387614838da
Alex May <alex.k.may@gmail.com>
2012-10-31 17:49:01 -0700

    hook up backend for step ingredients

--- app/admin/recipes.rb
+++ app/admin/recipes.rb
@@ -6,6 +6,10 @@ ActiveAdmin.register Recipe do
 
   form partial: 'form'
 
+  action_item :only => [:show, :edit] do
+    link_to('Edit step ingredients', associated_ingredients_admin_recipe_path(recipe))
+  end
+
   controller do
     def create
       ingredient_attrs = separate_ingredients
@@ -34,12 +38,15 @@ ActiveAdmin.register Recipe do
     end
   end
 
-  member_action :associate_ingredients, method: :get do
+  member_action :associated_ingredients, method: :get do
     @recipe = Recipe.find(params[:id])
   end
 
-  member_action :associate_ingredients, method: :put do
-    @recipe = Recipe.find(params[:id])
+  member_action :update_associated_ingredients, method: :put do
+    params[:step_ingredients].each do |id, ingredients|
+      Step.find(id).update_ingredients(ingredients)
+    end
+    redirect_to({action: :show}, notice: "Step's ingredients updated")
   end
 end
 
