53ac55cfbbb0d60fa8137df966330bb761db1cac
Alex May <alex.k.may@gmail.com>
2012-10-31 17:23:58 -0700

    add activity_id to accessible attrs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,7 @@ class Recipe < ActiveRecord::Base
 
   validates :title, presence: true
 
-  attr_accessible :title, :activity, :yield, :steps, :ingredients, :recipe_order_position
+  attr_accessible :title, :activity, :activity_id, :yield, :steps, :ingredients, :recipe_order_position
 
   accepts_nested_attributes_for :ingredients, :steps
 
