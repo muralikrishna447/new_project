26212a5f680adef47fd83faed1f626fd5f376814
Alex May <alex.k.may@gmail.com>
2012-11-02 15:32:40 -0700

    add name field to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, as: :admin
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, as: :admin
 end
