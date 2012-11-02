2f9f5971261b774f572d3ff78580e525765ce573
Alex May <alex.k.may@gmail.com>
2012-11-02 12:05:37 -0700

    add show_auth flag to nav to suppress auth links
    For example, we probably won't want the auth links to show up
    when viewing the global nav from the blog (at least not until
    we have SSO).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def global_navigation
-    render partial: 'layouts/header'
+    render partial: 'layouts/header', locals: {show_auth: false}
   end
 
 end
