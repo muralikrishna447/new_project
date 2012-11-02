011d6823e66beda98541e529bee555be096ce885
Alex May <alex.k.may@gmail.com>
2012-11-01 18:46:54 -0700

    fix copy display for admin edit

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,6 +1,11 @@
 class Copy < ActiveRecord::Base
   attr_accessible :location, :copy, as: :admin
 
+  # used by active admin for display
+  def title
+    location
+  end
+
   def to_s
     copy
   end
