baa7a7d52aca097273cc01edea53aa166379415c
Alex May <alex.k.may@gmail.com>
2012-11-01 17:25:22 -0700

    add dep destroy for Equipment -> ActivityEquipment

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,5 @@
 class Equipment < ActiveRecord::Base
-  has_many :activity_equipment, inverse_of: :equipment
+  has_many :activity_equipment, inverse_of: :equipment, dependent: :destroy
   has_many :activities, through: :activity_equipment, inverse_of: :equipment
 
   attr_accessible :title, :product_url
