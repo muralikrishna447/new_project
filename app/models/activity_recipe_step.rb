7dda6b4137cd2ad1effd863fe750fce84353180a
Shaun Dern <shaun@substantial.com>
2012-11-05 09:53:52 -0800

    wip

--- app/models/activity_recipe_step.rb
+++ app/models/activity_recipe_step.rb
@@ -5,3 +5,16 @@ class ActivityRecipeStep < ActiveRecord::Base
   attr_accessible :activity_id, :step_id
 end
 
+  # def create_activity_steps(recipe_ids)
+  #   recipe_steps = Step.joins(:recipe).where(recipe_id: recipe_ids)
+  #   recipe_steps.each do |step|
+  #     steps.find_or_create_by_step_id_and_activity_id(step.id, self.id)
+  #   end
+  # end
+
+  # def delete_old_steps(recipe_ids)
+  #   old_step_ids = steps.joins(:step).map(&:id) - Step.joins(:recipe).where(recipe_id: recipe_ids).map(&:id)
+  #   # old_steps_ids = steps.map(&:id) - Step.joins(:recipe).where(recipe_id: recipe_ids).map(&:id)
+  #   pp "\n\n\n\n***********************STEP IDS: #{old_step_ids}"
+  #   # steps.where(id: old_steps_ids).destroy_all
+  # end
