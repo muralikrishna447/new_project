8cbaf1312a7c0679a388a5d69ed003bc2a85b21c
Shaun Dern <shaun@substantial.com>
2012-11-05 15:27:00 -0800

    add ranked model and step order

--- app/models/activity_recipe_step.rb
+++ app/models/activity_recipe_step.rb
@@ -1,10 +1,17 @@
 class ActivityRecipeStep < ActiveRecord::Base
+  include RankedModel
+  ranks :step_order, with_same: :activity_id
+
   belongs_to :activity, touch: true, inverse_of: :recipe_steps
   belongs_to :step, touch: true, inverse_of: :activity_recipe_steps
 
   has_one :recipe, through: :step
 
-  attr_accessible :activity_id, :step_id
+  attr_accessible :activity_id, :step_id, :step_order
+
   delegate :title, :youtube_id, :directions, :image_id, :ingredient_ids, to: :step
+
+  scope :ordered, rank(:step_order)
+  default_scope { ordered }
 end
 
