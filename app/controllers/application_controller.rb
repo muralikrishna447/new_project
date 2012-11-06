1b9fe310b149361b50245155482a1ce734d6d751
Shaun Dern <shaun@substantial.com>
2012-11-06 12:22:59 -0800

    add terms of service page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
     render partial: 'layouts/header', locals: {show_auth: false}
   end
 
+  def terms_of_service
+    @terms_of_service = Copy.find_by_location('terms-of-service')
+    render 'home/terms_of_service'
+  end
+
   # expose devise helper method to views
   helper_method :after_sign_in_path_for
 
