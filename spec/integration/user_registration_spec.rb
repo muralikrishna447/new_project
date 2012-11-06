bcbf4498130cb45e607b7e308956a7d6dda621f6
Alex May <alex.k.may@gmail.com>
2012-11-05 23:40:47 -0800

    add poltergeist gem. registration spec works

--- spec/integration/user_registration_spec.rb
+++ spec/integration/user_registration_spec.rb
@@ -1,18 +1,25 @@
 require 'spec_helper'
 
-describe 'User registration application' do
+describe 'User registration application', :js do
 
   it "creates a user when valid inputs are supplied" do
     visit '/'
-    click_link 'Sign up'
-    fill_in 'Name', with: 'Bob'
+    click_link('Sign up')
+
+    wait_until { page.find("#sign-up").visible? }
+
+    page.should have_content('Create a new account')
+
+    fill_in 'Name', with: 'Bob Tester'
     fill_in 'Email', with: 'bob@bob.com'
     fill_in 'Password', with: 'password'
     fill_in 'Confirm Password', with: 'password'
-    click_button 'Log in'
+    click_button 'Log In'
+
+    page.should have_content('Bob Tester')
 
     User.all.should have(1).users
-    User.first.name.should == 'Bob'
+    User.first.name.should == 'Bob Tester'
   end
 
 end
