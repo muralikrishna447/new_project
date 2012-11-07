8da27b35f0dc0e9f8a3ead0ce49a51e840054c63
Shaun Dern <shaun@substantial.com>
2012-11-07 12:07:41 -0800

    DRY and update thank you

--- app/controllers/thank_you_controller.rb
+++ app/controllers/thank_you_controller.rb
@@ -2,16 +2,10 @@ class ThankYouController < ApplicationController
   include AggressiveCaching
   before_filter :authenticate_user!, only: :show
 
-  expose(:thank_you_intro) { Copy.find_by_location('thank-you-intro') }
-
-  expose(:thank_you_image_left) { Copy.find_by_location('thank-you-image-left') }
-  expose(:thank_you_image_left_caption) { Copy.find_by_location('thank-you-image-left-caption') }
-
-  expose(:thank_you_image_middle) { Copy.find_by_location('thank-you-image-middle') }
-  expose(:thank_you_image_middle_caption) { Copy.find_by_location('thank-you-image-middle-caption') }
-
-  expose(:thank_you_image_right) { Copy.find_by_location('thank-you-image-right') }
-  expose(:thank_you_image_right_caption) { Copy.find_by_location('thank-you-image-right-caption') }
-
+  [:thank_you_intro, :thank_you_image_left, :thank_you_image_left_caption,
+   :thank_you_image_middle, :thank_you_image_middle_caption, :thank_you_image_right,
+   :thank_you_image_right_caption].each do |key|
+     expose(key) { Copy.find_by_location(key.to_s.dasherize)}
+   end
 end
 
