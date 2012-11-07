24df981f3a40a6c05c00000b7c34c1ae8d0603cc
Shaun Dern <shaun@substantial.com>
2012-11-06 17:01:59 -0800

    use partial for rendering images with captions

--- app/controllers/thank_you_controller.rb
+++ app/controllers/thank_you_controller.rb
@@ -2,7 +2,7 @@ class ThankYouController < ApplicationController
   include AggressiveCaching
   before_filter :authenticate_user!, only: :show
 
-  expose(:thank_you_intro_copy) { Copy.find_by_location('thank-you-intro') }
+  expose(:thank_you_intro) { Copy.find_by_location('thank-you-intro') }
 
   expose(:thank_you_image_left) { Copy.find_by_location('thank-you-image-left') }
   expose(:thank_you_image_left_caption) { Copy.find_by_location('thank-you-image-left-caption') }
