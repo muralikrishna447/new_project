827210040b017ff3f70be10f18b27a32985327f9
Alex May <alex.k.may@gmail.com>
2012-11-07 15:25:25 -0800

    update AggressiveCaching to exclude actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,10 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
 
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
   RSpec.configure do |config|
     config.mock_with :rspec
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
