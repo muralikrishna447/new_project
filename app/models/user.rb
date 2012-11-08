dda2434789d4d5b9fabdfd6e3ddad1b8cc9d71e1
Alex May <alex.k.may@gmail.com>
2012-11-08 10:23:28 -0800

    add omniauth attributes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :provider, :uid
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, as: :admin
 
   validates_presence_of :name
