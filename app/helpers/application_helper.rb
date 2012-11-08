849e4771313103752f4e83b08f2152c16e47f7ab
Shaun Dern <shaun@substantial.com>
2012-11-08 13:46:09 -0800

    remove split_image_copy helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 module ApplicationHelper
-  def split_images_copy(copy)
-    images = copy.split("\n")
-    images.map! do |image|
-      s3_image_url(image)
-    end
-  end
-
   def s3_image_url(image_id)
     "http://d2eud0b65jr0pw.cloudfront.net/#{image_id}"
   end
