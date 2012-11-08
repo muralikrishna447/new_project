ef84202f1571053bad78cec77ae955ea9c0352d1
Alex May <alex.k.may@gmail.com>
2012-11-08 12:21:50 -0800

    basic facebook login support

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,22 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, as: :admin
 
   validates_presence_of :name
+
+  def self.find_for_facebook_oauth(auth)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    user = create_user_from_auth(auth) unless user
+    user
+  end
+
+  private
+
+  def self.create_user_from_auth(auth)
+    User.create(
+      name: auth.extra.raw_info.name,
+      provider: auth.provider,
+      uid: auth.uid,
+      email: auth.info.email,
+      password: Devise.friendly_token[0,20]
+    )
+  end
 end
