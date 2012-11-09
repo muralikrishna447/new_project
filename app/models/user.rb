c3f4275535d7f79ffac494f4bd099b75012aa4b3
Shaun Dern <shaun@substantial.com>
2012-11-09 12:00:00 -0800

    add gravtatstic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   include User::Facebook
+  include Gravtastic
+  gravtastic
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
