a7ffaf7ecd313d611ab6ce197a7aa9f8d91b1527
Alex May <alex.k.may@gmail.com>
2012-11-12 09:05:56 -0800

    override build_resource method instead of create
    We only need to hook into the build_resource method to make sure that
    facebook data is assigned over if present. This is cleaner than
    overriding the entire create method.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def create
-    build_resource
+  protected
+  def build_resource(hash=nil)
+    hash ||= resource_params || {}
+    self.resource = resource_class.new_with_session(hash, session)
 
     fb_data = session["devise.facebook_data"]
-    resource.assign_from_facebook(fb_data) if fb_data
-
-    if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
-    end
+    self.resource.assign_from_facebook(fb_data) if fb_data
   end
 end
