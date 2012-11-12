aba7e2ca3b12f39040a51be9e0029a10b17424c4
Alex May <alex.k.may@gmail.com>
2012-11-12 14:49:26 -0800

    add mailer configuration for staging/production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -54,6 +54,16 @@ Delve::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: DOMAIN }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'www.chefsteps.com',
+    :user_name => 'noreply@chefsteps.com',
+    :password => 'chefstepsnrply3#',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Enable threaded mode
   # config.threadsafe!
