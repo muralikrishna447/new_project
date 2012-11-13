d3013af6d3336fd9deba36013cb848dddc168800
Alex May <alex.k.may@gmail.com>
2012-11-13 11:56:48 -0800

    add functioning forum SSO
    still need to get the devise automatic redirects to work

--- app/controllers/forum_sso_controller.rb
+++ app/controllers/forum_sso_controller.rb
@@ -1,5 +1,30 @@
 class ForumSsoController < ApplicationController
+  DOZUKI_SECRET = '5J2HCTxVexxf9gXFtnuvsJ2kcFuiQeeL'
+  FORUM_LOGIN_URL = 'http://forum.chefsteps.com/Guide/User/remote_login'
+
+  before_filter :redirect_to_registration
+  before_filter :authenticate_user!
+
   def authenticate
-    head :ok
+    redirect_to "#{FORUM_LOGIN_URL}?#{auth_info}"
+  end
+
+  private
+  def auth_info
+    query = {
+      userid: current_user.id,
+      email: current_user.email,
+      name: current_user.name,
+      t: Time.now.to_i
+    }.to_query
+    query += "&hash=#{calculate_hash(query)}"
+  end
+
+  def calculate_hash(query)
+    Digest::SHA1.hexdigest("#{query}#{DOZUKI_SECRET}")
+  end
+
+  def redirect_to_registration
+    redirect_to new_user_registration_path if params[:register]
   end
 end
--- spec/controllers/forum_sso_controller_spec.rb
+++ spec/controllers/forum_sso_controller_spec.rb
@@ -1,15 +1,62 @@
 require 'spec_helper'
 
-describe ForumSsoController do
+describe ForumSsoController, type: :controller do
 
-  # If the register url parameter is set to "1", send the user to your register/signup page, and then back to this same URL.
-  # If the user is not signed in, send them to your signin page, and then back to this same URL.
-  # build a URL encoded HTTP query string of the required parameters (exclude the question mark)
-  # Compute the SHA1 hash of your SSO secret appended to the end of the query string. Note: the secret should NOT be sent in the query string.
-  # Append &hash=abcd01234... to end end of your query string
-  # Redirect the user to the URL: http://forum.chefsteps.com/Guide/User/remote_login with your query string from above appended to the end.
-  it 'has a route' do
+  it 'redirects to sign up if register param set' do
+    get :authenticate, register: 1
+    response.should redirect_to new_user_registration_path
+  end
+
+  it 'redirects to log in if there is no authenticated user' do
     get :authenticate
+    response.should redirect_to new_user_session_path
+  end
+
+  context 'user is authenticated' do
+    let(:user) { Fabricate(:user) }
+    before do
+      controller.stub(:calculate_hash) { 'test_hash' }
+      sign_in user
+      get :authenticate
+    end
+
+    subject { Rack::Utils.parse_query(URI.parse(response.location).query) }
+
+    it 'returns query string with user id' do
+      subject['userid'].should == user.id.to_s
+    end
+
+    it 'returns query string with user email' do
+      subject['email'].should == user.email.to_s
+    end
+
+    it 'returns query string with user name' do
+      subject['name'].should == user.name.to_s
+    end
+
+    it 'returns query string with unix timestamp' do
+      subject['t'].should be
+    end
+
+    it 'returns query string with hash' do
+      subject['hash'].should == 'test_hash'
+    end
+
+    it 'redirects the user to forum login' do
+      response.location.should start_with ForumSsoController::FORUM_LOGIN_URL
+    end
+  end
+
+  describe '#calculate_hash' do
+    let(:secret) { 'ABCD' }
+    let(:query) { 'foo=bar' }
+    before do
+      stub_const('ForumSsoController::DOZUKI_SECRET', secret)
+    end
+
+    it 'hashes query plus secret' do
+      controller.send(:calculate_hash, query).should == Digest::SHA1.hexdigest("#{query}#{secret}")
+    end
   end
 
 end
