3609b765f04352b88934fb98e74c3e88ac9a23cb
Shaun Dern <shaun@substantial.com>
2012-11-15 15:51:08 -0800

    remove unused sinon.js

--- spec/javascripts/spec.js
+++ spec/javascripts/spec.js
@@ -1,6 +1,5 @@
-#=require vendor/jquery-1.8.2
-#=require vendor/sinon-1.4.2
-#=require navigation_bootstrap
-#=require application
-#=require_tree ./
+//=require vendor/jquery-1.8.2
+//=require navigation_bootstrap
+//=require application
+//=require_tree .
 
