dc424c4c80cfa418c8da4f0962129c09ef76462f
Alex May <alex.k.may@gmail.com>
2012-11-16 09:19:09 -0800

    add login spec & fix database cleaning
    For some reason, transaction cleaning doesn't work for these javascript
    specs (think it has to do with separate threads). We need to come up
    with a better place to put the before/after hooks.

--- spec/acceptance/authentication_spec.rb
+++ spec/acceptance/authentication_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
-feature 'user registration', :js do
+feature 'user authentication', :js do
+  before :all do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  after :all do
+    DatabaseCleaner.strategy = :transaction
+  end
 
   scenario "creates a user when valid inputs are supplied" do
     visit '/'
@@ -23,4 +30,19 @@ feature 'user registration', :js do
     User.first.name.should == 'Bob Tester'
   end
 
+  scenario "authenticates a user when valid credentials are provided" do
+    Fabricate(:user, email: 'bob@bob.com', name: 'Bob Tester', password: 'password')
+
+    visit '/'
+    click_link('Log in')
+
+    wait_until { page.find("#log-in").visible? }
+
+    fill_in 'Email', with: 'bob@bob.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+
+    page.should have_content('Bob Tester')
+  end
 end
+
