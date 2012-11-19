f73870069cb36e4ff7bfed349c4ef2514c44387f
Shaun Dern <shaun@substantial.com>
2012-11-19 15:10:09 -0800

    create quiz

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117005144) do
+ActiveRecord::Schema.define(:version => 20121119230540) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -124,6 +124,12 @@ ActiveRecord::Schema.define(:version => 20121117005144) do
 
   create_table "private_tokens", :force => true do |t|
     t.string   "token",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "quizzes", :force => true do |t|
+    t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
