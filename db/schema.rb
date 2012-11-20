0e33006ee37c9af8da7a32c42af3beb22afeb91f
Shaun Dern <shaun@substantial.com>
2012-11-20 15:58:45 -0800

    add quiz_id to question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120235025) do
+ActiveRecord::Schema.define(:version => 20121120235558) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(:version => 20121120235025) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "quiz_id"
   end
 
   create_table "quizzes", :force => true do |t|
