e8194859c8863f5e852f658c7b81a33a17d584f7
Michael Natkin <michaelnatkin@gmail.com>
2012-11-21 10:17:43 -0800

    Restore this file I screwed up in last commit

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -12,6 +12,40 @@ describe ApplicationController, type: :controller do
   end
 end
 
+describe ApplicationController, '#last_stored_location_for', type: :controller do
+  controller do
+    before_filter :authenticate_user!
+
+    def show
+      head :ok
+    end
+  end
+  let(:session) { {'user_return_to' => '/return-path'} }
+
+  before do
+    controller.stub(:session) { session }
+    get :show, id: 1
+  end
+
+  it 'return nil if location has not been stored' do
+    controller.last_stored_location_for(:user).should_not be
+  end
+
+  context 'location has been stored' do
+    before { controller.stored_location_for(:user) }
+
+    it 'returns the last stored location' do
+      controller.last_stored_location_for(:user).should == '/return-path'
+    end
+
+    it 'clears last stored location on access' do
+      controller.last_stored_location_for(:user)
+      controller.last_stored_location_for(:user).should_not be
+    end
+  end
+
+end
+
 describe ApplicationController, 'version expose' do
   before do
     Version.stub(:current) { 'current version' }
