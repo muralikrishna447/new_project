5f2f643badd14ea56663c165511971ab1fd6e72b
Alex May <alex.k.may@gmail.com>
2012-11-21 18:15:22 -0800

    remove unused last_stored_location_for logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   # expose devise helper method to views
-  helper_method :after_sign_in_path_for, :last_stored_location_for
-
-  def stored_location_for(resource_or_scope)
-    session['last_user_return_to'] = session['user_return_to'] if session['user_return_to'].present?
-    super(resource_or_scope)
-  end
-
-  def last_stored_location_for(resource_or_scope)
-    session.delete('last_user_return_to')
-  end
+  helper_method :after_sign_in_path_for
 
   # On sign in, if profile isn't complete, nudge them to finish it now
   def after_sign_in_path_for(user)
