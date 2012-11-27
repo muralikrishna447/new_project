6af0c717378b2f994590e0e74b9d90810c96a6ef
Shaun Dern <shaun@substantial.com>
2012-11-27 14:00:27 -0800

    update questions controller spec

--- spec/controllers/admin/questions_controller_spec.rb
+++ spec/controllers/admin/questions_controller_spec.rb
@@ -10,10 +10,10 @@ describe Admin::QuestionsController do
   context 'index' do
     it 'returns JSON for new question' do
       Quiz.stub(:find).with('123') { quiz }
-      quiz.should_receive(:add_multiple_choice_question) { stub('question', id: 456) }
+      quiz.should_receive(:add_multiple_choice_question) { stub('question', id: 456, title: 'question title') }
 
       post :create, quiz_id: quiz.id
-      JSON.parse(response.body).should == {'id' => 456}
+      JSON.parse(response.body).should == {'id' => 456, 'title' => 'question title'}
     end
   end
 end
