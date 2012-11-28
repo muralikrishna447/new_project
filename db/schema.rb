8d9828af82e99f67db9ac628f6147908fc5714ef
Shaun Dern <shaun@substantial.com>
2012-11-27 16:57:42 -0800

    remove title from question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121121012755) do
+ActiveRecord::Schema.define(:version => 20121128002108) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(:version => 20121121012755) do
   end
 
   create_table "questions", :force => true do |t|
-    t.string   "title"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.integer  "quiz_id"
