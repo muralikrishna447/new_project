b2e63f3f39c9462fc2a34d41dff678c9879a97a7
Alex May <alex.k.may@gmail.com>
2012-11-29 13:53:26 -0800

    bootstrap quiz question data in show view

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,5 @@ class QuizzesController < ApplicationController
   before_filter :authenticate_user!
 
   expose(:quiz)
+  expose(:questions) { QuestionPresenter.present_collection(quiz.questions) }
 end
