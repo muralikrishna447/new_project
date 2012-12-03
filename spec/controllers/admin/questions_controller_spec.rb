6180c3ee89e6bdd82bbe6821f96b74c08447d7a9
Alex May <alex.k.may@gmail.com>
2012-12-03 09:53:43 -0800

    update spec to test presentation behavior

--- spec/controllers/admin/questions_controller_spec.rb
+++ spec/controllers/admin/questions_controller_spec.rb
@@ -5,15 +5,22 @@ describe Admin::QuestionsController do
 
   let(:quiz) { Fabricate.build(:quiz, id: 123) }
 
-  context 'index' do
+  context 'create' do
     let (:question) { stub('question', id: 456, contents: OpenStruct.new({foo: 'bar'})) }
 
-    it 'returns JSON for new question' do
+    before do
       Quiz.stub(:find).with('123') { quiz }
-      quiz.should_receive(:add_multiple_choice_question) { question }
+      QuestionPresenter.stub_chain(:new, :present) { {test: 'contents'} }
+    end
 
+    it 'adds a question to the quiz' do
+      quiz.should_receive(:add_multiple_choice_question) { question }
       post :create, quiz_id: quiz.id
-      JSON.parse(response.body).should == {'id' => 456, 'foo' => 'bar'}
+    end
+
+    it 'returns JSON for new question' do
+      post :create, quiz_id: quiz.id
+      JSON.parse(response.body).should == {'test' => 'contents'}
     end
   end
 end
