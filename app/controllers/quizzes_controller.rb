9762ce6d308a7ffcdce6cb880a2b861542f4b62d
Shaun Dern <shaun@substantial.com>
2012-12-03 15:37:49 -0800

    add results page to controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,8 @@ class QuizzesController < ApplicationController
   before_filter :authenticate_user!
 
   expose(:quiz) { Quiz.find_published(params[:id], params[:token]) }
+
+  def results
+
+  end
 end
