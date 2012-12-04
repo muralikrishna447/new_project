7b0781ce9c2f40e4db10735e26a7badf04d1c49f
Alex May <alex.k.may@gmail.com>
2012-12-04 11:10:20 -0800

    update question answer stats on Answer save

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,19 @@ class Answer < ActiveRecord::Base
   belongs_to :question, counter_cache: :answer_count
   belongs_to :user
 
+  after_save :update_question_statistics
+
   validates_uniqueness_of :question_id, scope: [:user_id]
   validates_presence_of :question_id
   validates_presence_of :user_id
   validates_presence_of :contents
+
+  private
+
+  def update_question_statistics
+    return if self.question.nil?
+    self.question.correct_answer_count += 1 if self.correct?
+    self.question.incorrect_answer_count += 1 unless self.correct?
+    self.question.save
+  end
 end
---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,18 @@
+describe Answer, '#update_question_statistics' do
+  let(:question) { Fabricate(:multiple_choice_question, correct_answer_count: 1, incorrect_answer_count: 1) }
+  let(:answer) { Fabricate.build(:answer, question: question, correct: @correct) }
+
+  it "increments question's correct answer count if answer is correct" do
+    @correct = true
+    answer.send(:update_question_statistics)
+    question.correct_answer_count.should == 2
+    question.incorrect_answer_count.should == 1
+  end
+
+  it "increments question's correct answer count if answer is incorrect" do
+    @correct = false
+    answer.send(:update_question_statistics)
+    question.correct_answer_count.should == 1
+    question.incorrect_answer_count.should == 2
+  end
+end
