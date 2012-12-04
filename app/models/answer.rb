ab77714787b3b162e5469da85c0dc7f1f495c358
Alex May <alex.k.may@gmail.com>
2012-12-04 12:06:45 -0800

    add support for creating answers of specific type

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ActiveRecord::Base
   include SerializeableContents
 
+  ANSWER_TYPES = [:multiple_choice]
+
   belongs_to :question, counter_cache: :answer_count
   belongs_to :user
 
@@ -11,7 +13,16 @@ class Answer < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :contents
 
+  def self.new_of_type(type, attributes = {})
+    type_valid = ANSWER_TYPES.include?(type.to_sym)
+    type_valid ? answer_class(type).new(attributes) : nil
+  end
+
   private
+
+  def self.answer_class(type)
+    "#{type}_answer".camelcase.constantize
+  end
 
   def update_question_statistics
     return if self.question.nil?
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -16,3 +16,17 @@ describe Answer, '#update_question_statistics' do
     question.incorrect_answer_count.should == 2
   end
 end
+
+describe Answer, '#new_of_type' do
+  it 'initializes an instance of specified type when type is a String' do
+    Answer.new_of_type('multiple_choice').should be_a MultipleChoiceAnswer
+  end
+
+  it 'initializes an instance of specified type when type is a Symbol' do
+    Answer.new_of_type(:multiple_choice).should be_a MultipleChoiceAnswer
+  end
+
+  it 'returns nil if specified type is unknown' do
+    Answer.new_of_type(:unknown).should be_nil
+  end
+end
