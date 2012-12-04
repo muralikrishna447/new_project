7c2972c43ec94f58b421dd9b6d14b3c9a881ad87
Alex May <alex.k.may@gmail.com>
2012-12-04 10:21:30 -0800

    add score_answer method for creating and scoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,7 @@ class Question < ActiveRecord::Base
   self.inheritance_column = :question_type
 
   belongs_to :quiz
+  has_many :answers
 
   attr_accessible :quiz_id, :contents
 
@@ -19,6 +20,15 @@ class Question < ActiveRecord::Base
 
   def contents_json(admin)
     self.contents.to_json(admin)
+  end
+
+  def score_answer(answer_data, user)
+    answer = answers.build
+    answer.user = user
+    answer.contents = answer_data
+    answer.correct = contents.correct(answer_data)
+    answer.save!
+    answer
   end
 
   private
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -22,3 +22,54 @@ describe Question, 'contents_json' do
     question.contents_json(false)
   end
 end
+
+describe Question, '#score_answer' do
+  let(:question) { Fabricate.build(:multiple_choice_question, id: 123) }
+  let(:user) { Fabricate.build(:user, id: 456) }
+
+  let(:answer_model) { Fabricate.build(:answer) }
+  let(:answer_data) { {answer: 'test'} }
+
+  before do
+    question.contents.stub(:correct)
+  end
+
+  subject { question.score_answer(answer_data, user) }
+
+  it 'returns the created answer' do
+    should be_a Answer
+  end
+
+  it "sets answer's user" do
+    subject.user.should == user
+  end
+
+  it "sets answer's contents" do
+    subject.contents.should == answer_data
+  end
+
+  it 'corrects the answer' do
+    question.contents.should_receive(:correct).with(answer_data)
+    subject
+  end
+
+  it "sets answer's correct value to true if correct call returns true" do
+    question.contents.stub(:correct).and_return(true)
+    subject.correct.should be_true
+  end
+
+  it "sets answer's correct value to false if correct call returns false" do
+    question.contents.stub(:correct).and_return(false)
+    subject.correct.should be_false
+  end
+
+  it 'associates answer with question' do
+    subject.question_id.should == question.id
+  end
+end
+
+describe Question, '#answer_count' do
+end
+
+describe Question, '#incorrect_answer_count' do
+end
