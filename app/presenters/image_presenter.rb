54c3280793da2c40747026b09b0c3b165b8106d0
Shaun Dern <shaun@substantial.com>
2012-12-04 16:45:28 -0800

    put id on newline for presenter hash

--- app/presenters/image_presenter.rb
+++ app/presenters/image_presenter.rb
@@ -1,6 +1,7 @@
 class ImagePresenter < Presenter
   def attributes
-    { id: @model.id,
+    {
+      id: @model.id,
       filename: @model.filename,
       caption: @model.caption,
       url: @model.url
