ae87525797df31a2ec421bbeb9c56392dbbb9109
Alex May <alex.k.may@gmail.com>
2012-12-07 17:40:51 -0800

    update question stats when answers are destroyed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,7 +6,8 @@ class Answer < ActiveRecord::Base
   belongs_to :question, counter_cache: :answer_count
   belongs_to :user
 
-  after_save :update_question_statistics
+  after_save :increment_question_statistics
+  before_destroy :decrement_question_statistics
 
   validates_uniqueness_of :question_id, scope: [:user_id]
   validates_presence_of :question_id
@@ -28,10 +29,17 @@ class Answer < ActiveRecord::Base
     "#{type}_answer".camelcase.constantize
   end
 
-  def update_question_statistics
+  def increment_question_statistics
     return if self.question.nil?
     self.question.correct_answer_count += 1 if self.correct?
     self.question.incorrect_answer_count += 1 unless self.correct?
+    self.question.save
+  end
+
+  def decrement_question_statistics
+    return if self.question.nil?
+    self.question.correct_answer_count -= 1 if self.correct?
+    self.question.incorrect_answer_count -= 1 unless self.correct?
     self.question.save
   end
 end
