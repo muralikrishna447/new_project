b0bab3638428e7ebda9624f602b7feaa63d3b2e9
Alex May <alex.k.may@gmail.com>
2012-12-11 10:24:55 -0800

    hook up instructions to form

--- app/models/box_sort_question.rb
+++ app/models/box_sort_question.rb
@@ -5,5 +5,13 @@ class BoxSortQuestion < Question
     update_contents(params)
     save!
   end
+
+  def instructions
+    contents.instructions || default_instructions
+  end
+
+  def default_instructions
+    "Please drag and drop the following images into one of the three piles based on which images you remember seeing in the course you've just completed."
+  end
 end
 
