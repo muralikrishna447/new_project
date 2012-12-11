d27caca3b0720c3d08f9b673b0e72e63e955df6e
Alex May <alex.k.may@gmail.com>
2012-12-10 16:39:03 -0800

    add temporary test page for box sort

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ Delve::Application.routes.draw do
   resources :quizzes, only: [:show] do
     member do
       get 'results' => 'quizzes#results'
+      get 'box-sort' => 'quizzes#box_sort'
       get ':token' => 'quizzes#show', as: 'private'
     end
   end
