289b89bba43e7c21e3e50bc46b4381a6feb2f825
Shaun Dern <shaun@substantial.com>
2012-12-11 15:49:53 -0800

    allow box sort image delegate allow nil

--- app/models/box_sort_image.rb
+++ app/models/box_sort_image.rb
@@ -2,7 +2,7 @@ class BoxSortImage < ActiveRecord::Base
   belongs_to :question, class_name: 'BoxSortQuestion'
   has_one :image, as: :imageable
 
-  delegate :filename, :url, :caption, to: :image
+  delegate :filename, :url, :caption, to: :image, allow_nil: true
 
   attr_accessible :key_image, :key_rationale, :question_id
 end
