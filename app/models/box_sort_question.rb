6c13b27819c4727dce4248b4be8cd9e739ff8186
Alex May <alex.k.may@gmail.com>
2012-12-11 10:28:03 -0800

    add option display to form

--- app/models/box_sort_question.rb
+++ app/models/box_sort_question.rb
@@ -10,8 +10,22 @@ class BoxSortQuestion < Question
     contents.instructions || default_instructions
   end
 
+  def options
+    contents.options || default_options
+  end
+
   def default_instructions
     "Please drag and drop the following images into one of the three piles based on which images you remember seeing in the course you've just completed."
+  end
+
+  def default_options
+    [{
+      text: "I remember"
+    },{
+      text: "I'm not sure"
+    },{
+      text: "I don't remember"
+    }]
   end
 end
 
