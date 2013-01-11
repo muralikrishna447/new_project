2f67f22dd4f8f82624b46858245e99afdc2c6159
David Balatero <dbalatero@gmail.com>
2013-01-11 15:05:56 -0800

    Add a OrderSortQuestion model.

--- spec/models/order_sort_question_spec.rb
+++ spec/models/order_sort_question_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe OrderSortQuestion do
+  describe '#update_from_params' do
+    let(:params) do
+      {
+        'order_sort_question' => {
+          'question' => 'How do you cook?',
+          'instructions' => 'Arrange the photos in order.',
+          'solutions' => [
+            { 'order_sort_image_ids' => [1, 2, 3, 4] }
+          ]
+        }
+      }
+    end
 
+    it 'should serialize correctly' do
+      subject.update_from_params(params)
+      subject.contents.question.should == 'How do you cook?'
+    end
+
+    it 'should save' do
+      subject.update_from_params(params)
+      subject.should_not be_new_record
+    end
+  end
 end
