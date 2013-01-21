4dcb50852e65a16f1d3f4ff49b49eb683d729e8d
hueezer <hueezer@gmail.com>
2013-01-21 10:42:30 -0800

    update seed with updated admin account

--- db/schema.rb
+++ db/schema.rb
@@ -297,7 +297,6 @@ ActiveRecord::Schema.define(:version => 20130119011958) do
     t.string   "website",                :default => ""
     t.text     "quote",                  :default => ""
     t.string   "chef_type",              :default => "", :null => false
-    t.string   "slug"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
