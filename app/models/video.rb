63722f780414971caf795d6b90ebfebffbe41366
hueezer <hueezer@gmail.com>
2013-01-22 13:09:36 -0800

    add some tests for filmstrip videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -14,8 +14,8 @@ class Video < ActiveRecord::Base
     featured_video = Video.featured_random
     if featured_video && filmstrip.any?
       [featured_video] + filmstrip.sample(4)
-    elsif featured_video
-      filmstrip.sample(5)
+    elsif featured_video && filmstrip.blank?
+      [featured_video] + Activity.new_content.limit(4)
     else
       Activity.new_content
     end
