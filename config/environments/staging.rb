c98542f9fdf9ae69b8434f88c79d04c6980e6adb
hueezer <hueezer@gmail.com>
2013-01-24 18:09:56 -0800

    undo assets test

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,7 +46,7 @@ Delve::Application.configure do
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://diufzcf311ks0.cloudfront.net"
+  config.action_controller.asset_host = "http://diufzcf311ks0.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( print.css styleguide.css global_navigation.css forum_nav.css active_admin.css active_admin/print.css navigation_bootstrap.js active_admin.js  jquery.mjs.nestedSortable.js)
