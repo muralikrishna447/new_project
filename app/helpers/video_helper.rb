bb8bf14063e82f9c66d38af11fae58acb0000a30
hueezer <hueezer@gmail.com>
2013-01-28 15:39:03 -0800

    autoplay only for home page

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -1,6 +1,6 @@
 module VideoHelper
-  def youtube_url(youtube_id)
-    "http://www.youtube.com/embed/#{youtube_id}?wmode=opaque&rel=0&modestbranding=1&showinfo=0&vq=hd720&autoplay=1"
+  def youtube_url(youtube_id, autoplay=0)
+    "http://www.youtube.com/embed/#{youtube_id}?wmode=opaque&rel=0&modestbranding=1&showinfo=0&vq=hd720&autoplay=#{autoplay}"
   end
 
   def youtube_image(youtube_id,thumbnail=0)
