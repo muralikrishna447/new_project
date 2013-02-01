dd9bfb5686d904a2665d8605b77e96d390998f09
hueezer <hueezer@gmail.com>
2013-01-31 16:02:40 -0800

    search css tweaks

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def index
     query = params[:q]
     if query
-      @results = Activity.published.where('title iLIKE ?', "%#{query}%")
+      @results = Activity.published.where('title iLIKE ?', "%#{query}%").reject{|a| a.step_images.blank? }
     else
       @results = Activity.published.where('title not iLIKE ?', '%quiz%').limit(9)
     end
