86cfe6cc743dba9a68e8bfb13f48335e6fc9bcb1
hueezer <hueezer@gmail.com>
2013-01-31 21:20:15 -0800

    full text search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,8 +6,6 @@ class SearchController < ApplicationController
       # @results = Activity.published.where('title @@ :q or description @@ :q', q: query).reject{|a| a.step_images.blank? }
       # @results = Activity.published.text_search(query).reject{|a| a.step_images.blank? }
       @results = PgSearch.multisearch(query)
-    else
-      @results = Activity.published.where('title not iLIKE ?', '%quiz%').limit(9)
     end
   end
 end
