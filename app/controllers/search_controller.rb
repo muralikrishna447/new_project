daf9015f57411940719fd65084bf367329f4c522
hueezer <hueezer@gmail.com>
2013-02-01 14:35:27 -0800

    updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,9 @@ class SearchController < ApplicationController
       # @results = Activity.published.where('title @@ :q or description @@ :q', q: query).reject{|a| a.step_images.blank? }
       # @results = Activity.published.text_search(query).reject{|a| a.step_images.blank? }
       @results = PgSearch.multisearch(query).paginate(:page => params[:page], :per_page => 12)
+      if @results.blank?
+        @noresults = Activity.published.limit(6)
+      end
     end
   end
 end
