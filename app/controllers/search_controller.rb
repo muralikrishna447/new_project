95f4aea1f322c475f8b2976a3762120dd5ef496b
hueezer <hueezer@gmail.com>
2013-02-02 16:33:30 -0800

    related results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,10 @@ class SearchController < ApplicationController
     if params[:q]
       @results = Search.query(params[:q]).page(params[:page]).per(12)
       if @results.blank?
-        @noresults = Activity.published.limit(6)
+        @noresults = Search.query('Recipe').limit(9)
+      elsif @results.count < 6
+        related_terms = @results.map(&:searchable).map(&:title).join(',')
+        @related_results = (Search.query(related_terms) - @results).take(3)
       end
     end
   end
