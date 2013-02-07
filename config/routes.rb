913cbed85091e9fe834e9817fba06f5a6a2aa21a
hueezer <hueezer@gmail.com>
2013-02-06 18:55:01 -0800

    fix registration bug

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Delve::Application.routes.draw do
 
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
+    get "sign_up", to: 'users/registrations#new'
   end
 
   get 'authenticate-sso' => 'sso#index', as: 'forum_sso'
