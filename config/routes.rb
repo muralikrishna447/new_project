2af27c8044af45b75f18da0b7ce3224573ceba22
hueezer <hueezer@gmail.com>
2013-02-07 17:56:47 -0800

    updates from aweber after password change

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Delve::Application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
   }
 
   devise_scope :user do
