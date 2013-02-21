be46c13e47c2f54208b77035ec0e9b9296bba74d
hueezer <hueezer@gmail.com>
2013-02-20 17:15:19 -0800

    update bootstrap

--- app/controllers/recipe_gallery_controller.rb
+++ app/controllers/recipe_gallery_controller.rb
@@ -1,6 +1,6 @@
 class RecipeGalleryController < ApplicationController
 
   def index
-      @recipes = Activity.published.joins(:ingredients).order('created_at DESC').uniq.page(params[:page]).per(12)
+      @recipes = Activity.published.joins(:ingredients).order('created_at DESC').uniq.page(params[:page]).per(1)
   end
 end
