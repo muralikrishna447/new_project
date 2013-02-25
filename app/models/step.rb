520078e1deb71d2c87f75fdb8ffdca2dbdeef6af
Michael Natkin <michaelnatkin@gmail.com>
2013-02-25 11:08:13 -0800

    ready to rok

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,7 +3,6 @@ class Step < ActiveRecord::Base
   ranks :step_order, with_same: :activity_id
 
   belongs_to :activity, touch: true, inverse_of: :steps
-  belongs_to :recipe, touch: true, inverse_of: :steps
 
   has_many :ingredients, class_name: StepIngredient, dependent: :destroy, inverse_of: :step
 
