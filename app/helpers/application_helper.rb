a74d1067ff0c7825e169434fcb08752ce304d4b8
Michael Natkin <michaelnatkin@gmail.com>
2013-03-08 14:36:23 -0800

    shortcodes working well enough to deploy, more to do

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,18 +46,21 @@ module ApplicationHelper
   def apply_shortcode(orig, shortcode, contents)
     case shortcode
       when 'c'
-        c = contents
-        f = (c.to_f * 1.8 + 32).round
-        "#{f} &deg;F / #{c} &deg;C"
+        "<a class='temperature-group'><span class='temperature'>#{contents}</span> <span class='temperature-unit'>&deg;C</span></a>"
 
-      when 'f'
-        f = contents
-        c = ((f.to_f - 32) / 1.8).round
-        "#{f} &deg;F / #{c} &deg;C"
+      when 'cm'
+        "<a class='length-group'><span class='length' data-orig-value='#{contents}'>#{contents} cm</span></a>"
+
+      when 'mm'
+        "<a class='length-group'><span class='length' data-orig-value='#{contents.to_f / 10.0}'>#{contents} mm</span></a>"
+
+      when 'g'
+        "<span class='text-quantity-group'><span class='quantity-group qtyfade'><span class='lbs-qty'></span> <span class='lbs-label'></span> <span class='main-qty'>#{contents}</span></span> <span class='unit qtyfade'>g</span></span>"
 
       when 'amzn'
         asin, anchor_text = contents.split(/\s/, 2)
-        link_to anchor_text, "http://www.amazon.com/dp/#{asin}/?tag=delvkitc-20"
+        link_to anchor_text, "http://www.amazon.com/dp/#{asin}/?tag=delvkitc-20", target: "_blank"
+
       else
         orig
     end
@@ -67,7 +70,7 @@ module ApplicationHelper
     text.gsub(/\[(\w+)\s+([^\]]*)\]/) do |orig|
       shortcode = $1
       contents = $2
-      apply_shortcode(orig, shortcode, contents)
+      apply_shortcode(orig, shortcode, contents).html_safe
     end
   end
 end
