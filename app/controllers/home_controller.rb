acef19a4c240588df111983149d01a8b243d3584
hueezer <hueezer@gmail.com>
2013-03-18 12:29:17 -0700

    home controller cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
-  # expose(:homepage_blurb) { Copy.find_by_location('homepage-blurb') }
-  # expose(:bio_chris) { Copy.find_by_location('creator-chris') }
-  # expose(:bio_grant) { Copy.find_by_location('creator-grant') }
-  # expose(:bio_ryan) { Copy.find_by_location('creator-ryan') }
-  # expose(:version) { Version.current }
 
   def index
-    # @featured_id = Video.featured_id
-    # @filmstrip = Video.filmstrip_videos
-    # @croppable = @filmstrip.map(&:class).include?(Activity)
     @recipes = Activity.published.recipes.includes(:steps).order("RANDOM()").first(6)
     @techniques = Activity.published.techniques.includes(:steps).order("RANDOM()").first(6)
     @sciences = Activity.published.sciences.includes(:steps).order("RANDOM()").first(6)
