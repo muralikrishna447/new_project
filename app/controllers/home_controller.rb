b815498e264b4696aa438e2066adb40f52b1e534
hueezer <hueezer@gmail.com>
2013-03-18 21:59:11 -0700

    update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
 
   def index
-    @recipes = Activity.published.recipes.includes(:steps).order("RANDOM()").first(6)
-    @techniques = Activity.published.techniques.includes(:steps).order("RANDOM()").first(6)
-    @sciences = Activity.published.sciences.includes(:steps).order("RANDOM()").first(6)
+    @recipes = Activity.published.recipes.includes(:steps).order('created_at ASC').first(6)
+    @techniques = Activity.published.techniques.includes(:steps).order('created_at ASC').first(6)
+    @sciences = Activity.published.sciences.includes(:steps).order('created_at ASC').first(6)
     @heroes = [ Activity.published.recipes.includes(:steps).order('updated_at ASC').last,
-                Activity.published.techniques.includes(:steps).last,
-                Activity.published.sciences.includes(:steps).last ]
+                Activity.published.techniques.includes(:steps).order('updated_at ASC').last,
+                Activity.published.sciences.includes(:steps).order('updated_at ASC').last ]
     @discussion = Forum.discussions.first
     @status = Twitter.status_embed
   end
