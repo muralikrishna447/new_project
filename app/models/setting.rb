764c090da2787375e386bfbe2844d9df5dd00f02
hueezer <hueezer@gmail.com>
2013-03-19 17:31:23 -0700

    default footer image

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,11 @@
 class Setting < ActiveRecord::Base
   attr_accessible :footer_image
+
+  def self.footer_image
+    if self.any?
+      self.last.footer_image
+    else
+      "final_dish_IMG_5402.jpg"
+    end
+  end
 end
