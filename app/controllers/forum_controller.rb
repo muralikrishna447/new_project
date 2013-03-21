12e1bd32382e52d69ef5925375904b9ddf496dbb
hueezer <hueezer@gmail.com>
2013-03-20 23:22:07 -0700

    action cache forum

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -1,5 +1,7 @@
 class ForumController < ApplicationController
   
+  caches_action :discussion, :expires_in => 10.minutes
+
   def discussion
     @discussion = Forum.discussions.first
     render json: @discussion
