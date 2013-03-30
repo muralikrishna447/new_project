505b53d35d75ab7d9b0df4142e65bfc9ca6c9f68
hueezer <hueezer@gmail.com>
2013-03-29 18:23:52 -0700

    user activity default order

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -2,4 +2,6 @@ class UserActivity < ActiveRecord::Base
   attr_accessible :action, :activity_id, :user_id
   belongs_to :user
   belongs_to :activity
+
+  default_scope order('created_at DESC')
 end
