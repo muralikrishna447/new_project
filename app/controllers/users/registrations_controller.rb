58a42d21f44d7069adbc88038a5bd464de1ed490
hueezer <hueezer@gmail.com>
2013-04-10 13:53:38 -0700

    update aweber listname for popup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,11 +11,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
         redirect_to sign_in_url(email: email)
       end
     else
+      if params[:source] == 'popup'
+        listname = 'popup'
+      else
+        listname = 'cs_c_sousvide'
+      end
       if Rails.env.production?
         uri = URI.parse("http://www.aweber.com/scripts/addlead.pl")
         response = Net::HTTP.post_form(uri,
                                         { "email" => email,
-                                          "listname" => "cs_c_sousvide",
+                                          "listname" => listname,
                                           "meta_adtracking" => "site_top_form"})
       else
         logger.debug 'Newsletter Signup'
