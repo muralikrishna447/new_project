2f7a82d38b536bc33e2975c8a86552926c9d99c9
hueezer <hueezer@gmail.com>
2013-04-15 13:23:59 -0700

    add spherification template

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,5 +5,12 @@ class CoursesController < ApplicationController
   expose(:course) { Course.find(params[:id]) }
   expose(:bio_chris) { Copy.find_by_location('instructor-chris') }
   expose(:bio_grant) { Copy.find_by_location('instructor-grant') }
+
+  def show
+    @course = Course.find(params[:id])
+    if @course.title == 'Spherification'
+      render 'spherification'
+    end
+  end
 end
 
