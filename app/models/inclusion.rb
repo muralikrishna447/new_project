04788dec591198586f847a20a1d65d174b018480
hueezer <hueezer@gmail.com>
2013-04-25 18:01:08 -0700

    eager load inclusion activity

--- app/models/inclusion.rb
+++ app/models/inclusion.rb
@@ -2,7 +2,7 @@ class Inclusion < ActiveRecord::Base
   include RankedModel
 
   ranks :activity_order, :with_same => :course_id
-
+  default_scope include: :activity
   scope :ordered, rank(:activity_order)
   belongs_to :course
   belongs_to :activity
