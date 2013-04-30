a79cbca0e8c3d308529feea5d8f181b6cf5fb6bb
hueezer <hueezer@gmail.com>
2013-04-30 13:21:05 -0700

    uploads update

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,7 +3,7 @@ class UploadsController < ApplicationController
     @upload = Upload.new(params[:upload])
     @upload.user_id = current_user.id
     if @upload.save
-      redirect_to session[:return_to]
+      redirect_to session[:return_to], notice: 'Your photo has been upload!'
       session[:return_to] = nil
     else
     end
