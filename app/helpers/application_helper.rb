73b342a2ef00231ad62f855830bf432b922aea46
hueezer <hueezer@gmail.com>
2013-05-09 17:56:44 -0700

    [spherification] where user left off btn update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -142,14 +142,14 @@ module ApplicationHelper
     end
   end
 
-  def where_user_left_off_in_course(course, user)
+  def where_user_left_off_in_course(course, user, btn_class = nil)
     viewed = user.viewed_activities_in_course(course)
     if viewed.count == 0
       first_activity = course.first_published_activity
-      link_to 'Start the Course', [course, first_activity], class: 'btn btn-primary'
+      link_to "Start the Course #{content_tag :i, nil, class: 'icon-chevron-right'}", [course, first_activity], class: btn_class
     else
       current_activity = viewed.last
-      link_to 'Continue Course', [course, current_activity], class: 'btn btn-primary'
+      link_to "Continue Course #{content_tag :i, nil, class: 'icon-chevron-right'}".html_safe, [course, current_activity], class: btn_class
     end
   end
 
