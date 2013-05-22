f3b4e22dd76d9abc0f53586e823c124a812e72f5
hueezer <hueezer@gmail.com>
2013-05-22 16:02:38 -0700

    [enrollment] validation

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,6 @@ class Enrollment < ActiveRecord::Base
   attr_accessible :course_id, :user_id
   belongs_to :user
   belongs_to :course
+
+  validates :course_id, uniqueness: {scope: :user_id, message: 'can only be enrolled once per student.'}
 end
