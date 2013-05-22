84b106537da353e4a83524a9b79b84b7cd2ad09c
hueezer <hueezer@gmail.com>
2013-05-22 16:42:03 -0700

    [likes] add like validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :likeable, polymorphic: true
+
+  validates :user_id, uniqueness: {scope: [:likeable_id, :likeable_type], message: 'can only like an item once.'}
 end
