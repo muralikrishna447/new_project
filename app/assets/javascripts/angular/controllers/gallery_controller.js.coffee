46b75f0f1cd932e90226eba4bee0a7f006c784dc
hueezer <hueezer@gmail.com>
2013-06-06 03:07:49 -0700

    [gallery] angular

--- app/assets/javascripts/angular/controllers/gallery_controller.js.coffee
+++ app/assets/javascripts/angular/controllers/gallery_controller.js.coffee
@@ -1,3 +1,13 @@
-angular.module('ChefStepsApp').controller 'GalleryController', ["$scope", ($scope) ->
-  $scope.data = {message: 'Hello'}
+angular.module('ChefStepsApp').controller 'GalleryController', ["$scope", "$resource", ($scope, $resource) ->
+  Recipe = $resource('/recipe-gallery/:id')
+  $scope.recipes = Recipe.query()
+
+  $scope.recipeImageURL = (recipe, width) ->
+    url = ""
+    if recipe.featured_image_id
+      url = JSON.parse(recipe.featured_image_id).url
+      url + "/convert?fit=max&w=#{width}&cache=true"
+    else
+      url = JSON.parse(recipe.image_id).url
+      url + "/convert?fit=max&w=#{width}&cache=true"
 ]
--- app/controllers/recipe_gallery_controller.rb
+++ app/controllers/recipe_gallery_controller.rb
@@ -5,6 +5,11 @@ class RecipeGalleryController < ApplicationController
 
   def index
     # @recipes = Activity.published.recipes.order('created_at DESC').uniq.page(params[:page]).per(12)
-    @recipes = apply_scopes(Activity).published.recipes.order('created_at DESC').uniq.page(params[:page]).per(12)
+    # @recipes = apply_scopes(Activity).published.recipes.order('created_at DESC').uniq.page(params[:page]).per(12)
+    @recipes = apply_scopes(Activity).published.recipes.order('created_at DESC')
+    respond_to do |format|
+      format.html { render :html => @recipes }
+      format.json { render :json => @recipes.to_json }
+    end
   end
 end
