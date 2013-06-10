e7b8f3c727458b88dfc3e7f734a25c5ad0795ca9
hueezer <hueezer@gmail.com>
2013-06-10 02:04:36 -0700

    [gallery] improvements

--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -1,10 +1,11 @@
 class TechniquesController < ApplicationController
   def index
     @techniques = Activity.techniques.published.page(params[:page]).per(12)
+    @techniques_count = Activity.published.techniques.count
   end
 
   def index_as_json
-    @techniques = Activity.techniques.published.randomize
+    @techniques = Activity.techniques.published.order('published_at DESC').includes(:steps).page(params[:page]).per(9)
     respond_to do |format|
       format.json { render :json => @techniques.to_json(only: [:title, :image_id, :featured_image_id, :difficulty, :published_at, :slug], :include => :steps) }
     end
