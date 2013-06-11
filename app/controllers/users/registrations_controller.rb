912a72b77c2d0a3ad4f600c9d9def99c274dad91
hueezer <hueezer@gmail.com>
2013-06-11 01:21:11 -0700

    [spherification] split should now be tracking conversions correctly

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,6 +61,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       @enrollment = Enrollment.new(user_id: current_user.id, course_id: @course.id)
       if @enrollment.save
         redirect_to course_url(@course), notice: "Thanks for enrolling! Please check your email now to confirm your registration."
+        finished('spherification', :reset => false)
       end
     else
       redirect_to course_url(@course), notice: "Sorry, there was a problem with the information provided.  Please try again."
