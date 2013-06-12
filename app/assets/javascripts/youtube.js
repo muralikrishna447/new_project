5aa7e8431df9e1738ab826c044ee703153c81d04
hueezer <hueezer@gmail.com>
2013-06-11 17:32:56 -0700

    [end action] setup

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -1,31 +1,21 @@
-// var tag = document.createElement('script');
 
-// tag.src = "https://www.youtube.com/iframe_api";
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('spherification-player', {
     events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
+      'onReady': onSpherificationPlayerReady,
+      'onStateChange': onSpherificationPlayerStateChange
     }
   });
 }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
+// The API will call this function when the video player is ready.
+// function onSpherificationPlayerReady(event) {
+//   event.target.playVideo();
+// }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-
-function onPlayerStateChange(event) {
+// The API calls this function when the player's state changes.
+function onSpherificationPlayerStateChange(event) {
   if (event.data == YT.PlayerState.ENDED) {
     console.log('ended');
   }
