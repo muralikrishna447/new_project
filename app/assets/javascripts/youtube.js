75597afe7df08225154c6520cec7915c013b96a6
hueezer <hueezer@gmail.com>
2013-06-11 18:32:20 -0700

    [end action] updates

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -10,13 +10,16 @@ function onYouTubeIframeAPIReady() {
 }
 
 // The API will call this function when the video player is ready.
-// function onSpherificationPlayerReady(event) {
-//   event.target.playVideo();
-// }
+function onSpherificationPlayerReady(event) {
+  event.target.playVideo();
+}
 
 // The API calls this function when the player's state changes.
 function onSpherificationPlayerStateChange(event) {
   if (event.data == YT.PlayerState.ENDED) {
     console.log('ended');
+    $(document).ready(function(){
+      $('.course-end-action').addClass('course-end-action-show');
+    });
   }
 }
