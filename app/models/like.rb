fa7b59f105a3e520ce401350c5e55808a60fc2e7
hueezer <hueezer@gmail.com>
2013-06-17 01:42:51 -0700

    [likes] counter cache

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ActiveRecord::Base
   attr_accessible :likeable_id, :likeable_type, :user_id
 
   belongs_to :user
-  belongs_to :likeable, polymorphic: true
+  belongs_to :likeable, polymorphic: true, counter_cache: true
 
   validates :user_id, uniqueness: {scope: [:likeable_id, :likeable_type], message: 'can only like an item once.'}
 end
