9e43c40f233a2dff8d7f2af2a9377ff1d3861eb7
hueezer <hueezer@gmail.com>
2013-06-18 18:13:10 -0700

    [courses] add additional script field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130617183832) do
+ActiveRecord::Schema.define(:version => 20130619005926) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(:version => 20130617183832) do
     t.datetime "updated_at",                           :null => false
     t.string   "short_description"
     t.text     "image_id"
+    t.text     "additional_script"
   end
 
   create_table "enrollments", :force => true do |t|
