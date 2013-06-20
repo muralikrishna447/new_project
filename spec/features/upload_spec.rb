bf1809f2548d96f5ec2ee8ac526a7be3526b4b0a
hueezer <hueezer@gmail.com>
2013-06-19 17:47:21 -0700

    [uploads] update upload spec

--- spec/features/upload_spec.rb
+++ spec/features/upload_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-feature 'uploads', js: true do
+feature 'uploads' do
 
   before :each do
     @activity_1 = Fabricate :activity, title: 'Activity', description: 'hello', published: true
   end
+
   scenario 'user can upload a photo for activities' do
     visit activity_path(@activity_1)
-    find_link('UPLOAD YOUR OWN').click
+    find_link('UPLOAD YOUR OWN').visible?
     find('#upload-user-creation').visible?
   end
+
 end
