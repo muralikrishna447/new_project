2769a186a3ebffa8e45cf6f98b018516fabdf429
Michael Natkin <michaelnatkin@gmail.com>
2013-06-21 13:57:43 -0700

    more refactoring and fix up sciences and techniques

--- app/controllers/sciences_controller.rb
+++ app/controllers/sciences_controller.rb
@@ -1,20 +1,25 @@
 class SciencesController < ApplicationController
 
   has_scope :by_published_at
+  has_scope :by_updated_at
   has_scope :difficulty
+  has_scope :published_status, default: "Published" do |controller, scope, value|
+    value == "Published" ? scope.published.sciences.includes(:steps) : scope.unpublished.sciences.where("title != 'DUMMY NEW ACTIVITY'")
+  end
 
   def index
-    @sciences = apply_scopes(Activity).sciences.published.page(params[:page]).per(12)
+    @sciences = apply_scopes(Activity).sciences.order('published_at DESC').uniq.page(params[:page]).per(12)
     @sciences_count = Activity.published.sciences.count
   end
 
   def index_as_json
-    @sciences = apply_scopes(Activity).sciences.published.order('published_at DESC').includes(:steps).page(params[:page]).per(12)
+    @sciences = apply_scopes(Activity).page(params[:page]).per(12)
+
     respond_to do |format|
       format.json { render :json => @sciences.to_json(only: [:title, :image_id, :featured_image_id, :difficulty, :published_at, :slug], :include => :steps) }
     end
   end
-
+  
   def show
     @science = Activity.sciences.find(params[:id])
   end
--- app/controllers/techniques_controller.rb
+++ app/controllers/techniques_controller.rb
@@ -1,20 +1,25 @@
 class TechniquesController < ApplicationController
 
   has_scope :by_published_at
+  has_scope :by_updated_at
   has_scope :difficulty
+  has_scope :published_status, default: "Published" do |controller, scope, value|
+    value == "Published" ? scope.published.techniques.includes(:steps) : scope.unpublished.techniques.where("title != 'DUMMY NEW ACTIVITY'")
+  end
 
   def index
-    @techniques = apply_scopes(Activity).techniques.published.page(params[:page]).per(12)
+    @techniques = apply_scopes(Activity).techniques.order('published_at DESC').uniq.page(params[:page]).per(12)
     @techniques_count = Activity.published.techniques.count
   end
 
   def index_as_json
-    @techniques = apply_scopes(Activity).techniques.published.order('published_at DESC').includes(:steps).page(params[:page]).per(12)
+    @techniques = apply_scopes(Activity).page(params[:page]).per(12)
+
     respond_to do |format|
       format.json { render :json => @techniques.to_json(only: [:title, :image_id, :featured_image_id, :difficulty, :published_at, :slug], :include => :steps) }
     end
   end
-
+  
   def show
     @technique = Activity.techniques.find(params[:id])
   end
