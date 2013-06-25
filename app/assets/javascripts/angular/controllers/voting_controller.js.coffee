f392cc92fc37296626b86b0602a6505adb125008
hueezer <hueezer@gmail.com>
2013-06-25 16:30:31 -0700

    [polls] angular

---
+++ app/assets/javascripts/angular/controllers/voting_controller.js.coffee
@@ -0,0 +1,3 @@
+angular.module('ChefStepsApp').controller 'VotingController', ["$scope", "$resource", "$location", ($scope, $resource, $location) ->
+  alert 'hello'
+]
--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -2,4 +2,8 @@ class PollsController < ApplicationController
   def index
     @polls = Poll.order('created_at DESC').page(params[:page]).per(12)
   end
+
+  def show
+    @poll = Poll.find(params[:id])
+  end
 end
