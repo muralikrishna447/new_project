9efe9ada57ed6fef5473d90598e70c0e2ffc51ed
hueezer <hueezer@gmail.com>
2013-07-01 15:07:18 -0700

    [polls] admin

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,6 @@
 class Poll < ActiveRecord::Base
-  attr_accessible :description, :slug, :status, :title
+  attr_accessible :description, :slug, :status, :title, :poll_items_attributes
   has_many :poll_items
+
+  accepts_nested_attributes_for :poll_items
 end
