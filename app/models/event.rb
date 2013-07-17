17b45cdb4a4bce8ca071f7e9e9697174c927c993
hueezer <hueezer@gmail.com>
2013-07-17 12:13:52 -0700

    [activity stream] limit to 4

--- app/models/event.rb
+++ app/models/event.rb
@@ -45,7 +45,7 @@ class Event < ActiveRecord::Base
   end
 
   def self.stream
-    # timeline.where(action: 'received_create').group_by{|e| e.group_name}
-    timeline.group_by{|e| e.group_name}
+    timeline.where(action: 'received_create').group_by{|e| e.group_name}
+    # timeline.group_by{|e| e.group_name}
   end
 end
