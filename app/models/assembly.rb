8ac44365f5c3725d022126fa1c09d8988bb70b6b
hueezer <hueezer@gmail.com>
2013-07-23 15:59:03 -0700

    [assembly] ingredients

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -3,5 +3,9 @@ class Assembly < ActiveRecord::Base
   friendly_id :title, use: [:slugged, :history]
   attr_accessible :description, :image_id, :title, :youtube_id
   has_many :assembly_inclusions
+  has_many :includables, through: :assembly_inclusions
 
+  def ingredients
+    assembly_inclusions.map(&:includable).map(&:ingredients).flatten.sort_by{|i|i.ingredient.title}
+  end
 end
