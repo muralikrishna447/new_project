2ba69c699997c64e5da7a6a7c1365aad1005eb38
hueezer <hueezer@gmail.com>
2013-07-23 19:13:46 -0700

    [assemblies] update

--- app/assets/javascripts/force_aspect.js.coffee
+++ app/assets/javascripts/force_aspect.js.coffee
@@ -1,7 +1,13 @@
-$ ->
+window.forceAspect = ->
   $('.force-aspect').each ->
-    width = $(this).closest('.span6').width()
+    width = $(this).closest('.force-aspect-wrapper').width()
     console.log width
     height = (width * 9.0 / 16.0) + "px"
     console.log height
     $(this).height(height)
+
+$ ->
+  window.forceAspect()
+
+  $(window).bind 'resize', ->
+    window.forceAspect()
--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -1,7 +1,7 @@
 class Assembly < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
-  attr_accessible :description, :image_id, :title, :youtube_id, :slug, :assembly_type
+  attr_accessible :description, :image_id, :title, :youtube_id, :slug, :assembly_type, :assembly_inclusions_attributes
   has_many :assembly_inclusions
   has_many :includables, through: :assembly_inclusions
 
