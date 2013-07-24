6154fd95b3267e31d082a96313321feb8fcf1250
hueezer <hueezer@gmail.com>
2013-07-23 18:59:34 -0700

    [assemblies] setup admin

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -1,9 +1,11 @@
 class Assembly < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
-  attr_accessible :description, :image_id, :title, :youtube_id
+  attr_accessible :description, :image_id, :title, :youtube_id, :slug, :assembly_type
   has_many :assembly_inclusions
   has_many :includables, through: :assembly_inclusions
+
+  accepts_nested_attributes_for :assembly_inclusions
 
   def ingredients
     assembly_inclusions.map(&:includable).map(&:ingredients).flatten.sort_by{|i|i.ingredient.title}.reject{|i| i.unit == 'recipe'}
