c95ceac8f6a721606b7274c5a841174c822ee89b
hueezer <hueezer@gmail.com>
2013-08-05 14:04:20 -0700

    [stream] angularized comments

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,14 +1,14 @@
 class Stream
   def self.recieved(user)
-    user.events.timeline.where(action: 'received_create').uniq!{|e| e.group_name}
+    user.events.timeline.where(action: 'received_create').order('created_at asc').uniq!{|e| e.group_name}
   end
 
   def self.created(user)
-    user.events.includes(:trackable).timeline.where('action != ?', 'received_create').uniq!{|e| e.group_name}
+    user.events.includes(:trackable).timeline.where('action != ?', 'received_create').order('created_at asc').uniq!{|e| e.group_name}
   end
 
   def self.followings(user)
-    stream_events = Event.includes(:trackable).timeline.where(user_id: user.following_ids).where('action != ?', 'received_create')
+    stream_events = Event.includes(:trackable).timeline.where(user_id: user.following_ids).where('action != ?', 'received_create').order('created_at asc')
     stream_events.uniq!{|e| e.group_name}
   end
 end
