17828844ff15e8331e33e0d7bc9b896dea313606
hueezer <hueezer@gmail.com>
2013-08-06 10:00:04 -0700

    [stream] likes

--- app/serializers/upload_serializer.rb
+++ app/serializers/upload_serializer.rb
@@ -1,4 +1,13 @@
 class UploadSerializer < ActiveModel::Serializer
-  attributes :id, :title, :notes, :image_id
+  attributes :id, :title, :notes, :featured_image, :likes_count, :like_users
   has_one :user
+
+  def featured_image
+    object.featured_image
+  end
+
+  def like_users
+    object.likes.map(&:user)
+  end
+
 end
