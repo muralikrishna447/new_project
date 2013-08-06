19bf5d63bba47b346036fa00f598e5b5b28203e2
hueezer <hueezer@gmail.com>
2013-08-06 09:51:30 -0700

    [streams] like activity

---
+++ app/serializers/activity_serializer.rb
@@ -0,0 +1,11 @@
+class ActivitySerializer < ActiveModel::Serializer
+  attributes :id, :title, :created_at, :youtube_id, :difficulty, :yield, :timing, :description, :published, :slug, :image_id, :featured_image_id, :activity_type, :last_edited_by_id, :source_activity_id, :source_type, :published_at, :author_notes, :likes_count, :currently_editing_user, :include_in_gallery, :creator, :featured_image, :like_users
+
+  def featured_image
+    object.featured_image
+  end
+
+  def like_users
+    object.likes.map(&:user)
+  end
+end
