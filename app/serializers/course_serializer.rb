72d75856a7e0ec88e85e35226adff84e80aeb7ce
hueezer <hueezer@gmail.com>
2013-08-06 10:14:14 -0700

    [streams] course enroll

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,3 +1,7 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :featured_image
+  attributes :id, :title, :featured_image, :path
+  
+  def path
+    course_path(object)
+  end
 end
