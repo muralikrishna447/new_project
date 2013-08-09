ca102c375899fbe186cad69989fea44d915f5ee1
hueezer <hueezer@gmail.com>
2013-08-09 12:39:01 -0700

    [streams] use uniq instead of uniq!

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -9,6 +9,7 @@ class Stream
 
   def self.followings(user)
     stream_events = Event.includes(:trackable).timeline.where(user_id: user.following_ids).where('action != ?', 'received_create').order('created_at asc')
-    stream_events.uniq!{|e| e.group_name}
+    # TODO find out why uniq and uniq! sometimes works
+    stream_events.uniq{|e| e.group_name}
   end
 end
