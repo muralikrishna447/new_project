bb68ef37818602bd30317a976ce722cc64b8c984
hueezer <hueezer@gmail.com>
2013-08-13 15:59:18 -0700

    [stream] event spec cleanup

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -53,52 +53,6 @@ describe Event do
       Stream.all_events.first.should == @event
     end
 
-    # Events that should not show up in the stream
-
-    it 'does not return a like create item if activity is unpublished' do
-      @unpublished = Fabricate :activity, title: 'Unpublished Activity', description: 'An unpublished activity', published: false
-      @like = Fabricate :like, likeable: @unpublished, user: @user
-      @event = Fabricate :event, trackable: @like, action: 'create', user: @user
-      Stream.all_events.length.should == 0
-    end
-
-    # it 'returns a comment received item when a user comments' do
-    #   @user2 = Fabricate :user, name: 'Tester 2'
-    #   @comment = Fabricate :comment, content: 'Comment 1 content', commentable: @upload, user: @user2
-    #   @comment_event = Fabricate :event, trackable: @comment, action: 'received_create', user: @user
-    #   @user.received_stream.keys.first[1].should == "Comment_#{@comment.id}_received_create_Upload_#{@upload.id}"
-    # end
-
-    # it 'returns a like received item when a user receives a like an object' do
-    #   @user2 = Fabricate :user, name: 'Tester 2'
-    #   @like = Fabricate :like, likeable: @upload, user: @user2
-    #   @like_event = Fabricate :event, trackable: @like, action: 'received_create', user: @user
-    #   @user.received_stream.keys.first[1].should == "Like_received_create_Upload_#{@upload.id}"
-    # end
-
-    # Events that should NOT show up in the stream
-
-    # it 'does not return a comment create item when a user comments' do
-    #   @comment = Fabricate :comment, content: 'Comment 1 content', commentable: @upload, user: @user
-    #   @comment_event = Fabricate :event, trackable: @comment, action: 'create', user: @user
-    #   @user.received_stream.keys.length.should == 0
-    # end
-
-    # it 'does not return a course enrolled item when a user enrolls into a course' do
-    #   @course = Fabricate :course, title: 'Test Course', description: 'Course description'
-    #   @course_event = Fabricate :event, trackable: @course, action: 'enroll', user: @user
-    #   @user.received_stream.keys.length.should == 0
-    # end
-
-    # it 'does not return a like create item when a user likes an object' do
-    #   @like = Fabricate :like, likeable: @upload, user: @user
-    #   @like_event = Fabricate :event, trackable: @like, action: 'create', user: @user
-    #   @user.received_stream.keys.length.should == 0
-    # end
-
-    # it 'does not return an upload create item when a user uploads a photo' do
-    #   @upload_event = Fabricate :event, trackable: @upload, action: 'create', user: @user
-    #   @user.received_stream.keys.length.should == 0
-    # end
   end
+
 end
