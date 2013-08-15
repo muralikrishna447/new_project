1b2cdc3eaec7fbe2762b0a87cd43286c23c541d5
hueezer <hueezer@gmail.com>
2013-08-15 12:11:40 -0700

    [polls] add image field

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,7 +2,7 @@ class Poll < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
   
-  attr_accessible :description, :slug, :status, :title, :poll_items_attributes
+  attr_accessible :description, :slug, :status, :title, :image_id, :poll_items_attributes
   has_many :poll_items
 
   accepts_nested_attributes_for :poll_items
