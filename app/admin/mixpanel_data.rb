c1b338b882b5870208957a3353dc5fd2b3efd126
hueezer <hueezer@gmail.com>
2013-09-04 16:15:24 -0700

    [mixpanel] work

--- app/admin/mixpanel_data.rb
+++ app/admin/mixpanel_data.rb
@@ -1,40 +1,50 @@
-ActiveAdmin.register MixpanelData do
-  # require 'digest/md5'
-
-  # expire = (DateTime.now + 5.minutes).utc.to_s(:number)
-  # api_key = 'ccf6e1e8a37992e4119e42a14557b4a2'
-  # api_secret = 'a2f59ee56f798fde108708277bddd4f7'
+ActiveAdmin.register_page 'Mixpanel' do
+  require 'csv'
+  require 'json'
   
-  # parameters = {api_key: api_key, expire: expire}.to_query
-  # sig = Digest::MD5.hexdigest(parameters + api_secret)
+  config = {api_key: 'ccf6e1e8a37992e4119e42a14557b4a2', api_secret: 'a2f59ee56f798fde108708277bddd4f7'}
+  client = Mixpanel::Client.new(config)
 
+  data = client.request('engage', {
 
-  # content do
-  #   link_to expire, "http://mixpanel.com/api/2.0/engage/?expire=#{expire}&api_key=#{api_key}&sig=#{sig}", target: '_blank'
-  # end
+  })
+  puts data['page']
 
+  while data['results'].length >= data['page'] do
+    next_page_number = data['page'] + 1
+    data = client.request('engage', { session_id: data['session_id'], page: next_page_number})
+    puts data['page']
+  end
 
-
-  
-  # csv = CSV.generate {|csv| data.to_a.each {|elem| csv << elem} }
-
-  # content do
-  #   # data
-  #   csv
-  # end
-
-  controller do
-    def index
-      require 'csv'
-      config = {api_key: 'ccf6e1e8a37992e4119e42a14557b4a2', api_secret: 'a2f59ee56f798fde108708277bddd4f7'}
-      client = Mixpanel::Client.new(config)
-
-      data = client.request('engage', {
-
-      })
-      csv = CSV.generate {|csv| data.to_a.each {|elem| csv << elem} }
-      send_data csv
+  page_action :get_csv, method: :get do
+    csv_string = CSV.generate do |csv| 
+      # data.to_a.each {|elem| csv << elem}
+      column_names = [:distinct_id, :browser, :city, :country_code, :created, :email, :first_name, :initial_referer, :initial_referring_domain, :os, :region, :last_seen]
+      csv << column_names
+      data['results'].each do |hash|
+        values = []
+        hash.keys.each do |key|
+          case key
+          when '$distinct_id'
+            values << hash[key]
+          when '$properties'
+            hash['$properties'].values.each do |value|
+              values << value
+            end
+          end
+        end
+        csv << values
+      end
     end
+    send_data csv_string, filename: 'mixpanel_engage.csv'
+  end
+
+  action_item do
+    link_to 'hello', admin_mixpanel_get_csv_path, method: :get
+  end
+
+  content do
+    data
   end
 
 end
