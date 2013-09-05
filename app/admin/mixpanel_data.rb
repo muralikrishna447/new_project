0973e00dccb4737f50f074b5a0ec1188c6d12644
hueezer <hueezer@gmail.com>
2013-09-04 17:56:16 -0700

    [mixpanel] download link

--- app/admin/mixpanel_data.rb
+++ app/admin/mixpanel_data.rb
@@ -5,27 +5,23 @@ ActiveAdmin.register_page 'Mixpanel' do
   config = {api_key: 'ccf6e1e8a37992e4119e42a14557b4a2', api_secret: 'a2f59ee56f798fde108708277bddd4f7'}
   client = Mixpanel::Client.new(config)
 
-  all_data = []
-  data = client.request('engage', {
+  page_action :get_csv, method: :get do
+    all_data = []
+    data = client.request('engage', {
 
-  })
-  data['results'].each do |item|
-    all_data << item
-  end
-
-  while data['results'].length >= data['page'] do
-    next_page_number = data['page'] + 1
-    data = client.request('engage', { session_id: data['session_id'], page: next_page_number})
+    })
     data['results'].each do |item|
       all_data << item
     end
-  end
 
-  page_action :get_csv, method: :get do
+    while data['results'].length >= data['page'] do
+      next_page_number = data['page'] + 1
+      data = client.request('engage', { session_id: data['session_id'], page: next_page_number})
+      data['results'].each do |item|
+        all_data << item
+      end
+    end
     csv_string = CSV.generate do |csv| 
-      # data.to_a.each {|elem| csv << elem}
-      # column_names = [:distinct_id, :browser, :city, :country_code, :created, :email, :first_name, :initial_referer, :initial_referring_domain, :os, :region, :last_seen]
-      # csv << column_names
       all_data.each do |hash|
         keys = []
         values = []
@@ -44,12 +40,8 @@ ActiveAdmin.register_page 'Mixpanel' do
     send_data csv_string, filename: 'mixpanel_engage.csv'
   end
 
-  action_item do
-    link_to 'hello', admin_mixpanel_get_csv_path, method: :get
-  end
-
   content do
-    all_data
+    link_to 'Download Data', admin_mixpanel_get_csv_path, method: :get
   end
 
 end
