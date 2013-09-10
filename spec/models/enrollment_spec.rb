b99709a37c0370d2bcb1e9da40645c94d9578576
hueezer <hueezer@gmail.com>
2013-09-09 18:25:31 -0700

    [course] enrollment specs

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Enrollment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = Fabricate :user, email: 'test@test.com', name: 'Test User'
+    @course = Fabricate :course, title: 'Test Course'
+    @assembly = Fabricate :assembly, title: 'Test Assembly'
+  end
+  
+  it 'should return a course object when a user enrolls into a course' do
+    enrollment = Fabricate :enrollment, user: @user, enrollable: @course
+    expect(enrollment.enrollable).to be_an_instance_of(Course)
+  end
+
+  it 'should return a course object when a user enrolls into a course' do
+    enrollment = Fabricate :enrollment, user: @user, enrollable: @assembly
+    expect(enrollment.enrollable).to be_an_instance_of(Assembly)
+  end
+
+  it 'should not allow a user to enroll into the same course twice' do
+    enrollment1 = Fabricate :enrollment, user: @user, enrollable: @assembly
+    enrollment2 = Fabricate.build(:enrollment, user: @user, enrollable: @assembly)
+    enrollment1.should be_valid
+    enrollment2.should_not be_valid
+  end
 end
