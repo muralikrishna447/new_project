b80ed3021fa96b9666ed0a6a247518dd3288bb0d
hueezer <hueezer@gmail.com>
2013-09-13 01:39:57 -0700

    [paid courses] redirect to landing

--- app/controllers/assemblies_controller.rb
+++ app/controllers/assemblies_controller.rb
@@ -13,10 +13,14 @@ class AssembliesController < ApplicationController
     assembly = Assembly.find_published(params[:id], params[:token], can?(:update, @activity))
     instance_variable_set("@#{assembly.assembly_type.underscore}", assembly)
     @upload = Upload.new
-    render "#{assembly.assembly_type.underscore.pluralize}_#{params[:action]}"
+    if current_user && current_user.enrolled?(assembly)
+      render "#{assembly.assembly_type.underscore.pluralize}_#{params[:action]}"
+    else
+      redirect_to landing_course_url(assembly)
+    end
   end
 
   def landing
-    @assembly = Assembly.find(params[:id])
+    @course = Assembly.find(params[:id])
   end
 end
