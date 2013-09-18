45cd50b0e5cfe0681332c31d0d7c8b4ec719b870
hueezer <hueezer@gmail.com>
2013-09-18 00:24:02 -0700

    [paid-course] faq tests

--- spec/features/courses_spec.rb
+++ spec/features/courses_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 feature 'courses' do
   before :each do
-    @course = Fabricate :course, title: 'Test Course', description: 'Test course description'
+    @course = Fabricate :assembly, title: 'Test Course', description: 'Test course description', published: true, assembly_type: 'Course'
   end
 
   context 'landing page' do
-    it 'displays an faq' do
+    it 'displays an faq section when an faq is present' do
+      faq = Fabricate :activity, title: 'Test Course Faq', description: 'Test course faq description'
+      visit landing_course_path @course
+      expect(page).to have_content('FAQ')
+    end
+
+    it 'does not display an faq when an faq is not present' do
+      visit landing_course_path @course
+      expect(page).to_not have_content('FAQ')
     end
   end
 end
