5b6a1f8747a8f3c6327603f92a52465a696629f8
hueezer <hueezer@gmail.com>
2013-09-25 17:03:13 -0700

    [course uplaods] specs

---
+++ spec/features/uploads_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'uploads' do
+  include AcceptanceMacros
+  before :each do
+    @assembly = Fabricate :assembly, title: 'Test Course', description: 'Test course description', published: true, assembly_type: 'Course'
+  end
+
+  context 'courses' do
+    describe 'upload page' do
+      it 'displays a form' do
+        login_user
+        visit course_path(@assembly)
+        expect(page.body).to have_content('Test Course')
+      end
+    end
+  end
+end
