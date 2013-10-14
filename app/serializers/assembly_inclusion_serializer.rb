78e22860ab82b48be35457c0d382f4eb39d76d58
Michael Natkin <michaelnatkin@gmail.com>
2013-10-14 15:16:21 -0700

    fix normal activity links to keep you in the course when possible

--- app/serializers/assembly_inclusion_serializer.rb
+++ app/serializers/assembly_inclusion_serializer.rb
@@ -1,9 +1,13 @@
 class AssemblyInclusionSerializer < ActiveModel::Serializer
-  attributes :includable_type, :includable_id, :position, :includable_title, :include_disqus
+  attributes :includable_type, :includable_id, :position, :includable_title, :include_disqus, :includable_slug
   has_one :includable
 
   def includable_title
     object.includable.title
+  end
+
+  def includable_slug
+    object.includable.slug
   end
 
   # This picks up the recursive tree of associations
