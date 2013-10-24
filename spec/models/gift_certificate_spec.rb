47eb9cc37bad12cd1bf32603b6b9fdfc23807bc6
Michael Natkin <michaelnatkin@gmail.com>
2013-10-24 16:09:59 -0700

    checkpoint

--- spec/models/gift_certificate_spec.rb
+++ spec/models/gift_certificate_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe GiftCertificate do
 
-  describe "creates a random token of at least 6 letters" do
+  describe "creates a random token of at least 6 characters" do
     gc = GiftCertificate.new
     gc.token.length.should >= 6
   end
