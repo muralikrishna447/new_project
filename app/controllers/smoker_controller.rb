ce2aa434d4cc4cbe5ef12bd06228795fb5441f0d
Michael Natkin <michaelnatkin@gmail.com>
2013-11-01 18:02:17 -0700

    remove dangle

--- app/controllers/smoker_controller.rb
+++ app/controllers/smoker_controller.rb
@@ -1,4 +1,9 @@
 class SmokerController < ApplicationController
+  before_filter :only_admin
+  def only_admin 
+    redirect_to "/" unless (current_user && current_user.admin?)
+  end
+
   def index
   end
 end
