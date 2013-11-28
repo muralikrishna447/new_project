443e947973d50f2722fc38268c2fdff509e5d04c
Michael Natkin <michaelnatkin@gmail.com>
2013-11-27 21:40:58 -0800

    farg

---
+++ app/serializers/ingredient_serializer.rb
@@ -0,0 +1,11 @@
+class IngredientSerializer < ActiveModel::Serializer
+  attributes :id, :title, :slug, :product_url, :created_at, :updated_at, :for_sale, :sub_activity_id, :density, :image_id
+  attributes :frequently_used_with
+  has_many :activities
+
+  def frequently_used_with
+
+    ActiveRecord::Base.connection.execute("select count(*), i.id, i.title, i.slug from activity_ingredients i_to_a join activity_ingredients a_to_i on a_to_i.activity_id = i_to_a.activity_id join ingredients i on i.id = a_to_i.ingredient_id where i_to_a.ingredient_id = #{object.id} group by i.id order by count DESC limit 5;")
+
+  end
+end
