f5b5d3f0ed83eed591c81efe568a1385b06546a8
Dan Ahern <danahern@gmail.com>
2013-12-11 15:55:55 -0800

    don't make the request without the cookies

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -1,11 +1,13 @@
 class AffiliatesController < ApplicationController
   def share_a_sale
-    amount = params[:amount]
-    tracking = params[:tracking]
-    uri = URI('https://www.shareasale.com/q.cfm')
-    uri.query = URI.encode_www_form({amount: amount, tracking: tracking, transtype: "SALE", merchantID: "51074", userID: cookies["SSAID"], ssaiddata: cookies["SSAIDDATA"]})
-    if Rails.env.production?
-      Net::HTTP.get_response(uri)
+    if cookies['SSAID'].present? && cookies['SSAIDDATA'].present?
+      amount = params[:amount]
+      tracking = params[:tracking]
+      uri = URI('https://www.shareasale.com/q.cfm')
+      uri.query = URI.encode_www_form({amount: amount, tracking: tracking, transtype: "SALE", merchantID: "51074", userID: cookies["SSAID"], ssaiddata: cookies["SSAIDDATA"]})
+      if Rails.env.production?
+        Net::HTTP.get_response(uri)
+      end
     end
     render nothing: true, status: 200
   end
