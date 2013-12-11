4d8f46468f83fd5542f23e9c871fceac9268ed1c
Dan Ahern <danahern@gmail.com>
2013-12-11 15:29:39 -0800

    Apparently they want the request over https

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -2,7 +2,7 @@ class AffiliatesController < ApplicationController
   def share_a_sale
     amount = params[:amount]
     tracking = params[:tracking]
-    uri = URI('http://www.shareasale.com/q.cfm')
+    uri = URI('https://www.shareasale.com/q.cfm')
     uri.query = URI.encode_www_form({amount: amount, tracking: tracking, transtype: "SALE", merchantID: "51074", userID: cookies["SSAID"], ssaiddata: cookies["SSAIDDATA"]})
     if Rails.env.production?
       Net::HTTP.get_response(uri)
