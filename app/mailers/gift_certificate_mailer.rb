a869abad9dfe5f33f24536ef4e9fbe0c738c08d9
Dan Ahern <danahern@gmail.com>
2013-12-27 10:31:10 -0800

    A lot more testing on purchase process

--- app/mailers/gift_certificate_mailer.rb
+++ app/mailers/gift_certificate_mailer.rb
@@ -2,11 +2,11 @@ class GiftCertificateMailer < ActionMailer::Base
   default from: "info@chefsteps.com"
 
   def recipient_email(to_recipient, purchaser, title, redeem_url, recipient_email, recipient_name, recipient_message)
-    puts "****************** MAILER"
-    puts to_recipient
+    # puts "****************** MAILER"
+    # puts to_recipient
     @gift_title = title
-    @redeem_url = redeem_url 
-    @recipient_name = recipient_name 
+    @redeem_url = redeem_url
+    @recipient_name = recipient_name
     @recipient_message = recipient_message
     @purchaser_name = purchaser.email
     @purchaser_name = "#{purchaser.name} (#{purchaser.email})" if purchaser.name
