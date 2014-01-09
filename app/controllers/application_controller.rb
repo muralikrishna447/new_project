b3e08c5eaab03c6e48476175f7413df4c57034cd
Dan Ahern <danahern@gmail.com>
2014-01-09 15:36:25 -0800

    CORs = PITA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,10 @@ class ApplicationController < ActionController::Base
     render partial: 'layouts/header', :locals => { :external => true }
   end
 
+  def options
+    render :text => '', :content_type => 'text/plain'
+  end
+
   # expose devise helper method to views
   helper_method :after_sign_in_path_for
 
@@ -143,30 +147,26 @@ private
     end
   end
 
-  # before_filter :cors_preflight_check
-  # after_filter :cors_set_access_control_headers
+  # http://nils-blum-oeste.net/cors-api-with-oauth2-authentication-using-rails-and-angularjs/
+  # do not use CSRF for CORS options
+  skip_before_filter :verify_authenticity_token, :only => [:options]
 
-  # # For all responses in this controller, return the CORS access control headers.
+  before_filter :cors_set_access_control_headers
+  before_filter :authenticate_cors_user
 
-  # def cors_set_access_control_headers
-  #   headers['Access-Control-Allow-Origin'] = '*'
-  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  #   headers['Access-Control-Max-Age'] = "1728000"
-  # end
+  def authenticate_cors_user
+    if request.xhr? && !user_signed_in?
+      error = { :error => "You must be logged in." }
+      render :json => error, :status => 401
+    end
+  end
 
-  # # If this is a preflight OPTIONS request, then short-circuit the
-  # # request, return only the necessary headers and return an empty
-  # # text/plain.
-
-  # def cors_preflight_check
-  #   if request.method == :options
-  #     headers['Access-Control-Allow-Origin'] = '*'
-  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  #     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-  #     headers['Access-Control-Max-Age'] = '1728000'
-  #     render :text => '', :content_type => 'text/plain'
-  #   end
-  # end
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
 
   protected
 
