804a0b1b4ef2bcf8ca3834e7e6dd70df1f85ffef
hueezer <hueezer@gmail.com>
2014-01-24 13:40:18 -0800

    remove text field survey results

--- db/migrate/20140124191615_add_survey_results_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSurveyResultsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :survey_results, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140124191615) do
+ActiveRecord::Schema.define(:version => 20140120193602) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -612,7 +612,8 @@ ActiveRecord::Schema.define(:version => 20140124191615) do
     t.string   "google_refresh_token"
     t.string   "google_access_token"
     t.string   "google_user_id"
-    t.text     "survey_results"
+    t.integer  "referrer_id"
+    t.string   "referred_from"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
