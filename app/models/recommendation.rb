dd39555ebdd9ebc69f4286653b188b537fa6316a
hueezer <hueezer@gmail.com>
2014-01-28 12:06:26 -0800

    [recommendation] start to map survey to recommendations

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,17 @@
 class Recommendation
   def self.activities_for(user)
-    Activity.chefsteps_generated.published
+    cook_type = user.survey_results['What kind of cook are you?']
+    case cook_type
+    when 'Amateur'
+      activities_by_cook_type = self.activities_by_difficulty('easy')
+    when 'Home Cook'
+      activities_by_cook_type = self.activities_by_difficulty('intermediate')
+    when 'Culinary Student'
+      activities_by_cook_type = self.activities_by_difficulty('intermediate')
+    when 'Professional'
+      activities_by_cook_type = self.activities_by_difficulty('advanced')
+    end
+    activities_by_cook_type
   end
 
   # Accepts a single tag or an array of tags
@@ -8,5 +19,9 @@ class Recommendation
   # Array: Recommendation.activities_by_tag(['Modernist','Butchery'])
   def self.activities_by_tags(tags)
     Activity.chefsteps_generated.published.tagged_with(tags, :any => true)
+  end
+
+  def self.activities_by_difficulty(difficulty)
+    Activity.chefsteps_generated.published.difficulty(difficulty)
   end
 end
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Recommendation do
 
   before :each do
-    @user1 = Fabricate :user, name: 'Bob Smith'
-    @activity1 = Fabricate :activity, title: 'Activity 1', published: true, tag_list: 'modernist, dinner'
-    @activity2 = Fabricate :activity, title: 'Activity 2', published: true, tag_list: 'butchery'
-    @activity3 = Fabricate :activity, title: 'Activity 3', published: true, tag_list: 'dinner'
+    @user1 = Fabricate :user, name: 'Bob Smith', survey_results: {"What kind of cook are you?"=>"Home Cook", "Tell us more about yourself:"=>"I am awesome!", "Which culinary topics interest you the most?"=>"Modernist Cuisine", "Dietary Restrictions. Select all that apply to you:"=>"Vegetarian"}
+    @activity1 = Fabricate :activity, title: 'Activity 1', published: true, tag_list: 'modernist, dinner', difficulty: 'easy'
+    @activity2 = Fabricate :activity, title: 'Activity 2', published: true, tag_list: 'butchery', difficulty: 'advanced'
+    @activity3 = Fabricate :activity, title: 'Activity 3', published: true, tag_list: 'dinner', difficulty: 'intermediate'
   end
 
   context 'activities_for' do
-    it 'returns an array of activities' do
-
+    it 'returns easy recipes when a user is a home cook' do
+      expect(Recommendation.activities_for(@user1)).to include(@activity3)
     end
   end
 
@@ -25,6 +25,13 @@ describe Recommendation do
       expect(Recommendation.activities_by_tags(['Modernist', 'Dinner'])).to include(@activity1)
       expect(Recommendation.activities_by_tags(['Modernist', 'Dinner'])).to include(@activity3)
       expect(Recommendation.activities_by_tags(['Modernist', 'Dinner'])).to_not include(@activity2)
+    end
+  end
+
+  context 'activities_by_difficulty' do
+    it 'returns easy activities' do
+      expect(Recommendation.activities_by_difficulty('easy')).to include(@activity1)
+      expect(Recommendation.activities_by_difficulty('easy')).to_not include(@activity2)
     end
   end
 end
