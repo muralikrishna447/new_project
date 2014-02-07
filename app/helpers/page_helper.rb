d084fc9052942cf199b513f0c38e8cc26b64742f
Dan Ahern <danahern@gmail.com>
2014-02-06 23:16:52 -0800

    Small changes to make macarons work

---
+++ app/helpers/page_helper.rb
@@ -0,0 +1,10 @@
+module PageHelper
+  def landing_page_content(course)
+    case course.slug
+    when "whipping-siphons"
+      Page.where(slug: "whipping-siphons-free-trial").first.content.html_safe
+    when "french-macarons"
+      Page.where(slug: "french-macarons-free-trial").first.content.html_safe
+    end
+  end
+end
