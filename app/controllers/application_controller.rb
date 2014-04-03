bf4d243d82800b392b8e25d1e782ca69e00a1ae2
Tim Salazar <tim.salazar@gmail.com>
2014-04-03 15:00:34 -0700

    [404] fix error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -233,7 +233,6 @@ private
   
   # if Rails.env.production?
    # unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: :render_500
     rescue_from ActionController::RoutingError, with: :render_404
     rescue_from ActionController::UnknownController, with: :render_404
     rescue_from ActionController::UnknownAction, with: :render_404
@@ -249,14 +248,7 @@ private
       format.all { render nothing: true, status: 404 }
     end
   end
- 
-  def render_500(exception)
-    logger.info exception.backtrace.join("\n")
-    respond_to do |format|
-      format.html { render template: 'errors/internal_server_error', layout: 'layouts/application', status: 500 }
-      format.all { render nothing: true, status: 500}
-    end
-  end
+
 
 
 
