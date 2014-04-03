29b9d731e2aaed3a5d4d5855ccd66f0e9468aca0
Tim Salazar <tim.salazar@gmail.com>
2014-04-03 11:47:38 -0700

    [404] checkpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
   # For dynamically setting the host to whatever it needs to be for the environment we're testing.
   before_filter :set_mailer_host
   def set_mailer_host
@@ -229,6 +230,35 @@ private
   def verified_request?
     super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
   end
+  
+  if Rails.env.development?
+    unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: :render_500
+      rescue_from ActionController::RoutingError, with: :render_404
+      rescue_from ActionController::UnknownController, with: :render_404
+      rescue_from ActionController::UnknownAction, with: :render_404
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    end
+  end
+
+
+  def render_404(exception)
+    @not_found_path = exception.message
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+ 
+  def render_500(exception)
+    logger.info exception.backtrace.join("\n")
+    respond_to do |format|
+      format.html { render template: 'errors/internal_server_error', layout: 'layouts/application', status: 500 }
+      format.all { render nothing: true, status: 500}
+    end
+  end
+
+
 
 end
 
