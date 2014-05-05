d70f71ecab79134a1386d8c9540552f6129db806
hueezer <hueezer@gmail.com>
2014-05-05 14:31:44 -0700

    [bloom] add rescue

--- app/models/user.rb
+++ app/models/user.rb
@@ -182,12 +182,13 @@ class User < ActiveRecord::Base
   end
 
   def encrypted_bloom_info
-    # user_json = self.to_json(only: [:id, :name], methods: :avatar_url)
     user_json = {'userId' => self.id.to_s}.to_json
-    # encrypted = ChefstepsBloom.encrypt(user_json)
-    # encrypted
-    response = Faraday.get 'http://api.usebloom.com/encrypt?string=' + user_json + '&apiKey=xchefsteps'
-    response.body
+    begin
+      response = Faraday.get 'http://api.usebloom.com/encrypt?string=' + user_json + '&apiKey=xchefsteps'
+      response.body
+    rescue SocketError => e
+      logger.warn "Unable to encrypt info for Bloom: #{e}"
+    end
   end
 end
 
