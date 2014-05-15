8a8ec4caec1cd130876ba6f1e91c163f443ee54c
hueezer <hueezer@gmail.com>
2014-05-14 18:05:01 -0700

    [cors] authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -210,7 +210,7 @@ private
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
+    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type, Authorization'
     headers['Access-Control-Max-Age'] = "1728000"
   end
 
