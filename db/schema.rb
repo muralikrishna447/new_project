a44c5c177fa9d74465f5e1b50e4f7a1f2856b894
Tim Salazar <tim.salazar@gmail.com>
2014-05-27 08:25:37 -0700

    [nav] checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -402,6 +402,17 @@ ActiveRecord::Schema.define(:version => 20140505192216) do
     t.string   "primary_path"
   end
 
+  create_table "pending_edits", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "serialized_content"
+    t.integer  "editable_id"
+    t.string   "editable_type"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  add_index "pending_edits", ["editable_id", "editable_type"], :name => "index_pending_edits_on_editable_id_and_editable_type"
+
   create_table "pg_search_documents", :force => true do |t|
     t.text     "content"
     t.integer  "searchable_id"
