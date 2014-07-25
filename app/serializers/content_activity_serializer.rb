b0bab047c2f03d15ab0ecd62650bce3146ab8af6
Huy Nguyen <hueezer@gmail.com>
2014-07-25 13:18:56 -0700

    [comments] add context for dashboard

--- app/serializers/content_activity_serializer.rb
+++ app/serializers/content_activity_serializer.rb
@@ -1,5 +1,5 @@
 class ContentActivitySerializer < ActiveModel::Serializer
-  attributes :id, :title, :image, :description, :url
+  attributes :id, :title, :image, :description, :url, :context
 
   def image
     filepicker_arbitrary_image(object.featured_image, 400)
@@ -7,5 +7,17 @@ class ContentActivitySerializer < ActiveModel::Serializer
 
   def url
     activity_url(object)
+  end
+
+  def context
+    assembly = object.containing_course
+    if assembly
+      if assembly.assembly_type == 'Course'
+        type_name = 'Class'
+      else
+        type_name = assembly.assembly_type
+      end
+      "#{assembly.title} #{type_name}" 
+    end
   end
 end
