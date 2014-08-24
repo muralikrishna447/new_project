4efee592f5c016bbc81273fa30d3928b3ad644a9
Huy Nguyen <hueezer@gmail.com>
2014-08-24 16:55:03 -0700

    [api] activities

--- app/serializers/api/activity_serializer.rb
+++ app/serializers/api/activity_serializer.rb
@@ -1,3 +1,11 @@
 class Api::ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :image, :url, :likes_count
+
+  def image
+    object.featured_image_id
+  end
+
+  def url
+    activity_url(object)
+  end
 end
