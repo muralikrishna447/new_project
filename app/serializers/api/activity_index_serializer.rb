7458d63741f26ccbd70f65f2cee4ca88ce0d9b7f
Huy Nguyen <hueezer@gmail.com>
2014-08-27 00:49:08 -0700

    [api] activities

--- app/serializers/api/activity_index_serializer.rb
+++ app/serializers/api/activity_index_serializer.rb
@@ -1,5 +1,5 @@
 class Api::ActivityIndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :image, :url, :likes_count
+  attributes :id, :title, :description, :image, :url, :likesCount
 
   def image
     filepicker_to_s3_url(object.featured_image_id)
@@ -7,5 +7,9 @@ class Api::ActivityIndexSerializer < ActiveModel::Serializer
 
   def url
     activity_url(object)
+  end
+
+  def likesCount
+    object.likes_count
   end
 end
---
+++ spec/controllers/api/v0/activities_controller_spec.rb
@@ -0,0 +1,38 @@
+describe Api::V0::ActivitiesController do
+  
+  before :each do
+    @activity = Fabricate :activity, title: 'Activity'
+  end
+
+  # GET /api/v0/activities
+  it 'should return an array of activities' do
+    get :index
+    response.should be_success
+
+    activity = JSON.parse(response.body).first
+
+    activity['title'].should == 'Activity'
+    activity['description'].should == nil
+    activity['image'].should == nil
+    activity['url'].should == 'http://test.host/activities/activity'
+    activity['likesCount'].should == nil
+  end
+
+  # GET /api/v0/activities/:id
+  it 'should return a single activity' do
+    get :index, id: @activity.id
+    response.should be_success
+
+    activity = JSON.parse(response.body).first
+
+    activity['title'].should == 'Activity'
+    activity['description'].should == nil
+    activity['image'].should == nil
+    activity['url'].should == 'http://test.host/activities/activity'
+    activity['likesCount'].should == nil
+
+    activity['ingredients'].should == nil
+    activity['steps'].should == nil
+    activity['equipment'].should == nil
+  end
+end
