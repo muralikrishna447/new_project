2898f6a29d07da57713a1390841f53204e5587d6
Huy Nguyen <hueezer@gmail.com>
2014-08-29 18:03:14 -0700

    [api] add more activity scopes

--- app/controllers/api/v0/activities_controller.rb
+++ app/controllers/api/v0/activities_controller.rb
@@ -1,6 +1,21 @@
 module Api
   module V0
     class ActivitiesController < ApplicationController
+      
+      has_scope :sort do |controller, scope, value|
+        case value
+          when "oldest"
+            scope.by_published_at("asc")
+          when "newest"
+            scope.by_published_at("desc")
+          when "popular"
+            scope.popular
+          else
+            # Relevance is the default sort for pg_search so don't need to do anything
+            scope
+        end
+      end
+
       has_scope :published, default: true,  type: :boolean
 
       def index
