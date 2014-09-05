8cccf03f4a996eaa9acd9830405d03fca1326543
Huy Nguyen <hueezer@gmail.com>
2014-09-05 17:24:26 +0100

    [projects] newest first

--- app/controllers/assemblies_controller.rb
+++ app/controllers/assemblies_controller.rb
@@ -6,10 +6,10 @@ class AssembliesController < ApplicationController
   def index
     if request.path == '/assemblies'
       @assembly_type = 'Assembly'
-      @assemblies = Assembly.published.order('created_at asc').page(params[:page]).per(12)
+      @assemblies = Assembly.published.order('created_at desc').page(params[:page]).per(12)
     else
       @assembly_type = request.path.gsub(/^\//, "").singularize.titleize
-      @assemblies = Assembly.published.where(assembly_type: @assembly_type).order('created_at asc').page(params[:page]).per(12)
+      @assemblies = Assembly.published.where(assembly_type: @assembly_type).order('created_at desc').page(params[:page]).per(12)
     end
   end
 
