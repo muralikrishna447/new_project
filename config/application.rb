32647e0ed716596aaa02ef1d1f0139c1face128a
Alex Thomson <alex@chefsteps.com>
2014-10-27 08:24:07 -0700

    Prefixing log lines with request UUID

--- config/application.rb
+++ config/application.rb
@@ -113,5 +113,7 @@ module Delve
     # Make sure there is no ssl within the class itself
     # config.middleware.insert_before ActionDispatch::Static, Rack::SslEnforcer, except: [%r{^(?!.*landing).*classes.*$|.*projects.*$}], only_environments: ['production', 'staging'], force_secure_cookies: false
     # config.middleware.insert_before ActionDispatch::Static, Rack::SslEnforcer, except: [%r{.*/activities.*}], only_environments: ['production', 'staging'], force_secure_cookies: false, strict: true
+
+    config.log_tags = [:uuid ]
   end
 end
