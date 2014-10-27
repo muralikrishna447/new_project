18fa9f8ab205a36ef47b9bbc500ec4fa803b948f
Alex Thomson <alex@chefsteps.com>
2014-10-27 08:26:02 -0700

    Adding config comment for log_tags

--- config/application.rb
+++ config/application.rb
@@ -114,6 +114,7 @@ module Delve
     # config.middleware.insert_before ActionDispatch::Static, Rack::SslEnforcer, except: [%r{^(?!.*landing).*classes.*$|.*projects.*$}], only_environments: ['production', 'staging'], force_secure_cookies: false
     # config.middleware.insert_before ActionDispatch::Static, Rack::SslEnforcer, except: [%r{.*/activities.*}], only_environments: ['production', 'staging'], force_secure_cookies: false, strict: true
 
+    # Prefix each log line with a per-request UUID
     config.log_tags = [:uuid ]
   end
 end
