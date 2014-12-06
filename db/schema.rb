39e07fbd9408a42005a32fc3485d9903a59365d2
Huy Nguyen <hueezer@gmail.com>
2014-12-06 13:44:35 -0800

    [gifts] passing tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141206210108) do
+ActiveRecord::Schema.define(:version => 20140912175712) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -74,8 +74,7 @@ ActiveRecord::Schema.define(:version => 20141206210108) do
   add_index "activity_equipment", ["activity_id", "equipment_id"], :name => "activity_equipment_index", :unique => true
   add_index "activity_equipment", ["equipment_order"], :name => "index_activity_equipment_on_equipment_order"
 
-  create_table "activity_ingredients", :id => false, :force => true do |t|
-    t.integer  "id",               :null => false
+  create_table "activity_ingredients", :force => true do |t|
     t.integer  "activity_id",      :null => false
     t.integer  "ingredient_id",    :null => false
     t.datetime "created_at",       :null => false
@@ -293,17 +292,17 @@ ActiveRecord::Schema.define(:version => 20141206210108) do
 
   create_table "gift_certificates", :force => true do |t|
     t.integer  "purchaser_id"
-    t.string   "recipient_email",                                  :default => "",    :null => false
-    t.string   "recipient_name",                                   :default => "",    :null => false
-    t.text     "recipient_message",                                :default => ""
+    t.string   "recipient_email",                                 :default => "",    :null => false
+    t.string   "recipient_name",                                  :default => "",    :null => false
+    t.text     "recipient_message",                               :default => ""
     t.integer  "assembly_id"
-    t.decimal  "price",              :precision => 8, :scale => 2, :default => 0.0
-    t.decimal  "sales_tax",          :precision => 8, :scale => 2, :default => 0.0
+    t.decimal  "price",             :precision => 8, :scale => 2, :default => 0.0
+    t.decimal  "sales_tax",         :precision => 8, :scale => 2, :default => 0.0
     t.string   "token"
-    t.boolean  "redeemed",                                         :default => false
-    t.datetime "created_at",                                                          :null => false
-    t.datetime "updated_at",                                                          :null => false
-    t.boolean  "followed_up",                                      :default => false
+    t.boolean  "redeemed",                                        :default => false
+    t.datetime "created_at",                                                         :null => false
+    t.datetime "updated_at",                                                         :null => false
+    t.boolean  "followed_up",                                     :default => false
     t.boolean  "email_to_recipient"
   end
 
@@ -404,6 +403,17 @@ ActiveRecord::Schema.define(:version => 20141206210108) do
     t.string   "primary_path"
   end
 
+  create_table "pending_edits", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "serialized_content"
+    t.integer  "editable_id"
+    t.string   "editable_type"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+  add_index "pending_edits", ["editable_id", "editable_type"], :name => "index_pending_edits_on_editable_id_and_editable_type"
+
   create_table "pg_search_documents", :force => true do |t|
     t.text     "content"
     t.integer  "searchable_id"
@@ -477,31 +487,6 @@ ActiveRecord::Schema.define(:version => 20141206210108) do
 
   add_index "quizzes", ["activity_id"], :name => "index_quizzes_on_activity_id"
   add_index "quizzes", ["slug"], :name => "index_quizzes_on_slug", :unique => true
-
-  create_table "recipe_ingredients", :force => true do |t|
-    t.integer  "recipe_id",        :null => false
-    t.integer  "ingredient_id",    :null => false
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-    t.string   "unit"
-    t.decimal  "quantity"
-    t.integer  "ingredient_order"
-  end
-
-  add_index "recipe_ingredients", ["ingredient_order"], :name => "index_recipe_ingredients_on_ingredient_order"
-  add_index "recipe_ingredients", ["recipe_id", "ingredient_id"], :name => "index_recipe_ingredients_on_recipe_id_and_ingredient_id", :unique => true
-
-  create_table "recipes", :force => true do |t|
-    t.string   "title"
-    t.integer  "activity_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "yield"
-    t.integer  "recipe_order"
-  end
-
-  add_index "recipes", ["activity_id"], :name => "index_recipes_on_activity_id"
-  add_index "recipes", ["recipe_order"], :name => "index_recipes_on_recipe_order"
 
   create_table "revision_records", :force => true do |t|
     t.string   "revisionable_type", :limit => 100,                    :null => false
