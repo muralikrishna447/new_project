c4ac55b4467a79a29414b713208756b0175df91b
Huy Nguyen <hueezer@gmail.com>
2014-12-06 14:00:12 -0800

    gifts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140912175713) do
+ActiveRecord::Schema.define(:version => 20141206210108) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
