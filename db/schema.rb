1403f72f0bfa9b3054afd2fcf18d000e5b4f9e6a
Dan Ahern <danahern@gmail.com>
2015-02-05 10:43:29 -0800

    Add resque command to the procfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150204222833) do
+ActiveRecord::Schema.define(:version => 20141223074816) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -153,7 +153,7 @@ ActiveRecord::Schema.define(:version => 20150204222833) do
     t.integer  "position"
     t.datetime "created_at",                        :null => false
     t.datetime "updated_at",                        :null => false
-    t.boolean  "include_disqus",  :default => true
+    t.boolean  "include_disqus",  :default => false
   end
 
   add_index "assembly_inclusions", ["assembly_id"], :name => "index_assembly_inclusions_on_assembly_id"
