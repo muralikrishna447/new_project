e2e1d11986c10595339cab075bc6dec14183141e
Huy Nguyen <hueezer@gmail.com>
2015-02-11 02:32:07 -0800

    [token] debug

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -14,17 +14,12 @@ module Api
       end
      
       def cors_preflight_check
-        puts "CORS PREFLIGHT CHECK BFORE"
+        puts "CORS PREFLIGHT CHECK BEFORE"
 
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
         headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
         headers['Access-Control-Max-Age'] = '1728000'
-      end
-
-      def options
-        puts "OPTIONS CALLED"
-        render :text => '', :content_type => 'text/plain'
       end
       
       protected
