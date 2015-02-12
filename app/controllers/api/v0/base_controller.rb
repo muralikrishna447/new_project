c6eafecabdb6c324dd9222467943fdba0a5ecbe9
Huy Nguyen <hueezer@gmail.com>
2015-02-12 01:05:22 -0800

    [token] update cors

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -15,11 +15,13 @@ module Api
      
       def cors_preflight_check
         puts "CORS PREFLIGHT CHECK BEFORE"
-
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-        headers['Access-Control-Max-Age'] = '1728000'
+        if request.method == 'OPTIONS'
+          headers['Access-Control-Allow-Origin'] = '*'
+          headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+          headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+          headers['Access-Control-Max-Age'] = '1728000'
+          render :text => '', :content_type => 'text/plain'
+        end
       end
 
       def options
