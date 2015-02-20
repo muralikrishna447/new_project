eb8ad15b23d01d234a6394b7314cc02f6599b447
Huy Nguyen <hueezer@gmail.com>
2015-02-20 00:18:26 -0800

    [token] simulate production secret key env

--- config/application.rb
+++ config/application.rb
@@ -117,8 +117,8 @@ module Delve
     # Prefix each log line with a per-request UUID
     config.log_tags = [:uuid ]
 
-    unless Rails.env.production?
-      ENV["AUTH_SECRET_KEY"] = File.read("config/rsa_test.pem")
-    end
+    # unless Rails.env.production?
+    #   ENV["AUTH_SECRET_KEY"] = File.read("config/rsa_test.pem")
+    # end
   end
 end
