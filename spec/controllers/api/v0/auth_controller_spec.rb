e70664f70e8bc7f53695836fac7bbe43daaa14b3
Huy Nguyen <hueezer@gmail.com>
2015-02-26 17:04:32 -0800

    [token] add validate token endpoint

--- spec/controllers/api/v0/auth_controller_spec.rb
+++ spec/controllers/api/v0/auth_controller_spec.rb
@@ -2,6 +2,7 @@ describe Api::V0::AuthController do
 
   before :each do
     @user = Fabricate :user, email: 'johndoe@chefsteps.com', password: '123456', name: 'John Doe'
+    @key = OpenSSL::PKey::RSA.new ENV["AUTH_SECRET_KEY"], 'cooksmarter'
   end
 
   context 'POST /authenticate' do
@@ -25,7 +26,6 @@ describe Api::V0::AuthController do
         response.should be_success
         response.code.should eq("200")
         @token = JSON.parse(response.body)['token']
-        @key = OpenSSL::PKey::RSA.new ENV["AUTH_SECRET_KEY"], 'cooksmarter'
         
       end
 
@@ -47,6 +47,37 @@ describe Api::V0::AuthController do
 
     end
 
+  end
+
+  context 'POST /validate' do
+    before :each do
+      @user = Fabricate :user, id: 200, email: 'user@chefsteps.com', password: '123456', name: 'A User', role: 'user'
+      issued_at = (Time.now.to_f * 1000).to_i
+      claim = { 
+        iat: issued_at,
+        user: @user
+      }
+      jws = JSON::JWT.new(claim.as_json).sign(@key.to_s)
+      jwe = jws.encrypt(@key.public_key)
+      @valid_token = 'Bearer ' + jwe.to_s
+      @invalid_token = 'Bearer Some Bad Token'
+    end
+
+    it 'should validate a valid token' do
+      request.env['HTTP_AUTHORIZATION'] = @valid_token
+      post :validate
+      response.should be_success
+      puts response.body
+      expect(JSON.parse(response.body)['tokenValid']).to be_true
+    end
+
+    it 'should not validate an invalid token' do
+      request.env['HTTP_AUTHORIZATION'] = @invalid_token
+      post :validate
+      response.should_not be_success
+      puts response.body
+      expect(JSON.parse(response.body)['tokenValid']).to be_false
+    end
   end
 
 end
