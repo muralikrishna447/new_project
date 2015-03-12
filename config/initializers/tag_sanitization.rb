857f75fe26c74b5ffe4f202f35a6a7e402fc649e
Dan Ahern <danahern@gmail.com>
2015-03-12 13:37:22 -0700

    fix for the tag sanitization

--- config/initializers/tag_sanitization.rb
+++ config/initializers/tag_sanitization.rb
@@ -1,7 +1,7 @@
 class TagSanitizationParser < ActsAsTaggableOn::GenericParser
   def parse
     ActsAsTaggableOn::TagList.new.tap do |tag_list|
-      tag_list.add @tag_list.split(',')
+      tag_list.add @tag_list.map{|s| Sanitize.fragment(s, Sanitize::Config.merge(Sanitize::Config::RELAXED, remove_contents: true)) }
     end
   end
 end
