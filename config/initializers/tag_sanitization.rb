d85e3649d75f6d89e726ea30db4f53cb2b467a4b
Dan Ahern <danahern@gmail.com>
2015-03-12 12:26:57 -0700

    Fix for tagging

--- config/initializers/tag_sanitization.rb
+++ config/initializers/tag_sanitization.rb
@@ -1,7 +1,7 @@
 class TagSanitizationParser < ActsAsTaggableOn::GenericParser
   def parse
     ActsAsTaggableOn::TagList.new.tap do |tag_list|
-      tag_list.add Sanitize.fragment(@tag_list).split(',')
+      tag_list.add @tag_list.split(',')
     end
   end
 end
