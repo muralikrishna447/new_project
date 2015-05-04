08f42c3f40988e2d26424f0ebcbb3072a1c9bfae
Huy Nguyen <hueezer@gmail.com>
2015-05-04 14:13:45 -0700

    [homepage] update components migration

--- db/migrate/20150427031541_create_components.rb
+++ db/migrate/20150427031541_create_components.rb
@@ -1,6 +1,8 @@
 class CreateComponents < ActiveRecord::Migration
   def change
     create_table :components do |t|
+      t.string :name
+      t.string :slug
       t.string :component_type
       t.string :mode
       t.column :metadata, :hstore
