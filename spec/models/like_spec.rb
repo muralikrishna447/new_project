b3c05a0c64769793115d4b4c14129d15828b1083
Michael Natkin <michaelnatkin@gmail.com>
2015-05-27 21:45:09 -0700

    Spec booboo :

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,7 +4,6 @@ describe Like do
   context 'Liking an activity' do
     it 'updates like count in Algolia' do
       @activity = Fabricate :activity, title: 'Blahh!!'
-      WebMock.reset!
       @user = Fabricate :user, name: 'Bob Smith'
       @like = Fabricate :like, likeable: @activity, user: @user
     end
