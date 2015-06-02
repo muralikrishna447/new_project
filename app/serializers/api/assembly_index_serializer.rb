30190437990bace43766973273c030e42b989485
Huy Nguyen <hueezer@gmail.com>
2015-06-02 12:28:15 -0700

    [api] add description in assembly serializer

--- app/serializers/api/assembly_index_serializer.rb
+++ app/serializers/api/assembly_index_serializer.rb
@@ -1,6 +1,6 @@
 class Api::AssemblyIndexSerializer < ApplicationSerializer
   format_keys :lower_camel
-  attributes :id, :title, :image, :url, :price
+  attributes :id, :title, :image, :url, :price, :description
 
   def image
     filepicker_to_s3_url(object.image_id)
