a35a83d499adb5df8862ecd86ea21c16db859e35
Huy Nguyen <hueezer@gmail.com>
2015-06-15 18:47:10 -0700

    [homepage] fix failing test

--- db/schema.rb
+++ db/schema.rb
@@ -227,7 +227,6 @@ ActiveRecord::Schema.define(:version => 20150611165828) do
     t.string   "notes"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
-    t.string   "circulator_id", :null => false
   end
 
   create_table "comments", :force => true do |t|
