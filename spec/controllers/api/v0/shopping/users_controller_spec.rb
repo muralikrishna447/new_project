aa775f587587f59da0cc356e6c70c8002b66596a
Dan Ahern <danahern@gmail.com>
2015-08-05 20:57:36 -0700

    New passing specs

---
+++ spec/controllers/api/v0/shopping/users_controller_spec.rb
@@ -0,0 +1,29 @@
+describe Api::V0::Shopping::UsersController do
+  before :each do
+    ShopifyAPI::Base.stub(:site).and_return(URI.parse('https://test.myshopify.com/admin'))
+    ShopifyMultipass.any_instance.stub(:generate_token).and_return('abc123')
+  end
+
+  context 'GET /multipass' do
+    before :each do
+      @user = Fabricate :user, email: "test@example.com", name: "Tester Example"
+      sign_in @user
+    end
+
+    describe 'autoredirect' do
+      it 'should redirect to multipass' do
+        get :multipass, {product_id: 123, quantity: 1, autoredirect: true}
+        response.should redirect_to("https://test.myshopify.com/account/login/multipass/abc123")
+      end
+
+    end
+    describe 'ajax request' do
+      it "should return a json with multipass" do
+        get :multipass, {product_id: 123, quantity: 1}
+        redirect = JSON.parse(response.body)
+        redirect['redirect_to'].should == "https://test.myshopify.com/account/login/multipass/abc123"
+      end
+    end
+  end
+
+end
