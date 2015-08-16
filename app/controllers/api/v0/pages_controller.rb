458551dba48f40cbefce28d0dfe73d1a97a21c66
Huy Nguyen <hueezer@gmail.com>
2015-08-16 15:54:56 -0700

    [pages] accepts nested attributes

--- app/controllers/api/v0/pages_controller.rb
+++ app/controllers/api/v0/pages_controller.rb
@@ -35,12 +35,35 @@ module Api
         # Allows the api to accept params[:page][:component] and updates it to something rails prefers for nested associations
         page_params = value
         if value[:components]
-          page_params[:components_attributes] = value[:components]
+          converted_components = []
+          value[:components].each do |component|
+            converted_components << convert_hash_keys(component)
+          end
+          page_params[:components_attributes] = converted_components
           page_params.delete :components
         end
         page_params
       end
 
+      def underscore_key(k)
+        if k == 'componentType' || k == 'componentParentType' || k == 'componentParentId'
+          k.to_s.underscore.to_sym
+        else
+          k
+        end
+      end
+
+      def convert_hash_keys(value)
+        case value
+          when Array
+            value.map { |v| convert_hash_keys(v) }
+            # or `value.map(&method(:convert_hash_keys))`
+          when Hash
+            Hash[value.map { |k, v| [underscore_key(k), convert_hash_keys(v)] }]
+          else
+            value
+        end
+      end
     end
   end
 end
--- app/models/component.rb
+++ app/models/component.rb
@@ -2,7 +2,7 @@ class Component < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: [:slugged]
 
-  attr_accessible :component_type, :meta, :name, :component_parent_type, :component_parent_id, :position
+  attr_accessible :component_type, :meta, :name, :component_parent_type, :component_parent_id, :position, :slug
   serialize :meta, ActiveRecord::Coders::NestedHstore
 
   validates :component_type, presence: true
