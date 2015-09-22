e7964656648c4f990e4c788e8ba4e9da9e877d47
Nick Robin <nrobin24@gmail.com>
2015-09-22 14:59:50 -0700

    add text_fields attribute

--- app/serializers/api/ingredient_serializer.rb
+++ app/serializers/api/ingredient_serializer.rb
@@ -1,6 +1,6 @@
 class Api::IngredientSerializer < ApplicationSerializer
   format_keys :lower_camel
-  attributes :id, :title, :image, :url, :product_url
+  attributes :id, :title, :image, :url, :product_url, :text_fields
 
   def image
     filepicker_to_s3_url(object.image_id)
