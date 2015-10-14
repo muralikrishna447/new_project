c1fe29b0b0965b1aa7952bf1cd63353a1feab764
Huy Nguyen <hueezer@gmail.com>
2015-10-14 14:17:22 -0700

    [likes] update serializer

--- app/serializers/api/activity_like_serializer.rb
+++ app/serializers/api/activity_like_serializer.rb
@@ -1,6 +1,6 @@
 class Api::ActivityLikeSerializer < ApplicationSerializer
   format_keys :lower_camel
-
-  has_one :user, serializer: Api::ProfileSerializer
+  attributes :id, :user_id
+  # has_one :user, serializer: Api::ProfileSerializer
 
 end
