57c97494e7712f9ed09b200b71d1f4f1945e7bad
Huy Nguyen <hueezer@gmail.com>
2015-10-14 17:38:32 -0700

    [activities] add hero image

--- app/serializers/api/activity_serializer.rb
+++ app/serializers/api/activity_serializer.rb
@@ -1,6 +1,6 @@
 class Api::ActivitySerializer < ApplicationSerializer
   format_keys :lower_camel
-  attributes :used_in, :id, :title, :description, :image, :youtube_id, :vimeo_id, :url, :likes_count, :yield, :timing , :short_description, :tag_list, :chefsteps_generated
+  attributes :used_in, :id, :title, :description, :image, :youtube_id, :vimeo_id, :url, :likes_count, :yield, :timing , :short_description, :tag_list, :chefsteps_generated, :hero_image
   has_one :creator, serializer: Api::ProfileSerializer
   has_one :source_activity, serializer: Api::ActivityIndexSerializer
 
@@ -10,6 +10,10 @@ class Api::ActivitySerializer < ApplicationSerializer
 
   def image
     filepicker_to_s3_url(object.featured_image)
+  end
+
+  def hero_image
+    filepicker_to_s3_url(object.image_id)
   end
 
   def url
