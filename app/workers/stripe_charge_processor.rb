a3aff8cff03a8070c146ed727399640a37a75098
Michael Natkin <michaelnatkin@gmail.com>
2015-10-16 17:16:33 -0700

    Basic buy button stubbed onto gallery now. Charge controller hooked up. Lots more todo.

---
+++ app/workers/stripe_charge_processor.rb
@@ -0,0 +1,20 @@
+# ECOMTODO: retry failures, and handle cases where
+# retry didn't work. This may need to be differentiated
+# between digital and hard goods.
+
+class StripeChargeProcessor
+  @queue = :stripe_charge_processor
+  def self.perform(email, token, price, description)
+    customer = Stripe::Customer.create(
+      email: email,
+      card: token
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => (price.to_f * 100).to_i,
+      :description => description,
+      :currency    => 'usd'
+    )
+  end
+end
