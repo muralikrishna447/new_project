67ebe64c208be19d746879e7efbd1a7df75db8e0
Michael Natkin <michaelnatkin@gmail.com>
2015-10-16 17:16:33 -0700

    Basic buy button stubbed onto gallery now. Charge controller hooked up. Lots more todo.

--- app/controllers/api/v0/charges_controller.rb
+++ app/controllers/api/v0/charges_controller.rb
@@ -4,6 +4,8 @@ module Api
       before_filter :ensure_authorized
 
       def create
+        @user = User.find @user_id_from_token
+
         # ECOMTODO: this bit is just a stub where Dan can plug in real products/orders/tax/etc.
         # Harcoded right now to accept a single sku, 1000, for Premium and get the price from global settings.
         skus = JSON.parse(params[:skus])
@@ -12,39 +14,19 @@ module Api
         # ECOMTODO: this is hardcoded only to buy memberships for now; needs a robust system
         # for differentiating digital entitlements from physical products.
 
-        # ECOMTODO: this is implemented synchronously which has the same old bug - if Stripe
-        # times out we'll rollback the membership incorrectly. Probably needs a combo of
-        # synchronous, optimism, and webhook.
+        # ECOMTODO: since this has already gone through stripe.js which validates the card, we go ahead
+        # and give them premium right away which is a smoother, faster experience on the frontend, and
+        # queue up the charge which can take a little while.
 
-        puts "-----"
-        puts params[:stripeToken]
+        @user.make_premium_member()
 
-        User.transaction do
-
-          customer = Stripe::Customer.create(
-            email: current_user.email,
-            card: params[:stripeToken]
-          )
-
-          charge = Stripe::Charge.create(
-            :customer    => customer.id,
-            :amount      => Setting.last.premium_membership_price.to_i * 100,
-            :description => 'ChefSteps Premium',
-            :currency    => 'usd'
-          )
-
-          current_user.make_premium_member()
-
-        end
+        Resque.enqueue(StripeChargeProcessor, @user.email, params[:stripeToken], Setting.last.premium_membership_price,  'ChefSteps Premium')
 
         render_api_response 200
 
-      # If anything goes wrong and we weren't able to complete the charge, tell the frontend
       rescue Exception => e
+        puts e.inspect
         msg = (e.message || "(blank)")
-        logger.info "Charge failed with error: " + msg
-        logger.info "Backtrace: "
-        e.backtrace.take(20).each { |x| logger.debug x}
         render_api_response 422, { error: msg}
       end
     end
---
+++ spec/controllers/api/v0/charges_controller_spec.rb
@@ -0,0 +1,31 @@
+describe Api::V0::ChargesController do
+  context 'POST /create' do
+
+    it 'should error if not logged in' do
+      post :create, skus: [1000]
+      expect(response.status).to eq(401)
+    end
+
+    context 'logged in' do
+      before :each do
+        @user = Fabricate :user, email: 'johndoe@chefsteps.com', password: '123456', name: 'John Doe'
+        @setting = Fabricate :setting, premium_membership_price: 31393.22
+        token = ActorAddress.create_for_user(@user, client_metadata: "create").current_token
+        request.env['HTTP_AUTHORIZATION'] = token.to_jwt
+      end
+
+      it 'should error if purchasing anything other than premium membership' do
+        post :create, skus: '[1000, 2000]'
+        expect(response.status).to eq(422)
+      end
+
+      it 'should queue charge and make member premium when called correctly' do
+        User.any_instance.should_receive(:make_premium_member)
+        Resque.should_receive(:enqueue)
+        post :create, skus: '[1000]', stripeToken: 'xxx'
+        expect(response.status).to eq(200)
+      end
+
+    end
+  end
+end
