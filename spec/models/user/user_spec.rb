b1624e56b134d4b51d008e0ff2383967b9471ff4
Michael Natkin <michaelnatkin@gmail.com>
2015-10-19 11:15:52 -0700

    Store premium membership price paid on user for analytics/segmenting purposes in the future. Don't allow a user to buy premium if already bought. Specs.

--- spec/models/user/user_spec.rb
+++ spec/models/user/user_spec.rb
@@ -1,36 +1,56 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { Fabricate(:user) }
-  let(:assembly) { Fabricate(:assembly, price: 20.00) }
+  before do
+    @user = Fabricate(:user)
+    @assembly = Fabricate(:assembly, price: 20.00)
+  end
 
   context "#enrolled?" do
     context "normal enrollment" do
       it "should return true if enrollment exists" do
-        Fabricate(:enrollment, user_id: user.id, enrollable: assembly)
-        user.enrolled?(assembly).should be true
+        Fabricate(:enrollment, user_id: @user.id, enrollable: @assembly)
+        @user.enrolled?(@assembly).should be true
       end
 
       it "should return false if enrollment doesn't exist" do
-        user.enrolled?(assembly).should be false
+        @user.enrolled?(@assembly).should be false
       end
     end
   end
 
   context "tokens" do
     it 'should return a valid auth token when no actor address exists' do
-      token = user.valid_website_auth_token
-      aa = ActorAddress.find_for_user_and_unique_key(user, 'website')
+      token = @user.valid_website_auth_token
+      aa = ActorAddress.find_for_user_and_unique_key(@user, 'website')
       aa.valid_token?(token).should be true
       # TODO - use timecop to avoid time hacks like this - 5
       token[:exp].should >= (Time.now.to_i + 1.year.to_i - 5)
     end
 
     it 'should return a valid auth token when no actor address exists' do
-      aa = ActorAddress.create_for_user(user, unique_key: 'website')
-      token = user.valid_website_auth_token
+      aa = ActorAddress.create_for_user(@user, unique_key: 'website')
+      token = @user.valid_website_auth_token
       token.claim[:a].should == aa.address_id
       aa.valid_token?(token).should be true
+    end
+  end
+
+  context "premium member" do
+    it "should default false" do
+      @user.premium?.should be false
+    end
+
+    it "make_premium_member should set correct fields" do
+      @user.make_premium_member(10)
+      @user.premium?.should be true
+      @user.premium_membership_created_at.should be > DateTime.now - 1.hour
+      expect(@user.premium_membership_price).to eq(10)
+    end
+
+    it "should only allow you to become premium once" do
+      @user.make_premium_member(10)
+      expect {@user.make_premium_member(10)}.to raise_error
     end
   end
 end
