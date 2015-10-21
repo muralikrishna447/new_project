3687176a8de4888f1ff9e0a8acbd08c1a6b41d0b
Michael Natkin <michaelnatkin@gmail.com>
2015-10-21 13:34:31 -0700

    fix spec

--- app/serializers/api/user_me_serializer.rb
+++ app/serializers/api/user_me_serializer.rb
@@ -1,5 +1,8 @@
 class Api::UserMeSerializer < ApplicationSerializer
-  format_keys :lower_camel
+  # TODO: the controller that uses this wasn't previously returning lowerCamel. Fixing
+  # that will break some clients so not doing it now.
+  #format_keys :lower_camel
+
   attributes :id, :name, :slug, :email, :intercom_user_hash, :avatar_url, :encrypted_bloom_info, :premium, :admin
 
   def intercom_user_hash
