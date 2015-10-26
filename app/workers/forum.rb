48f85134561a0078ed49e92b996bb96df63a698c
Huy Nguyen <hueezer@gmail.com>
2015-10-26 10:20:59 -0700

    [forum] fix worker bug

--- app/workers/forum.rb
+++ app/workers/forum.rb
@@ -4,7 +4,7 @@ class Forum
     case type
     when "update_user"
       Faraday.get do |req|
-        req.url "#{endpoint}/users/#{user.id}/initial?apiKey=xchefsteps&ssoId=#{user.id}"
+        req.url "#{endpoint}/users/#{user_id}/initial?apiKey=xchefsteps&ssoId=#{user_id}"
         req.options[:timeout] = 3
         req.options[:open_timeout] = 2
       end
