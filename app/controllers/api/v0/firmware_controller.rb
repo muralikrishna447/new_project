ee7f8731f08eb49c55ddbc9abce57fdf566b4f26
Kenshi Kawaguchi <kenshi@recurse.ca>
2015-10-29 12:59:21 -0700

    dfu: create a temporary S3 link to firmware

--- app/controllers/api/v0/firmware_controller.rb
+++ app/controllers/api/v0/firmware_controller.rb
@@ -2,17 +2,33 @@ module Api
   module V0
     class FirmwareController < BaseController
       before_filter :ensure_authorized_or_anonymous
+
       def latest_version
         if @user_id_from_token
           @user = User.find @user_id_from_token
         else
           @user = nil
         end
+        version = 'v1.0.0'
+
+        link = get_firmware_link('v1.0.0')
         data = {
-          "version" => "1.0.0",
-          "location" => "https://www.foo.com/firmware.tar.gz"
+          "version" => version,
+          "location" => link
         }
         render json: data
+      end
+
+      private
+
+      def get_firmware_link(version)
+        s3_client = AWS::S3::Client.new(region: 'us-east-1')
+        bucket_name = 'chefsteps-firmware-staging'
+        key_name = "joule/#{version}/application.bin"
+        bucket = AWS::S3::Bucket.new(bucket_name, :client => s3_client)
+        o = bucket.objects[key_name]
+        expire_secs = 60 * 5
+        o.url_for(:get, {:secure => true, :expires => expire_secs}).to_s
       end
     end
   end
