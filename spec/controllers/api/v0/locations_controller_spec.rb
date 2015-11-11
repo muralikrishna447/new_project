07cf19b0fffcc0ae30e189e51c0fb45fe3c8a6de
Dan Ahern <danahern@gmail.com>
2015-11-11 14:07:40 -0800

    Hopefully fixes codeship error

--- spec/controllers/api/v0/locations_controller_spec.rb
+++ spec/controllers/api/v0/locations_controller_spec.rb
@@ -1,10 +1,5 @@
 describe Api::V0::LocationsController do
   context 'GET /locations' do
-    before :each do
-      geoip_response = Hashie::Mash.new(country: {iso_code: 'US'}, location: {latitude: 47.5943, longitude: -122.6265}, city: {names: {en: 'Bremerton'}}, subdivisions: [{iso_code: 'WA'}], postal: {code: '98310'})
-      Geoip2.stub(:city).and_return(geoip_response)
-      AvaTax::TaxService.any_instance.stub(:estimate).and_return('Rate' => '0.084')
-    end
     it "should respond with blank location data" do
       Geoip2.stub(:city).and_return(nil)
       get :index
@@ -14,6 +9,10 @@ describe Api::V0::LocationsController do
     end
 
     it "should respond with location data" do
+      @request.env['REMOTE_ADDR'] = '1.2.3.4'
+      geoip_response = Hashie::Mash.new(country: {iso_code: 'US'}, location: {latitude: 47.5943, longitude: -122.6265}, city: {names: {en: 'Bremerton'}}, subdivisions: [{iso_code: 'WA'}], postal: {code: '98310'})
+      Geoip2.stub(:city).and_return(geoip_response)
+      AvaTax::TaxService.any_instance.stub(:estimate).and_return('Rate' => '0.084')
       get :index
       response.should be_success
       location = JSON.parse(response.body)
