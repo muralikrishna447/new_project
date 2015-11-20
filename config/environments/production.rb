c606db30c9f008079291f005db60b3e0d60404f1
Dan Ahern <danahern@gmail.com>
2015-11-19 17:52:58 -0800

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,5 +99,5 @@ Delve::Application.configure do
   }
   ENV['MAILCHIMP_API_KEY'] = config.mailchimp[:api_key] # for gibbon
 
-  config.middleware.insert_before('BromboneProxy', 'PreauthEnforcer', [/^\/tpq/])
+  # config.middleware.insert_before('BromboneProxy', 'PreauthEnforcer', [/^\/tpq/])
 end
