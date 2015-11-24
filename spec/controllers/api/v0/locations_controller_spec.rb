35353fb85400c9ee09070de6a893bfbe803401da
Dan Ahern <danahern@gmail.com>
2015-11-24 09:21:47 -0800

    Fix and test for the geocode

--- spec/controllers/api/v0/locations_controller_spec.rb
+++ spec/controllers/api/v0/locations_controller_spec.rb
@@ -8,6 +8,16 @@ describe Api::V0::LocationsController do
       location.should include('country' => nil, 'latitude' => nil, 'longitude' => nil, 'city' => nil, 'state' => nil, 'zip' => nil, 'taxPercent' => nil)
     end
 
+    it "should respond with blank" do
+      @request.env['REMOTE_ADDR'] = '1.2.3.4'
+      geoip_response = Hashie::Mash.new(location: {latitude: 47.5943, longitude: -122.6265}, city: {names: {en: 'Bremerton'}}, subdivisions: [{iso_code: 'WA'}], postal: {code: '98310'})
+      Geoip2.stub(:city).and_return(geoip_response)
+      get :index
+      response.should be_success
+      location = JSON.parse(response.body)
+      location.should include('country' => nil, 'latitude' => nil, 'longitude' => nil, 'city' => nil, 'state' => nil, 'zip' => nil, 'taxPercent' => nil)
+    end
+
     it "should respond with location data" do
       @request.env['REMOTE_ADDR'] = '1.2.3.4'
       geoip_response = Hashie::Mash.new(country: {iso_code: 'US'}, location: {latitude: 47.5943, longitude: -122.6265}, city: {names: {en: 'Bremerton'}}, subdivisions: [{iso_code: 'WA'}], postal: {code: '98310'})
