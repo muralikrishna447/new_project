4ad97f50a649969dcafea91013f4411fab4b2567
Michael Natkin <michaelnatkin@gmail.com>
2015-11-23 16:37:48 -0800

    change subject per Karen

--- app/mailers/joule_confirmation_mailer.rb
+++ app/mailers/joule_confirmation_mailer.rb
@@ -1,6 +1,6 @@
 class JouleConfirmationMailer < BaseMandrillMailer
   def prepare(user)
-    subject = "Joule is Headed Your Way!"
+    subject = "Thank You For Purchasing Joule!"
     merge_vars = {}
     body = mandrill_template("joule-purchase-confirmation", merge_vars)
     send_mail(user.email, subject, body)
