02df2f1148d90e629f8fca42445acf832d8e1e22
Alex Thomson <alex@chefsteps.com>
2015-11-30 13:15:29 -0800

    [auth] Unifying sign-up mailchimp subscribe and track code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -78,12 +78,10 @@ private
     if @user.save
       @user.ensure_authentication_token!
       if @new_signup
-        # Trigger as a signup
         sign_in @user
-        email_list_signup(@user.name, @user.email, "ajax_signup_form_social")
         cookies.delete(:viewed_activities)
-        mixpanel.alias(@user.email, mixpanel_anonymous_id) if mixpanel_anonymous_id
-        mixpanel.track(@user.email, 'Signed Up')
+        subscribe_and_track @user, false, "ajax_signup_form_social"
+
         return render status: 200, json: {success: true, new_user: @new_signup, info: "Signed Up", user: current_user.as_json(include: :enrollments)}
       else
         # Trigger as a login
