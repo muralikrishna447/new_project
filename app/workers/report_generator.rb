60c3ac2893f7795b3f7b69813e04f5b745ee3852
Dan Ahern <danahern@gmail.com>
2015-12-07 13:52:15 -0800

    New report for Ed

--- app/workers/report_generator.rb
+++ app/workers/report_generator.rb
@@ -5,6 +5,9 @@ class ReportGenerator
     when "stripe"
       user = User.find(user_id)
       stripe_data = StripeReport.quickbooks_report(start_date, end_date)
+      f = File.new("tmp/stripe_data_export.csv", 'wb')
+      f.puts stripe_data
+      f.close
       ReportMailer.send_report_file(user.email, 'stripe_data', 'here is the stripe data', "quickbooks-file-#{start_date}-#{end_date}.tsv", stripe_data).deliver
     when "sales"
     end
