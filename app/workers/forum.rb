0c8f788005a1cf55b1c0b3cf7f8645f55c42988f
Huy Nguyen <hueezer@gmail.com>
2015-12-14 11:28:39 -0800

    [forum] add logging in the forum sync worker

--- app/workers/forum.rb
+++ app/workers/forum.rb
@@ -3,7 +3,9 @@ class Forum
   def self.perform(type, endpoint, user_id)
     case type
     when "update_user"
-      HTTParty.get "#{endpoint}/users/#{user.id}/initial?apiKey=xchefsteps&ssoId=#{user.id}"
+      url = "#{endpoint}/users/#{user.id}/initial?apiKey=xchefsteps&ssoId=#{user.id}"
+      response = HTTParty.get url
+      @logger.info "User: #{user.id} Response: #{response.inspect}"
     end
   end
 end
