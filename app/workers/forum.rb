49bd4f92d61eb5691c14e6d262490da99c5214dc
Huy Nguyen <hueezer@gmail.com>
2015-12-14 14:02:46 -0800

    [bloom] user sync use Faraday and increase timeout

--- app/workers/forum.rb
+++ app/workers/forum.rb
@@ -3,16 +3,13 @@ class Forum
   def self.perform(type, endpoint, user_id)
     case type
     when "update_user"
-      url = "#{endpoint}/users/#{user.id}/initial?apiKey=xchefsteps&ssoId=#{user.id}"
-      @logger.info "User: #{user.id} Request url: #{url}"
-      response = HTTParty.get url, timeout: 30
-      
-      if /^2/.match(response.code.to_s)
-        @logger.info "Bloom successfully synced user: #{user.id} Response: #{response.inspect}"
-      else
-        @logger.info "Bloom failed to sync user: #{user.id} Response: #{response.inspect}"
-        raise "Bloom failed to sync user: #{user.id}"
+
+      Faraday.get do |req|
+        req.url "#{endpoint}/users/#{user_id}/initial?apiKey=xchefsteps&ssoId=#{user_id}"
+        req.options[:timeout] = 30
+        @logger.info "User: #{user.id} Request: #{req}"
       end
+
     end
   end
 end
