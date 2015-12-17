8458e51b616dd7d6c206489cc9a47cd9e26d0022
Dan Ahern <danahern@gmail.com>
2015-12-17 04:00:42 +0000

    raise cache time to smooth over errors

--- app/controllers/api/v0/products_controller.rb
+++ app/controllers/api/v0/products_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V0
     class ProductsController < BaseController
       def index
-        circulator, premium = Rails.cache.fetch("stripe_products", expires_in: 5.minutes){
+        circulator, premium = Rails.cache.fetch("stripe_products", expires_in: 1.hour){
           StripeOrder.stripe_products
         }
         result = {products: {circulator[:sku] => circulator, premium[:sku] => premium}}
