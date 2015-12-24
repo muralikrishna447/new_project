939020314cd7ae985b6c271649941142cd120bef
Michael Natkin <michaelnatkin@gmail.com>
2015-12-24 16:59:58 -0500

    admin for gift certs

--- app/admin/gift_certificate.rb
+++ app/admin/gift_certificate.rb
@@ -1,10 +1,17 @@
 ActiveAdmin.register PremiumGiftCertificate do
   remove_filter :user
 
+  scope("Redeemed") { |scope| scope.where(redeemed: true) }
+  scope("Not Redeemed") { |scope| scope.where(redeemed: false) }
+
   index do
     column "Purchaser", :user
-    column :price
-    column :sales_tax
+    column :price do |g|
+       g.price / 100.0
+    end
+    column :sales_tax do |g|
+      g.sales_tax / 100.0
+    end
     column :redeemed
   end
 end
