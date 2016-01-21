2932f9cde9386dcba1d4c25972ae6b84eff4e113
Alex Thomson <alex@chefsteps.com>
2016-01-21 15:25:04 -0800

    [shopify] Adding add_to_cart multipass endpoint

--- spec/controllers/api/v0/shopping/users_controller_spec.rb
+++ spec/controllers/api/v0/shopping/users_controller_spec.rb
@@ -4,19 +4,21 @@ describe Api::V0::Shopping::UsersController do
     Shopify::Multipass.any_instance.stub(:generate_token).and_return('abc123')
   end
 
-  context 'GET /multipass' do
+  context 'GET /multipass or /add_to_cart' do
     before :each do
       @user = Fabricate :user, email: "test@example.com", name: "Tester Example"
       sign_in @user
     end
 
     describe 'autoredirect' do
-      it 'should redirect to multipass' do
+      it 'should redirect from and to multipass' do
         get :multipass, {product_id: 123, quantity: 1, autoredirect: true}
         response.should redirect_to("https://chefsteps-staging.myshopify.com/account/login/multipass/abc123")
       end
-
+      it 'should redirect from add_to_cart to multipass' do
+        get :add_to_cart, {variant_id: 123}
+        response.should redirect_to("https://chefsteps-staging.myshopify.com/account/login/multipass/abc123")
+      end
     end
   end
-
 end
