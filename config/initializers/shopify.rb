ea719baae8fe154b231558d98257960941a64f5a
Alex Thomson <alex@chefsteps.com>
2016-02-24 20:26:17 -0800

    [shopify] Adding account subdomain since API access requires a different host from the public store

--- config/initializers/shopify.rb
+++ config/initializers/shopify.rb
@@ -6,6 +6,7 @@ require "json"
 if Rails.env.production?
   Rails.configuration.shopify = {
     store_domain: 'store.chefsteps.com',
+    account_subdomain: 'delve',
     api_key: ENV["SHOPIFY_KEY"],
     password: ENV["SHOPIFY_SECRET"],
     multipass_secret: ENV["SHOPIFY_MULTIPASS_SECRET"]
@@ -13,6 +14,7 @@ if Rails.env.production?
 elsif Rails.env.staging? || Rails.env.staging2?
   Rails.configuration.shopify = {
     store_domain: 'store.chocolateyshatner.com',
+    account_subdomain: 'chefsteps-staging',
     api_key: ENV["SHOPIFY_KEY"],
     password: ENV["SHOPIFY_SECRET"],
     multipass_secret: ENV["SHOPIFY_MULTIPASS_SECRET"]
@@ -20,6 +22,7 @@ elsif Rails.env.staging? || Rails.env.staging2?
 elsif Rails.env.test?
   Rails.configuration.shopify = {
     store_domain: 'test.myshopify.com',
+    account_subdomain: 'test',
     api_key: '123',
     password:  '321',
     multipass_secret: "abc"
@@ -27,9 +30,11 @@ elsif Rails.env.test?
 else
   Rails.configuration.shopify = {
     store_domain: 'store.chocolateyshatner.com',
+    account_subdomain: 'chefsteps-staging',
     api_key: 'f3c79828c0f50b04866481389eacb2d2',
     password:  '5553e01d45c426ced082e9846ad56eee',
     multipass_secret: '905c5b3b3ad7804d28e8a33e6a247eca'
   }
 end
-ShopifyAPI::Base.site = "https://#{Rails.configuration.shopify[:api_key]}:#{Rails.configuration.shopify[:password]}@#{Rails.configuration.shopify[:store_domain]}/admin"
+
+ShopifyAPI::Base.site = "https://#{Rails.configuration.shopify[:api_key]}:#{Rails.configuration.shopify[:password]}@#{Rails.configuration.shopify[:account_subdomain]}.myshopify.com/admin"
