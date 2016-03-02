2023b395a111e7001cb8cb545092033657507f62
Huy Nguyen <hueezer@gmail.com>
2016-03-01 16:40:00 -0800

    [shopify]

--- app/controllers/api/v0/shopping/products_controller.rb
+++ app/controllers/api/v0/shopping/products_controller.rb
@@ -2,14 +2,41 @@ module Api
   module V0
     module Shopping
       class ProductsController < BaseController
+
+        def index
+          get_cached_products
+          render(json: @products)
+        end
+
+        # Get product by sku
         def show
-          product = Rails.cache.fetch("shopping/product/#{params[:id]}", expires_in: 1.minute) do
-            product_result = ShopifyAPI::Product.get(params[:id])
-            product_result[:quantity] = product_result["variants"].first["inventory_quantity"]
-            product_result[:price] = product_result["variants"].first["price"]
-            product_result
+          sku = params[:id]
+          product_id = product_id_by_sku(sku)
+          @product = Rails.cache.fetch("shopping/products/#{product_id}", expires_in: 1.second) do
+            result = ShopifyAPI::Product.get(product_id)
+            result
           end
-          render(json: product)
+          render(json: @product)
+        end
+
+        private
+        def get_cached_products
+          @products = Rails.cache.fetch("shopping/products", expires_in: 1.second) do
+            results = ShopifyAPI::Product.find(:all)
+            results = results.map do |product|
+              {
+                id: product.id,
+                title: product.title,
+                sku: product.variants.first.sku
+              }
+            end
+            results
+          end
+        end
+
+        def product_id_by_sku(sku)
+          get_cached_products
+          @products.select{|product| product[:sku] == sku}.first[:id]
         end
       end
     end
