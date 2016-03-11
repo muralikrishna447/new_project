e72aee4771fd03b6ba57c87c16520afd69669e9c
Michael Natkin <michaelnatkin@gmail.com>
2016-03-11 09:15:22 -0800

    add librato metrics

--- app/workers/algolia_sync.rb
+++ app/workers/algolia_sync.rb
@@ -5,10 +5,12 @@ class AlgoliaSync
     begin
       Activity.find(activity_id).index!
     rescue Exception => e
+      Librato.increment("algolia.sync_failed.activity")
       msg = "Exception #{e.message} while syncing activity [#{activity_id}] to Algolia"
       Rails.logger.error msg
       raise msg
     end
+    Librato.increment("algolia.synced.activity")
     Rails.logger.info "Finished syncing activity [#{activity_id}] to Algolia"  
   end
 end
