9643e692062c0468ea55eed4c9118f7d65f321d0
Alex Thomson <alex@chefsteps.com>
2016-03-30 19:28:21 -0700

    [firmware] Updating firmware updates endpoint

--- app/controllers/api/v0/firmware_controller.rb
+++ app/controllers/api/v0/firmware_controller.rb
@@ -3,32 +3,63 @@ module Api
     class FirmwareController < BaseController
       before_filter :ensure_authorized_or_anonymous
 
-      LINK_EXPIRE_SECS = 60 * 5
+      LINK_EXPIRE_SECS = 60 * 20
 
-      def latest_version
-        if @user_id_from_token
-          @user = User.find @user_id_from_token
-        else
-          @user = nil
+      def updates
+        # How this currently works
+        # - Static mapping of app version to firmware app version
+        # - Soft device bootloader etc not really supported
+
+        # TODO - validate parameters - we're currently plugging them into s3 urls which has to be unsafe
+        # TODO: app should specify android or ios
+        # TODO: need to communicate that app itself needs update
+
+        app_version = params[:appVersion]
+        if app_version.nil?
+          return render_api_response 400, {code: 'invalid_request_error', message: 'Must specify mobile app version'}
         end
-        version = 'v1.0.0'
-        link = get_firmware_link(version)
-        logger.debug("Handing out firmware link for #{version} to user " \
-                     "[#{@user_id_from_token}]: #{link}")
 
-        data = {
-          "version" => version,
-          "location" => link
-        }
-        render json: data
+        potential_updates = get_firmware_for_app_version(app_version)
+        if potential_updates.nil?
+          logger.info "No manifest found for app version #{app_version}"
+          return render_empty_response
+        end
+
+        updates = []
+        potential_updates.each do |u|
+          current_version = params[u['versionType']]
+          if current_version == u['version']
+            logger.info "Correct version for type [#{u['type']}]"
+            break
+          end
+          # TODO - store the versionType / type mapping not in JSON
+          u.delete('versionType')
+          u['location'] = get_firmware_link(u['type'], u['version'])
+          updates << u
+        end
+
+        render_api_response 200, {'updates' => updates, 'bootModeType' => 'APPLICATION_BOOT_MODE'}
       end
 
       private
+      def render_empty_response
+        render_api_response 200, {:updates => []}
+      end
 
-      def get_firmware_link(version)
+      def get_firmware_for_app_version(version)
         s3_client = AWS::S3::Client.new(region: 'us-east-1')
         bucket_name = Rails.application.config.firmware_bucket
-        key_name = "joule/#{version}/application.bin"
+        key_name = "manifests/#{version}/manifest"
+        bucket = AWS::S3::Bucket.new(bucket_name, :client => s3_client)
+        o = bucket.objects[key_name]
+        return nil if !o.exists?
+        JSON.parse(o.read)
+      end
+
+      def get_firmware_link(type, version)
+        s3_client = AWS::S3::Client.new(region: 'us-east-1')
+        bucket_name = Rails.application.config.firmware_bucket
+        key_name = "joule/#{type}/#{version}/application.bin"
         bucket = AWS::S3::Bucket.new(bucket_name, :client => s3_client)
         o = bucket.objects[key_name]
         o.url_for(:get, {:secure => true, :expires => LINK_EXPIRE_SECS}).to_s
--- spec/controllers/api/v0/firmware_controller_spec.rb
+++ spec/controllers/api/v0/firmware_controller_spec.rb
@@ -5,27 +5,47 @@ describe Api::V0::FirmwareController do
 
     @link = 'http://www.foo.com'
     controller.stub(:get_firmware_link).and_return(@link)
+    
+    WebMock.stub_request(:head, "https://chefsteps-firmware-staging.s3.amazonaws.com/manifests/0.18.0/manifest").
+      to_return(:status => 200, :body => "[]")
+      
+    manifest = [{
+      "versionType" => "appFirmwareVersion",
+      "type" => "APPLICATION_FIRMWARE",
+      "version" => "alex_latest" }]
+
+    WebMock.stub_request(:get, "https://chefsteps-firmware-staging.s3.amazonaws.com/manifests/0.18.0/manifest").
+      to_return(:status => 200, :body => manifest.to_json, :headers => {})
   end
 
   it 'should get firmware version' do
     request.env['HTTP_AUTHORIZATION'] = @token.to_jwt
-    get :latest_version
+      
+    post :updates, {'appVersion'=> '0.18.0'}
+        
     response.should be_success
     resp = JSON.parse(response.body)
-    resp['version'].should_not be_nil
-    resp['location'].should_not be_nil
+    puts resp.inspect
+    resp['updates'].length.should == 1
+    update = resp['updates'].first
+    update['type'].should == 'APPLICATION_FIRMWARE'
+    update['location'].should == @link
+    
   end
 
-  it 'should get firmware version if not logged in' do
-    get :latest_version
+  it 'should get no updates if no manifest found' do
+    WebMock.stub_request(:head, "https://chefsteps-firmware-staging.s3.amazonaws.com/manifests/0.10.0/manifest").
+      to_return(:status => 404, :body => "", :headers => {})
+    post :updates, {'appVersion'=> '0.10.0'}
+    puts response.code
     response.should be_success
     resp = JSON.parse(response.body)
-    resp['version'].should_not be_nil
-    resp['location'].should == @link
+    resp['updates'].should be_empty
   end
+
   it 'should fail if bad token' do
     request.env['HTTP_AUTHORIZATION'] = 'fooooooo'
-    get :latest_version
+    post :updates
     response.should_not be_success
   end
 end
