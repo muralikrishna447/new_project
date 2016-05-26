9a2985eac5439ede0ea5f0dba1605dcd7f668cbf
Alex Thomson <alex@chefsteps.com>
2016-05-26 16:05:48 -0700

    [push] Configuring prod SNS endpoints (still using APNS-dev though)

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -8,9 +8,10 @@ sns.platform_applications = {}
 if Rails.env.production?
   Rails.configuration.remote_log_bucket = 'remote-logs-production'  
   
-  sns.platform_applications['android'] = 'arn:aws:sns:us-east-1:021963864089:app/GCM/production-joule-android'
-  sns.platform_applications['ios'] = 'arn:aws:sns:us-east-1:021963864089:app/APNS/production-joule-ios'
-
+  # TODO - update to prod sns applications - not worried about forgetting since
+  # we'll be forced to do this when app is released.
+  sns.platform_applications['android'] = 'arn:aws:sns:us-east-1:021963864089:app/GCM/joule-android'
+  sns.platform_applications['ios'] = 'arn:aws:sns:us-east-1:021963864089:app/APNS_SANDBOX/joule-ios-dev'
 else
   Rails.configuration.remote_log_bucket = 'remote-logs-staging'
   
