676d9bbe189f22f94d76dc2ff309cab5323d02c5
Alex Thomson <alex@chefsteps.com>
2016-06-13 11:51:35 -0700

    [mail] Allow development email send to @chefsteps.com to be sent to the intended recipient

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,8 +1,14 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
-    all_to = [message.to,message.bcc,message.cc].compact.join(",")
-    message.subject = "#{all_to} #{message.subject}"
-    message.to = "dev@chefsteps.com"
+
+    # Allow emails to chefsteps employees to send normally
+    unless message.to.first.match /@chefsteps.com$/
+      all_to = [message.to,message.bcc,message.cc].compact.join(",")
+      message.to = "dev@chefsteps.com"
+      message.subject += " [#{all_to}]"
+    end
+
+    message.subject += " [#{Rails.env}]"
     message.bcc = nil
     message.cc = nil
   end
