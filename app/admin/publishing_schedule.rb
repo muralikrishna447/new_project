259ff248cdc5fba778d1b6e053d8ac3425ccc5ae
Michael Natkin <michaelnatkin@gmail.com>
2016-07-01 12:20:54 -0700

    timezone fun

--- app/admin/publishing_schedule.rb
+++ app/admin/publishing_schedule.rb
@@ -6,6 +6,9 @@ ActiveAdmin.register Activity, as: 'Publishing Schedule' do
 
   menu priority: 2
 
+  form partial: 'form'
+
+
   controller do
     def scoped_collection
       Activity
@@ -19,7 +22,7 @@ ActiveAdmin.register Activity, as: 'Publishing Schedule' do
     def update
       @activity = Activity.find(params[:id])
       begin
-        @activity.publishing_schedule.update_attributes!(params[:publishing_schedule][:publishing_schedule_attributes])
+        @activity.publishing_schedule.update_attributes!(params[:activity][:publishing_schedule_attributes])
         redirect_to admin_publishing_schedules_path
         return
       rescue Exception => e
@@ -51,7 +54,7 @@ ActiveAdmin.register Activity, as: 'Publishing Schedule' do
   index title: 'Publishing Schedule' do
     column "Publish At" do |activity|
       if activity.publishing_schedule
-        text =  activity.publishing_schedule.publish_at.localtime.strftime('%a %b %d, %Y %l:%M %p %Z')
+        text =  activity.publishing_schedule.publish_at.in_time_zone(Rails.application.config.chefsteps_timezone).strftime('%a %b %d, %Y %l:%M %p %Z')
         link_to(text, edit_admin_publishing_schedule_path(activity))
       else
         link_to("Schedule...", edit_admin_publishing_schedule_path(activity))
@@ -65,13 +68,6 @@ ActiveAdmin.register Activity, as: 'Publishing Schedule' do
     column :title, sortable: :title do |activity|
       link_to activity.title.html_safe, activity_path(activity), target: '_blank'
     end
-  end
-
-  form do |f|
-    f.inputs "#{f.object.title}", for: [:publishing_schedule, f.object.publishing_schedule] do |ps|
-      ps.input :publish_at, as: :just_datetime_picker
-    end
-    f.actions
   end
 end
 
--- app/models/publishing_schedule.rb
+++ app/models/publishing_schedule.rb
@@ -1,12 +1,33 @@
 class PublishingSchedule < ActiveRecord::Base
   belongs_to :activity
-  attr_accessible :publish_at
-  just_define_datetime_picker :publish_at, add_to_attr_accessible: true
+  attr_accessible :publish_at, :publish_at_pacific
+
+
+  #just_define_datetime_picker :publish_at, add_to_attr_accessible: true
 
   # Can't use normal rails validation because this needs to happen after just_define_datetime_picker
   # copies its fields to the real publish_at. There is probably a better way.
   before_save :validate_future
   def validate_future
     raise "Date must be in future" if self.publish_at < DateTime.now
+  end
+
+  # Convert rails DateTime to the limited ISO-8601 expected by browser, in Pacific time
+  # https://www.w3.org/TR/html-markup/input.datetime-local.html
+  # Example: 1996-12-19T16:39:57 - at least in Chrome it will not work with the -7:00 zone part.
+  def publish_at_pacific
+    pst = self.publish_at.in_time_zone(Rails.application.config.chefsteps_timezone).iso8601()
+    puts "PST out: #{pst}"
+    pst.gsub(/:\d\d[-+]\d\d:\d\d$/, '')
+  end
+
+  def publish_at_pacific=(pst)
+    # Wanted to use Rails.application.config.chefsteps_timezone.formatted_offset but it
+    # doesn't account for daylight savings so doesn't roundtrip with #publish_at_pacific
+    offset = Time.now.in_time_zone(Rails.application.config.chefsteps_timezone).utc_offset
+    hours = offset / 3600
+    pst = pst + format("%02d:00", hours)
+    puts "PST in: #{pst}"
+    self.publish_at = DateTime.parse(pst)
   end
 end
