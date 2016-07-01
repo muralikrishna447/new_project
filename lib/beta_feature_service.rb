739fa6cdfd95aff20e567e6e870057cb7f99e62f
Kenshi Kawaguchi <kenshi@recurse.ca>
2016-07-01 15:55:03 -0700

    MockBetaFeatureService returns true by default - stub to override

--- lib/beta_feature_service.rb
+++ lib/beta_feature_service.rb
@@ -17,16 +17,16 @@ module BetaFeature
     end
   end
 
+
   class MockBetaFeatureService
     def initialize
       @items = []
     end
 
-    def add(email, feature_name)
-      @items.push([email, feature_name])
-      puts @items
-    end
-
+    # We default to always returning true for tests.  To override this
+    # behaviour for specific tests:
+    #
+    #   BetaFeatureService.stub(:user_has_feature).and_return(false)
     def user_has_feature(email, feature_name)
       return true
     end
