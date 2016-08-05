ea4cdcc3390c5d5ecb6c2ff54b14047c11fb05fa
Alex Thomson <alex@chefsteps.com>
2016-08-05 16:53:32 -0700

    [email] Adding refund email template

--- app/workers/joule_ship_date_sender.rb
+++
@@ -1,8 +0,0 @@
-class JouleShipDateSender
-  @queue = :joule_ship_date_sender
-  def self.perform(email)
-    Rails.logger.info "Starting JouleShipDateSender job for email [#{email}]"
-    user = User.where(email: email).first
-    JouleShipDateMailer.prepare(user).deliver
-  end
-end
