bae1d3cc169c0137a31db83530a690293233038b
Huy Nguyen <hueezer@gmail.com>
2016-08-23 16:37:06 -0700

    [promotions] remove uneeded methods

--- app/controllers/api/v0/shopping/discounts_controller.rb
+++ app/controllers/api/v0/shopping/discounts_controller.rb
@@ -7,7 +7,9 @@ module Api
 
         def show
           begin
-            @discount = ShopifyAPI::Discount.find(params[:id])
+            @discount = CacheExtensions::fetch_with_rescue("shopping/discounts/#{params[:id]}", 1.minute, 1.minute) do
+              ShopifyAPI::Discount.find(params[:id])
+            end
             @discount.valid = valid?(@discount)
             render_api_response 200, @discount
           rescue
@@ -18,29 +20,10 @@ module Api
 
         private
 
-        # Don't expose all discounts but make sure they're cached
-        def get_all_discounts
-          # Test kept failing with error: undefined class/module ShopifyAPI::Discount::AppliesTo
-          if !Rails.env.test?
-            @discounts = CacheExtensions::fetch_with_rescue('shopping/discounts', 1.minute, 1.minute) do
-              map_valid
-            end
-          else
-            @discounts = map_valid
-          end
-        end
-
         # Adding a :valid attribute because Shopify::Discount can be expired but still enabled
         def valid?(discount)
           expired = (discount.ends_at && DateTime.parse(discount.ends_at) < Date.today) ? true : false
           discount.status == 'enabled' && !expired
-        end
-
-        def map_valid
-          ShopifyAPI::Discount.all.map do |discount|
-            discount.valid = valid?(discount)
-            discount
-          end
         end
 
       end
--- spec/controllers/api/v0/shopping/discounts_controller_spec.rb
+++ spec/controllers/api/v0/shopping/discounts_controller_spec.rb
@@ -12,13 +12,6 @@ describe Api::V0::Shopping::DiscountsController do
       @controller = Api::V0::Shopping::DiscountsController.new
     end
 
-    context 'get_all_discounts' do
-      it "should return an array of all discounts" do
-        discounts = @controller.instance_eval { get_all_discounts }
-        expect(discounts.length).to eq(4)
-      end
-    end
-
     context 'valid?(discount)' do
       it "should return true for a valid discount" do
         discount = ShopifyAPI::Discount.find(101)
@@ -45,17 +38,9 @@ describe Api::V0::Shopping::DiscountsController do
       end
     end
 
-    context 'map_valid' do
-      it "should return an array of discounts and set the valid attribute" do
-        discounts = @controller.instance_eval { map_valid }
-        puts "discounts here: #{discounts.inspect}"
-        expect(discounts.length).to eq(4)
-        expect(discounts.map{|d| d.valid}).to eq([true,false,false,true])
-      end
-    end
   end
 
-  describe 'GET /discounts/:code' do
+  describe 'GET /discounts/:id' do
 
     it "should return a discount" do
       get :show, id: '101'
