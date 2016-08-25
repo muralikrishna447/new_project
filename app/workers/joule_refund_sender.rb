12ae5a815bf40745d66cd7fa3460de239d2d9977
Huy Nguyen <hueezer@gmail.com>
2016-08-25 13:58:49 -0700

    update template

--- app/workers/joule_refund_sender.rb
+++ app/workers/joule_refund_sender.rb
@@ -1,6 +1,7 @@
 class JouleRefundSender
   @queue = :JouleRefundSender
   def self.perform(user_id, refund_amount)
+    
     user = User.find(user_id)
     Rails.logger.info "Starting JouleRefundSender job for user with email: [#{user.email}] for refund_amount: $#{refund_amount} "
     JouleRefundMailer.prepare(user, refund_amount).deliver
