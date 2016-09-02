8ccb1beae665528e7ce754cc1b28fbc8e9cdbde7
Dan Ahern <danahern@gmail.com>
2016-09-02 14:11:46 -0700

    Copy values

--- app/controllers/api/v0/circulators_controller.rb
+++ app/controllers/api/v0/circulators_controller.rb
@@ -159,12 +159,12 @@ module Api
             hardwareVersion: 'JL.p5',
             appFirmwareVersion: '1.1', # Will be 47
             coefficients: {
-              tempAdcBias: 1,
-              tempAdcScale: 2,
-              tempRef: 3,
-              tempCoeffA: 4,
-              tempCoeffB: 5,
-              tempCoeffC: 6
+              tempAdcBias: -65536,
+              tempAdcScale: 7.629452739355006e-06, # (1.0f / (65535 - -65536))
+              tempRef: 2.49e-04, #2.49e4f
+              tempCoeffA: 0.0012978594740199803, # cs_config_mgmt.c - Line 61
+              tempCoeffB: 0.00020601808214437865, # cs_config_mgmt.c - Line 62
+              tempCoeffC: 2.0092461335894591e-07 # cs_config_mgmt.c - Line 63
             }
           }
         ]
