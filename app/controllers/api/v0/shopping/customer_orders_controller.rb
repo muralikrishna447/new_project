032a7cf02c7c3757143f156f1d4f57cb1d97fdd9
Huy Nguyen <hueezer@gmail.com>
2016-09-07 15:54:10 -0700

    [update orders] update proxy and dry up controller

--- app/controllers/api/v0/shopping/customer_orders_controller.rb
+++ app/controllers/api/v0/shopping/customer_orders_controller.rb
@@ -4,12 +4,12 @@ module Api
       class CustomerOrdersController < BaseController
 
         before_filter :ensure_authorized_or_anonymous
-        before_filter :load_user
 
         def show
+          puts "CURRENT API USER DUDE: #{current_api_user.inspect}"
           begin
             @order = ShopifyAPI::Order.find(params[:id])
-            if @order.email == @user.email || @user.role == 'admin'
+            if current_api_user && (@order.email == current_api_user.email || current_api_user.role == 'admin')
               response = {
                 id: @order.id,
                 shipping_address: @order.shipping_address
@@ -30,16 +30,6 @@ module Api
           puts "CUSTOMER ORDER shipping_address: #{shipping_address}"
           # TODO Actually create the record
           render_api_response 200, {message: "Successfully created shipping address for Order Id #{order_id}", order_id: order_id, shipping_address: shipping_address}
-        end
-
-        private
-
-        def load_user
-          if @user_id_from_token
-            @user = User.find @user_id_from_token
-          else
-            @user = nil
-          end
         end
 
       end
