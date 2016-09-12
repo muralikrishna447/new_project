7c10fb97d93c4131456760a61e50cf0af2461d02
Huy Nguyen <hueezer@gmail.com>
2016-09-12 11:56:19 -0700

    [allowOutOfStock] update page serializer

--- app/serializers/api/page_serializer.rb
+++ app/serializers/api/page_serializer.rb
@@ -1,6 +1,6 @@
 class Api::PageSerializer < ApplicationSerializer
   format_keys :lower_camel
-  attributes :id, :title, :short_description
+  attributes :id, :title, :short_description, :is_promotion, :discount_id, :redirect_path
   has_many :components, serializer: Api::ComponentSerializer
 
 end
