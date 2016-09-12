6c9336de36378e33dad93e9e595485cd8a8de7c9
Jeremy Shaffer <jshaffer@chefsteps.com>
2016-09-12 16:06:27 -0700

    Add logging

--- lib/shipwire/order.rb
+++ lib/shipwire/order.rb
@@ -46,31 +46,45 @@ module Shipwire
     end
 
     def sync_to_shopify(shopify_order)
+      Rails.logger.info "Starting sync for Shopify order id #{shopify_order.id} and Shipwire order id #{id}"
       validate_sync(shopify_order)
 
       # For an existing fulfillment, first we sync the trackings and then save.
       # Then we complete the fulfillment in Shopify if Shipwire says it's done.
       shopify_fulfillment = joule_fulfillment(shopify_order)
       if shopify_fulfillment
+        Rails.logger.info "Found existing Joule fulfillment with id #{shopify_fulfillment.id} for Shopify order with id #{shopify_order.id}, will update it"
         sync_trackings_to_shopify(shopify_fulfillment)
         shopify_fulfillment.save
-        shopify_fulfillment.complete if fulfillment_complete?
+        if fulfillment_complete?
+          Rails.logger.info "Completing fulfillment for Shopify order with id #{shopify_order.id}, Shipwire status for order with id #{id} is #{status}"
+          shopify_fulfillment.complete
+        else
+          Rails.logger.info "Fulfillment is not complete for Shopify order with id #{shopify_order.id}, Shipwire status for order with id #{id} is #{status}"
+        end
       else
+        line_item = joule_line_item(shopify_order)
+        Rails.logger.info "No Joule fulfillment exists for Shopify order with id #{shopify_order.id}, will create one for line item with id #{line_item.id}"
         # For a new fulfillment, we create a new object and save it to Shopify.
         shopify_fulfillment = ShopifyAPI::Fulfillment.new
         shopify_fulfillment.prefix_options[:order_id] = shopify_order.id
-        shopify_fulfillment.attributes[:line_items] = [{ id: joule_line_item(shopify_order).id }]
+        shopify_fulfillment.attributes[:line_items] = [{ id: line_item.id }]
         sync_trackings_to_shopify(shopify_fulfillment)
         if fulfillment_complete?
+          Rails.logger.info "Setting new fulfillment status to success for Shopify order with id #{shopify_order.id}, Shipwire status for order with id #{id} is #{status}"
           shopify_fulfillment.attributes[:status] = 'success'
         elsif fulfillment_pending? || fulfillment_held?
+          Rails.logger.info "Setting new fulfillment status to open for Shopify order with id #{shopify_order.id}, Shipwire status for order with id #{id} is #{status}"
           shopify_fulfillment.attributes[:status] = 'open'
         end
         shopify_fulfillment.save
       end
 
       # Lastly we add tags to the Shopify order if Shipwire held it.
-      sync_held_state_to_shopify(shopify_order) if fulfillment_held?
+      if fulfillment_held?
+        Rails.logger.info "Shopify order with id #{shopify_order.id} has a hold in Shipwire order with id #{id}"
+        sync_held_state_to_shopify(shopify_order)
+      end
     end
 
     private_class_method
@@ -135,6 +149,7 @@ module Shipwire
         fulfillment.tracking_urls << tracking.url
       end
       fulfillment.attributes[:carrier] = shopify_carrier
+      Rails.logger.info "Setting tracking for Shopify fulfillment with id #{fulfillment.id} with carrier #{shopify_carrier}, tracking numbers #{fulfillment.tracking_numbers.inspect}, tracking URLs #{fulfillment.tracking_urls.inspect}"
     end
 
     def sync_held_state_to_shopify(shopify_order)
@@ -142,6 +157,7 @@ module Shipwire
       holds.each do |hold|
         hold_tags << "shipwire-held-#{hold.type}-#{hold.sub_type}"
       end
+      Rails.logger.info "Will update hold tags for Shopify order with id #{shopify_order.id} to include #{hold_tags.inspect}"
       shopify_order.save if Shopify::Utils.add_to_order_tags(shopify_order, hold_tags)
     end
 
