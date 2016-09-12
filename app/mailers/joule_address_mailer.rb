42e84450cde84ec5b29ea53c7a6f468c1e0cd2c9
lukeclum <clum.luke@gmail.com>
2016-09-12 12:25:05 -0700

    added order name to email

--- app/mailers/joule_address_mailer.rb
+++ app/mailers/joule_address_mailer.rb
@@ -3,15 +3,15 @@ class JouleAddressMailer < ActionMailer::Base
   default reply_to: "info@chefsteps.com"
 
   def prepare(order)
-    logger.info("Preparing joule address change mail for user [#{user_email}]")
-
     user_email = order.email
+    logger.info("Preparing joule address change mail for user [#{user_email}]")
     order_id = order.id
+    order_name = order.name
     shipping_address = order.shipping_address
     user_name = shipping_address.name
     subject = "Important: Verify Your Shipping Address For Joule"
     base_url = Rails.application.config.shared_config[:chefsteps_endpoint]
-    
+
     substitutions = {
       sub: {
         "*|SUBJECT|*" => [subject],
@@ -23,13 +23,14 @@ class JouleAddressMailer < ActionMailer::Base
         "*|PROVINCE|*" => [shipping_address.province],
         "*|ZIP|*" => [shipping_address.zip],
         "*|ORDER_ID|*" => [order_id],
+        "*|ORDER_NAME|*" => [order_name],
         "*|UPDATE_URL|*" => [base_url + "/orders/#{order_id}/update-address"],
         "*|CONFIRM_URL|*" => [base_url + "/orders/#{order_id}/address-confirmed"]
       }
     }
 
     headers['X-SMTPAPI'] = substitutions.to_json
-    headers['X-IDEMPOTENCY'] = "Important: Verify Your Shipping Address For Joule"
+    headers['X-IDEMPOTENCY'] = "Important: Verify Your Shipping Address For Joule #{order_id}"
     mail(to: user_email, subject: subject)
   end
 end
