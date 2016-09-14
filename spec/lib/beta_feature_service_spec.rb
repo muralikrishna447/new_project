ce8255201d174f2d7308d16933961a9885177d2a
Kenshi Kawaguchi <kenshi@recurse.ca>
2016-09-14 16:35:25 -0700

    Helper functions, multiple users

--- spec/lib/beta_feature_service_spec.rb
+++ spec/lib/beta_feature_service_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe BetaFeatureService do
 
-  def set_user_groups(groups)
-    BetaFeature::DynamoBetaFeatureService.any_instance.stub(:get_groups_for_user) \
+  def set_user_groups(user, groups)
+    throw Error.new("Need a user") unless user
+    BetaFeature::DynamoBetaFeatureService.any_instance \
+      .stub(:get_groups_for_user) \
+      .with(user) \
       .and_return(groups)
   end
 
@@ -16,12 +19,25 @@ describe BetaFeatureService do
   end
 
   before :each do
-    @user = User.new({:name => "Bob Hope", :email => 'a@b.com', :password => '123456'})
-    @user.save!
+    # By default, return nothing
+    BetaFeature::DynamoBetaFeatureService.any_instance \
+      .stub(:get_groups_for_user) \
+      .and_return([])
+    BetaFeature::DynamoBetaFeatureService.any_instance \
+      .stub(:get_feature_group_info) \
+      .and_return([])
+    BetaFeature::DynamoBetaFeatureService.any_instance \
+      .stub(:get_feature_info) \
+      .and_return(nil)
+
+    @user1 = User.new({:name => "RZA", :email => 'rza@b.com', :password => '123456'})
+    @user1.save!
+    @user2 = User.new({:name => "GZA", :email => 'gza@b.com', :password => '123456'})
+    @user2.save!
   end
 
   it 'respects group rules' do
-    set_user_groups(['dev'])
+    set_user_groups(@user1, ['dev'])
     set_feature_groups(
       'dfu',
       [
@@ -29,7 +45,7 @@ describe BetaFeatureService do
         {'group_name' => 'reviewers', 'feature_name' => 'dfu', 'is_enabled' => false},
       ]
     )
-    is_enabled = BetaFeatureService.user_has_feature(@user, 'dfu')
+    is_enabled = BetaFeatureService.user_has_feature(@user1, 'dfu')
     expect(is_enabled).to eq(true)
   end
 
