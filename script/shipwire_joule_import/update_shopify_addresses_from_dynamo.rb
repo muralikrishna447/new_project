1103664f1b5a2ec16d855dedf9a2bf89d7941fcf
Jeremy Shaffer <jshaffer@chefsteps.com>
2016-09-20 17:25:15 -0700

    Don't verify addresses in this script, we already do that in the validation script

--- script/shipwire_joule_import/update_shopify_addresses_from_dynamo.rb
+++ script/shipwire_joule_import/update_shopify_addresses_from_dynamo.rb
@@ -37,18 +37,6 @@ option_parser = OptionParser.new do |option|
     options[:store] = store
   end
 
-  option.on('-v', '--verify-addresses') do
-    options[:verify_addresses] = true
-  end
-
-  option.on('-l', '--lob-key LOB_KEY') do |lob_key|
-    options[:lob_key] = lob_key
-  end
-
-  option.on('-e', '--error-output ERROR_OUTPUT_FILE', 'Error output file') do |error_output|
-    options[:error_output] = error_output
-  end
-
   option.on('-d', '--dry-run', 'Do a dry run and do not update Shopify') do
     options[:dry_run] = true
   end
@@ -65,39 +53,11 @@ unless options[:dry_run]
   raise '--key is required' unless options[:key]
   raise '--password is required' unless options[:password]
   raise '--store is required' unless options[:store]
-  STDERR.puts "NOTE: --dry-run was specified, not really updating Shopify"
+  STDERR.puts 'NOTE: --dry-run was specified, not really updating Shopify'
 end
 
 # Configure shopify client
 ShopifyAPI::Base.site = "https://#{options[:api_key]}:#{options[:password]}@#{options[:store]}.myshopify.com/admin"
-
-# Configure Lob address verification client
-if options[:verify_addresses]
-  Lob.api_key = options[:lob_key]
-  @lob = Lob.load
-end
-
-def address_verifies?(params)
-  begin
-    address = {
-      address_line1: params[:address_1],
-      address_city: params[:city],
-      address_state: params[:province],
-      address_zip: params[:zip],
-      address_country: 'US'
-    }
-    if params['address_2'] && params['address_2'] != 'true'
-      address[:address_line2] = params['address_2']
-    end
-
-    @lob.addresses.verify(address)
-  rescue Lob::InvalidRequestError => e
-    error = JSON.parse(e.json_body)
-    return false if error.fetch('error').fetch('message') == 'address not found'
-    raise "Unexpected error from address verification API: #{e.inspect}"
-  end
-  true
-end
 
 CSV.foreach(options[:input_file], headers: true) do |input_row|
   order_id = input_row['orderId (S)']
@@ -108,20 +68,6 @@ CSV.foreach(options[:input_file], headers: true) do |input_row|
   zip = input_row['zip (S)']
 
   STDERR.puts "Updading order with id #{order_id} with data #{input_row}"
-
-  if options[:verify_addresses]
-    if address_verifies?(
-      address_1: address_1,
-      address_2: address_2,
-      city: city,
-      province: province,
-      zip: zip
-    )
-      STDERR.puts "Address is valid: #{input_row}"
-    else
-      STDERR.puts "Address is not valid: #{input_row}"
-    end
-  end
 
   unless options[:dry_run]
     order = ShopifyAPI::Order.find(order_id)
