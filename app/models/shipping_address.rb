59b4c8aa75e3568fdc88207a55a399385279f4cc
Huy Nguyen <hueezer@gmail.com>
2016-09-28 23:08:02 -0700

    [update address] update Shopify directly

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,24 +3,14 @@ class ShippingAddress < MailingAddress
   @@client = Aws::DynamoDB::Client.new(region: 'us-east-1')
 
   def save_record(order_id,user_id)
-    item = {
-      'orderId' => order_id,
-      'userId' => user_id,
-      'address1' => address1,
-      'address2' => address2,
-      'city' => city,
-      'province' => province,
-      'zip' => zip,
-      'createdAt' => DateTime.now.to_i
-    }
     begin
-      if !Rails.env.test?
-        @@client.put_item(
-        {
-          table_name: Rails.configuration.dynamodb.shipping_address_update_table_name,
-          item: item
-        })
-      end
+      order = ShopifyAPI::Order.find(order_id)
+      order.shipping_address.address1 = address1
+      order.shipping_address.address2 = address2
+      order.shipping_address.city = city
+      order.shipping_address.province_code = province
+      order.shipping_address.zip = zip
+      order.save
     rescue => e
       Rails.logger.error "ShippingAddress update error: #{e} while trying to save item: #{item}"
       raise Exception.new("Error saving ShippingAddress")
