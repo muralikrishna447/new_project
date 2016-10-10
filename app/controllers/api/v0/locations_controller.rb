0ad6c088965ce3d4696af2a9b0b35459b100e1a3
Kenshi Kawaguchi <kenshi@recurse.ca>
2016-10-10 14:35:52 -0700

    readd tax code

--- app/controllers/api/v0/locations_controller.rb
+++ app/controllers/api/v0/locations_controller.rb
@@ -3,9 +3,34 @@ module Api
     class LocationsController < BaseController
       def index
         location = geolocate_ip()
-        result = location.merge('taxPercent' => nil)
+        if sales_tax_states.include?(location[:state])
+          tax_percent = get_tax_estimate(location)
+        end
+        result = location.merge('taxPercent' => tax_percent)
         render(json: result)
       end
+
+      protected
+      def get_tax_estimate(location)
+        #Null for no geocode and 0 for no tax
+        tax_service = AvaTax::TaxService.new
+        if location[:latitude] && location[:longitude]
+          begin
+            geo_tax_result = tax_service.estimate({latitude: location[:latitude], longitude: location[:longitude]}, 100)
+            geo_tax_result["Rate"]
+          rescue => e
+            Rails.logger.error("Failed to calculate tax - #{e.response}")
+            nil
+          end
+        else
+          nil
+        end
+      end
+
+      def sales_tax_states
+        ["WA"]
+      end
+
     end
   end
 end
