ba12c6404e791bd1a88fc30c6a8b2902dd53ed8a
Huy Nguyen <hueezer@gmail.com>
2016-10-28 18:51:35 -0700

    remove any premium

--- spec/controllers/api/v0/shopping/products_controller_spec.rb
+++ spec/controllers/api/v0/shopping/products_controller_spec.rb
@@ -40,57 +40,11 @@ describe Api::V0::Shopping::ProductsController do
       products = JSON.parse(response.body)
       products.length.should eq(4)
     end
-
-    it "should respond with an array of products when a user is not premium" do
-      sign_in @non_premium_user
-      controller.request.env['HTTP_AUTHORIZATION'] = @non_premium_user.valid_website_auth_token.to_jwt
-      get :index
-      response.should be_success
-      products = JSON.parse(response.body)
-      products.length.should eq(4)
-    end
-
-    it "should respond with an array of products when a user is premium" do
-      sign_in @premium_user
-      controller.request.env['HTTP_AUTHORIZATION'] = @premium_user.valid_website_auth_token.to_jwt
-      get :index
-      response.should be_success
-      products = JSON.parse(response.body)
-      products.length.should eq(4)
-    end
   end
 
   describe 'private methods' do
     before :each do
       @controller = Api::V0::Shopping::ProductsController.new
-    end
-
-    context 'get_product_discount' do
-      it "should return a discount for a product" do
-        product = ShopifyAPI::Product.find(123)
-        product_discount = @controller.instance_eval { get_product_discount(product) }
-        expect(product_discount).to eq(800)
-      end
-    end
-
-    context 'get_price' do
-      it "should return the correct price for a none premium user" do
-        product = ShopifyAPI::Product.find(567)
-        price = @controller.instance_eval { get_price(product, false, false) }
-        expect(price).to eq(22900)
-      end
-
-      it "should return the correct price for a premium user where used_circulator_discount is false" do
-        product = ShopifyAPI::Product.find(567)
-        price = @controller.instance_eval { get_price(product, true, false) }
-        expect(price).to eq(22900)
-      end
-
-      it "should return the correct price for a premium user where used_circulator_discount is true" do
-        product = ShopifyAPI::Product.find(567)
-        price = @controller.instance_eval { get_price(product, true, true) }
-        expect(price).to eq(22900)
-      end
     end
 
     context 'get_product_sku' do
@@ -121,55 +75,12 @@ describe Api::V0::Shopping::ProductsController do
       # product['title'].should eq('Product1')
     end
 
-    it "should respond with the product price" do
-      get :show, id: 'cs123'
-      response.should be_success
-      product = JSON.parse(response.body)
-      expect(product.has_key?('price')).to eq(true)
-    end
-
     it "should properly handle cases where a sku cannot be found" do
       get :show, id: 'csNoSku'
       response.should be_success
       responseBody = JSON.parse(response.body)
       responseBody['message'].should eq('No product found for sku.')
     end
-
-    it "should show the correct price when user is not logged in" do
-      get :show, id: 'cs123'
-      response.should be_success
-      product = JSON.parse(response.body)
-      expect(product['price']).to eq(10.99)
-    end
-
-    it "should show the correct price when user is not premium" do
-      sign_in @non_premium_user
-      controller.request.env['HTTP_AUTHORIZATION'] = @non_premium_user.valid_website_auth_token.to_jwt
-      get :show, {id: 'cs123'}, {'HTTP_AUTHORIZATION' => @non_premium_user.valid_website_auth_token.to_jwt}
-      response.should be_success
-      product = JSON.parse(response.body)
-      expect(product['price']).to eq(10.99)
-    end
-
-    it "should show the correct price when user is premium" do
-      sign_in @premium_user
-      controller.request.env['HTTP_AUTHORIZATION'] = @premium_user.valid_website_auth_token.to_jwt
-      get :show, {id: 'cs10001'}, {'HTTP_AUTHORIZATION' => @premium_user.valid_website_auth_token.to_jwt}
-      response.should be_success
-      product = JSON.parse(response.body)
-      expect(product['price']).to eq(229.00)
-    end
-
-    it "should show the correct price when user is premium and has already used the circulator discount" do
-      sign_in @premium_user_used_discount
-      controller.request.env['HTTP_AUTHORIZATION'] = @premium_user_used_discount.valid_website_auth_token.to_jwt
-      get :show, {id: 'cs10001'}, {'HTTP_AUTHORIZATION' => @premium_user_used_discount.valid_website_auth_token.to_jwt}
-      response.should be_success
-      product = JSON.parse(response.body)
-      expect(product['price']).to eq(229.00)
-    end
-
-
 
   end
 end
