40bd779e8bfc698aee203dcb2450faa5b552783c
Kenshi Kawaguchi <kenshi@recurse.ca>
2016-11-08 16:13:49 -0800

    add simple script for thermoworks export

---
+++ script/shipping/export_thermoworks_orders.rb
@@ -0,0 +1,38 @@
+require 'csv'
+require 'optparse'
+require 'shopify_api'
+require 'logger'
+require_relative './rails_shim'
+
+options = {}
+option_parser = OptionParser.new do |option|
+  option.on('-k', '--key API_KEY', 'Shopify API key') do |api_key|
+    options[:api_key] = api_key
+  end
+
+  option.on('-p', '--password PASSWORD', 'Shopify password') do |password|
+    options[:password] = password
+  end
+
+  option.on('-s', '--store STORE', 'Shopify store name') do |store|
+    options[:store] = store
+  end
+end
+option_parser.parse!
+options[:api_key] ||= 'f3c79828c0f50b04866481389eacb2d2'
+options[:password] ||= '5553e01d45c426ced082e9846ad56eee'
+options[:store] ||= 'chefsteps-staging'
+
+raise '--key is required' unless options[:api_key]
+raise '--password is required' unless options[:password]
+raise '--store is required' unless options[:store]
+ShopifyAPI::Base.site = "https://#{options[:api_key]}:#{options[:password]}@#{options[:store]}.myshopify.com/admin"
+puts ShopifyAPI::Base.site
+
+params = {
+  open_fulfillment: false,
+  storage: 'file',
+  quantity: 10
+}
+
+Fulfillment::ThermoworksOrderExporter.perform(params)
