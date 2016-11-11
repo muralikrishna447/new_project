430395935bb766a52a84ca39284297123d18e359
Jeremy Shaffer <jshaffer@chefsteps.com>
2016-11-10 21:38:55 -0800

    Add V2 import/export for ShipStation

---
+++ app/workers/fulfillment/shipstation_order_exporter.rb
@@ -0,0 +1,76 @@
+require 'csv'
+
+module Fulfillment
+  class ShipstationOrderExporter
+    include Fulfillment::CSVOrderExporter
+
+    @queue = :ShipstationOrderExporter
+
+    def self.type
+      'orders'
+    end
+
+    def self.job_params(params)
+      job_params = params.merge(skus: ['cs10001'])
+      job_params[:storage] ||= 'stdout'
+      job_params
+    end
+
+    def self.schema
+      [
+        'id',
+        'name',
+        'processed_at',
+        'shipping_name',
+        'shipping_company',
+        'shipping_address_1',
+        'shipping_address_2',
+        'shipping_city',
+        'shipping_province',
+        'shipping_zip',
+        'shipping_country',
+        'email',
+        'shipping_phone',
+        'sku',
+        'quantity',
+        'tags'
+      ]
+    end
+
+    def self.transform(fulfillable)
+      line_items = []
+      fulfillable.line_items.each do |line_item|
+        Rails.logger.info("ShipStation order export adding order with id #{fulfillable.order.id} and line item with id #{line_item.id} and quantity #{line_item.quantity}")
+        line_items <<
+          [
+            fulfillable.order.id,
+            fulfillable.order.name,
+            fulfillable.order.processed_at,
+            fulfillable.order.shipping_address.name,
+            fulfillable.order.shipping_address.company,
+            fulfillable.order.shipping_address.address1,
+            fulfillable.order.shipping_address.address2,
+            fulfillable.order.shipping_address.city,
+            fulfillable.order.shipping_address.province_code,
+            fulfillable.order.shipping_address.zip,
+            fulfillable.order.shipping_address.country_code,
+            fulfillable.order.email,
+            fulfillable.order.shipping_address.phone,
+            line_item.sku,
+            line_item.quantity,
+            fulfillable.order.tags
+          ]
+      end
+      line_items
+    end
+
+    def self.include_order?(order)
+      # Filter out any order that has filtered tags
+      unless (Shopify::Utils.order_tags(order) & FILTERED_TAGS).empty?
+        Rails.logger.info("ShipStation order export filtering order with id #{order.id} because it has one or more filtered tags: #{order.tags}")
+        return false
+      end
+      true
+    end
+  end
+end
---
+++ script/shipping/v2_open_fulfillments_shipstation.rb
@@ -0,0 +1,52 @@
+require './rails_shim'
+
+#
+# Exports Shopify Joule orders using the API to a CSV on standard output.
+# Options:
+#   --key: Your Shopify API key
+#   --password: Your Shopify API key password
+#   --store: The name of the Shopify store ('delve' for prod, or 'chefsteps-staging')
+#
+
+# Options parsing
+options = {}
+option_parser = OptionParser.new do |option|
+  option.on('-k', '--key API_KEY', 'Shopify API key') do |api_key|
+    options[:api_key] = api_key
+  end
+
+  option.on('-p', '--password PASSWORD', 'Shopify password') do |password|
+    options[:password] = password
+  end
+
+  option.on('-s', '--store STORE', 'Shopify store name') do |store|
+    options[:store] = store
+  end
+
+  option.on('-o', '--open-fulfillment', 'Open fulfillment') do
+    options[:open_fulfillment] = true
+  end
+
+  option.on('-i', '--input INPUT', 'Input file') do |input|
+    options[:input] = input
+  end
+end
+option_parser.parse!
+raise '--key is required' unless options[:api_key]
+raise '--password is required' unless options[:password]
+raise '--store is required' unless options[:store]
+
+# Configure shopify client
+ShopifyAPI::Base.site = "https://#{options[:api_key]}:#{options[:password]}@#{options[:store]}.myshopify.com/admin"
+
+orders = []
+CSV.foreach(options[:input], headers: true) do |input_row|
+  order = ShopifyAPI::Order.find(input_row['id'])
+  raise "No order for id input_row['id']" unless order
+  orders << order
+end
+
+fulfillables = Fulfillment::ShipstationOrderExporter.fulfillables(orders, ['cs10001'])
+if options[:open_fulfillment]
+  Fulfillment::ShipstationOrderExporter.open_fulfillments(fulfillables)
+end
