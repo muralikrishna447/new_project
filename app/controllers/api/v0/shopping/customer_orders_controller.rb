0e08f830006b27405088eac6869fa53101a3e29d
Huy Nguyen <hueezer@gmail.com>
2016-11-21 15:25:41 -0800

    [shipping] bugfix

--- app/controllers/api/v0/shopping/customer_orders_controller.rb
+++ app/controllers/api/v0/shopping/customer_orders_controller.rb
@@ -8,6 +8,7 @@ module Api
         def show
           begin
             @order = ShopifyAPI::Order.find(params[:id])
+            puts "ORDER FOUND: #{@order}"
             customer_multipass_identifier = @order.customer.multipass_identifier
             current_api_user_id = current_api_user.id.to_s
             current_api_user_role = current_api_user.role
@@ -16,7 +17,7 @@ module Api
               response = {
                 id: @order.id,
                 fulfillment_status: @order.fulfillment_status,
-                shipping_address: @order.shipping_address,
+                shipping_address: @order.method_defined?(:shipping_address) ? @order.shipping_address : nil,
                 shipping_address_updatable: shipping_address_updatable[:updatable],
                 update_message: shipping_address_updatable[:update_message]
               }
@@ -24,7 +25,8 @@ module Api
             else
               render_api_response(401, {message: 'Unauthorized'})
             end
-          rescue
+          rescue Exception => e
+            puts "EXCEPTION: #{e}"
             render_api_response(404, {message: 'Order not found'})
           end
         end
