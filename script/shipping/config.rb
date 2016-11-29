1eae623763616795f85a50c975ec5fb2e54b2024
Kenshi Kawaguchi <kenshi@recurse.ca>
2016-11-29 13:51:47 -0800

    extract config

---
+++ script/shipping/config.rb
@@ -0,0 +1,20 @@
+
+def get_config(env)
+  conf = {
+    'staging' => {
+      shopify_key: ENV['SHOPIFY_API_KEY'],
+      shopify_pass: ENV['SHOPIFY_PASSWORD'],
+      shopify_store: 'chefsteps-staging',
+    }
+  }[env]
+  raise "No config for environment: #{env}" unless conf
+  return conf
+end
+
+def setup_shopify(conf)
+  raise "No API key provided" unless conf[:shopify_key]
+  raise "No API password provided" unless conf[:shopify_pass]
+  raise "No Shopify store provided" unless conf[:shopify_store]
+  ShopifyAPI::Base.site = "https://#{conf[:shopify_key]}:#{conf[:shopify_pass]}" \
+                          "@#{conf[:shopify_store]}.myshopify.com/admin"
+end
--- script/shipping/thermoworks_import_shipments.rb
+++ script/shipping/thermoworks_import_shipments.rb
@@ -2,26 +2,7 @@ require 'csv'
 require 'optparse'
 require 'shopify_api'
 require_relative './rails_shim'
-
-def get_config(env)
-  conf = {
-    'staging' => {
-      shopify_key: ENV['SHOPIFY_API_KEY'],
-      shopify_pass: ENV['SHOPIFY_PASSWORD'],
-      shopify_store: 'chefsteps-staging',
-    }
-  }[env]
-  raise "No config for environment: #{env}" unless conf
-  return conf
-end
-
-def setup_shopify(conf)
-  raise "No API key provided" unless conf[:shopify_key]
-  raise "No API password provided" unless conf[:shopify_pass]
-  raise "No Shopify store provided" unless conf[:shopify_store]
-  ShopifyAPI::Base.site = "https://#{conf[:shopify_key]}:#{conf[:shopify_pass]}" \
-                          "@#{conf[:shopify_store]}.myshopify.com/admin"
-end
+require_relative './config'
 
 options = {}
 option_parser = OptionParser.new do |option|
