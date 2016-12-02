3df6b30df8d41c126e4dc3f4ab14447125ec8243
Huy Nguyen <hueezer@gmail.com>
2016-12-02 09:40:47 -0800

    [random drop] add env in table name

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -16,7 +16,7 @@ Rails.configuration.dynamodb.beta_features_table_config = {
 Rails.configuration.dynamodb.shipping_address_update_table_name = "shipping-address-updates-#{Rails.env}"
 Rails.configuration.dynamodb.shipping_address_confirmation_table_name = "shipping-address-confirmations-#{Rails.env}"
 
-Rails.configuration.dynamodb.random_drop_table_name = "random-drop-winners"
+Rails.configuration.dynamodb.random_drop_table_name = "random-drop-winners-#{Rails.env}"
 
 if Rails.env.production?
   Rails.configuration.remote_log_bucket = 'remote-logs-production'
