a67e9b2d24075d65e60144dfb1c0c0fada48940b
Huy Nguyen <hueezer@gmail.com>
2016-12-02 14:10:13 -0800

    [random drop] query users instead of get

--- app/controllers/api/v0/random_drops_controller.rb
+++ app/controllers/api/v0/random_drops_controller.rb
@@ -5,7 +5,7 @@ module Api
       before_filter :ensure_authorized, only: [:show]
 
       def show
-        if params[:id] == @user_id_from_token.to_s
+        if (params[:id] == @user_id_from_token.to_s) || current_user.role == 'admin'
           @random_drop = RandomDrop.get(params[:id])
           render_api_response 200, @random_drop
         else
--- app/models/random_drop.rb
+++ app/models/random_drop.rb
@@ -5,16 +5,25 @@ class RandomDrop
   def self.get(user_id)
     begin
       if !Rails.env.test?
+        response = @@client.query(
+          table_name: Rails.configuration.dynamodb.random_drop_table_name,
+          select: "ALL_ATTRIBUTES",
+          key_condition_expression: 'user_id = :user_id',
+          expression_attribute_values: {
+            ':user_id' => user_id.to_s
+          }
+        )
+        puts "RESPONSE: #{response.inspect}"
+        items = response.items
+          .map {|item| Hash[item.each_pair.to_a]}
+          .map {|item|
+            created_at = item['created_at']
+            item['created_at_int'] = Time.parse(created_at).to_i
+            item
+          }
+          .sort {|item| item['created_at_int']}
 
-        itemStruct = @@client.get_item(
-          {
-            table_name: Rails.configuration.dynamodb.random_drop_table_name,
-            key: {
-              'user_id' => user_id.to_s
-            }
-          })
-
-        @item = Hash[itemStruct.each_pair.to_a]
+        return items.last
       end
     rescue => e
       Rails.logger.error "Error: #{e} while trying to get random drop for user id: #{user_id}"
