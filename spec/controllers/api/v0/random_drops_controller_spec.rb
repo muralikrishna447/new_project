3cb1d4ff1af6418b025b02516c9227ef69530902
Huy Nguyen <hueezer@gmail.com>
2016-12-01 23:18:00 -0800

    [random drop] update spec

--- spec/controllers/api/v0/random_drops_controller_spec.rb
+++ spec/controllers/api/v0/random_drops_controller_spec.rb
@@ -4,6 +4,12 @@ describe Api::V0::RandomDropsController do
     @user = Fabricate :user, name: 'User Random Drop', email: 'user@random.drop'
     sign_in @user
     controller.request.env['HTTP_AUTHORIZATION'] = @user.valid_website_auth_token.to_jwt
+    RandomDrop.stub(:get).and_return({
+      'user_id' => @user.id,
+      'discount_code' => 'RANDOMDISCOUNT',
+      'variant_id' => 'VARIANTID',
+      'price' => '49'
+    })
   end
 
   # GET /api/v0/random_drops/:id
@@ -14,6 +20,7 @@ describe Api::V0::RandomDropsController do
     parsed['user_id'].should eq(1)
     parsed['discount_code'].should eq('RANDOMDISCOUNT')
     parsed['variant_id'].should eq('VARIANTID')
+
   end
 
 end
