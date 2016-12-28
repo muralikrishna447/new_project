c2c8428fa50dc8ff3a5325a661a027024835a3c1
John Rivera <jrivera@chefsteps.com>
2016-12-28 09:22:21 -0800

    Revise Serializers

--- app/serializers/api/cook_history_item_serializer.rb
+++ app/serializers/api/cook_history_item_serializer.rb
@@ -1,6 +1,8 @@
 class Api::CookHistoryItemSerializer < ApplicationSerializer
   format_keys :lower_camel
-  attributes :program_type, :user_content_id, :user_id
-  has_one :joule_cook_history_program, serializer: Api::JouleCookHistoryProgramSerializer
+  attributes :id, :program_type, :user_content_id, :user_id
+  has_one :joule_cook_history_program,
+    serializer: Api::JouleCookHistoryProgramSerializer,
+    :dependent => :destroy
   
 end
--- app/serializers/api/joule_cook_history_program_serializer.rb
+++ app/serializers/api/joule_cook_history_program_serializer.rb
@@ -1,10 +1,24 @@
 class Api::JouleCookHistoryProgramSerializer < ApplicationSerializer
   format_keys :lower_camel
-  attributes :cook_time, :guide_id, :holding_temperature, :program_type, 
-  :set_point, :timer_id, :cook_id, :delayed_start, :wait_for_preheat, 
-  :turbo, :predictive
+  attributes :cook_time, :guide, :holding_temperature, :program_type, 
+  :set_point, :cook_id, :delayed_start, :wait_for_preheat, 
+  :turbo, :predictive, :created_at, :program_metadata
+  
+  belongs_to :cook_history_item
   
   def created_at
     object.created_at.strftime("%s").to_i
   end
+  
+  def guide
+    object.guide_id
+  end
+  
+  def program_metadata
+    {
+      guideId: object.guide_id,
+      cookId: object.cook_id,
+    }
+  end
+  
 end
