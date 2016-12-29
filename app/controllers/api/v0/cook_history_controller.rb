b98f4857fc268ef56ee76a0317adb7dc34e86faa
John Rivera <jrivera@chefsteps.com>
2016-12-28 16:13:52 -0800

    switch to render_api_response and improve case handling

--- app/controllers/api/v0/cook_history_controller.rb
+++ app/controllers/api/v0/cook_history_controller.rb
@@ -5,20 +5,26 @@ module Api
 
       def index
         user = User.find(@user_id_from_token)
-        cook_history_items = user.cook_history_items.includes(:joule_cook_history_program)
-        render json: cook_history_items, each_serializer: Api::CookHistoryItemSerializer
+        cook_history_items = user.cook_history_items.includes(:joule_cook_history_program).first(20)
+        render_api_response 200, cook_history_items, Api::CookHistoryItemSerializer
       end
       
       def create
         user = User.find(@user_id_from_token)
         cook_history_item = user.cook_history_items.new(params[:cook_history])
-        render nothing: true, status: 200 if cook_history_item.save
+        if cook_history_item.save
+          render_api_response 200, cook_history_item, Api::CookHistoryItemSerializer
+        end
       end
       
       def destroy
         user = User.find(@user_id_from_token)
         item = user.cook_history_items.find(params[:id])
-        render nothing: true, status: 200 if item.destroy
+        if item && item.destroy
+          render_api_response 200, { message: "Successfully destroyed #{params[:id]}" }
+        else
+          render_api_response 404, { message: "Item not found" }
+        end
       end
       
     end
