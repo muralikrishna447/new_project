70d55889b8017e00cb609859f71327b6963dd8f0
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-01-04 15:30:57 -0800

    Add validation for tracking number format

--- app/workers/fulfillment/rosti_shipment_importer.rb
+++ app/workers/fulfillment/rosti_shipment_importer.rb
@@ -83,7 +83,11 @@ module Fulfillment
       end
 
       unless row[TRACKING_NUMBER_COLUMN] && !row[TRACKING_NUMBER_COLUMN].empty?
-        raise "Tracking number column is invalid: #{row['CRN']}"
+        raise "Tracking number column is invalid: #{row[TRACKING_NUMBER_COLUMN]}"
+      end
+
+      unless row[TRACKING_NUMBER_COLUMN] =~ /^[0-9]{12,14}$/
+        raise "Tracking number column is invalid: #{row[TRACKING_NUMBER_COLUMN]}"
       end
     end
   end
