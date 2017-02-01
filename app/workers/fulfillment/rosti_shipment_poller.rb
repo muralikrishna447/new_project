ffdf2660e7f22dd46dc237d78be319b161f5618b
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-02-01 15:07:13 -0800

    Add retries for AWS calls in shipment poller

--- app/workers/fulfillment/rosti_shipment_poller.rb
+++ app/workers/fulfillment/rosti_shipment_poller.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'retriable'
 
 module Fulfillment
   class RostiShipmentPoller
@@ -29,7 +30,10 @@ module Fulfillment
       job_params = params.deep_symbolize_keys
 
       sqs = sqs_client
-      result = sqs.receive_message(queue_url: sqs_url)
+      result = nil
+      Retriable.retriable tries: 3 do
+        result = sqs.receive_message(queue_url: sqs_url)
+      end
 
       if result.messages.empty?
         Rails.logger.info('RostiShipmentPoller no messages received')
@@ -42,10 +46,12 @@ module Fulfillment
         sns_message.fetch('Records').each do |s3_record|
           process_s3_record(s3_record, job_params)
         end
-        sqs.delete_message(
-          queue_url: sqs_url,
-          receipt_handle: received_message.receipt_handle
-        )
+        Retriable.retriable tries: 3 do
+          sqs.delete_message(
+            queue_url: sqs_url,
+            receipt_handle: received_message.receipt_handle
+          )
+        end
       end
 
       Librato.increment 'fulfillment.rosti.shipment-poller.success', sporadic: true
@@ -69,7 +75,11 @@ module Fulfillment
       # file still exists in case Rosti added the file then deleted it
       # shortly after. (It happens!)
       s3 = s3_client
-      unless s3.bucket(bucket).object(key).exists?
+      exists = false
+      Retriable.retriable tries: 3 do
+        exists = s3.bucket(bucket).object(key).exists?
+      end
+      unless exists
         Rails.logger.info("RostiShipmentPoller S3 key does not exist, skipping: #{bucket}:#{key}")
         return
       end
@@ -86,7 +96,9 @@ module Fulfillment
       # archival location and the buckets are versioned to maintain history.
       # We delete objects after processing so it's easy to see what files
       # have completed processing versus pending.
-      s3.bucket(bucket).object(key).delete
+      Retriable.retriable tries: 3 do
+        s3.bucket(bucket).object(key).delete
+      end
       Rails.logger.info("RostiShipmentPoller deleted S3 object #{bucket}:#{key}")
     end
 
