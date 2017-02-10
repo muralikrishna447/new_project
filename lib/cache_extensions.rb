769cc478c443b821f1e2fc6f0bf9640d3759d83f
Kenshi Kawaguchi <kenshi@recurse.ca>
2017-02-10 14:30:40 -0800

    only cache CacheFetchErrors, so we don't ignore unexpected errors

--- lib/cache_extensions.rb
+++ lib/cache_extensions.rb
@@ -1,15 +1,23 @@
  module CacheExtensions
   class << self
-    # This allows caching with an expiration, but if there is an exception thrown from the block that produces
-    # the cache value, it uses the old value from the cache, retrying occasionally. The trick is to
-    # not put an expiration on the cache value, but on a separate generated key, because Rails.cache.read honors
-    # expirations set earlier. I'm surprised Rails.cache.fetch doesn't just handle this.
+
+    class CacheFetchError < StandardError
+    end
+
+    # This allows caching with an expiration, but if there is an
+    # CacheFetchError thrown from the block that produces the cache
+    # value, it uses the old value from the cache, retrying
+    # occasionally. The trick is to not put an expiration on the cache
+    # value, but on a separate generated key, because Rails.cache.read
+    # honors expirations set earlier. I'm surprised Rails.cache.fetch
+    # doesn't just handle this.
     #
-    # key - the cache key to read and update if needed
-    # expiration - how long the key should be good for under normal circumstances
-    # retry_expiration - how long between retries if the block raises an exception. if expiration is long, you may want to
-    # make this shorter.
-    # block - should return the updated value, or raise an exception if it can't be retrieved
+    # key              - the cache key to read and update if needed
+    # expiration       - how long the key should be good for under normal circumstances
+    # retry_expiration - how long between retries if the block raises an exception.
+    #                    if expiration is long, you may want to make this shorter.
+    # block            - should return the updated value, or raise an exception if it
+    #                    can't be retrieved
     #
     # Inspired by https://github.com/ReliveRadio/reliveradio-website/blob/4874cf4158361c73a693e65643d9e7f11333d9d6/app/helpers/external_api_helper.rb
     def fetch_with_rescue(key, expiration, retry_expiration)
@@ -22,7 +30,7 @@
           result = yield
           Rails.cache.write(key, result)
           Rails.cache.write(freshness_key, 'fresh', expires_in: expiration)
-        rescue StandardError => error
+        rescue CacheFetchError => error
           Rails.cache.write(freshness_key, 'retrying', expires_in: retry_expiration)
           Rails.logger.error("Got error #{error} attempting to update cache #{key}. Using saved value for #{retry_expiration} additional time.")
           Librato.increment 'fetch_with_rescue.block_raised.#{key}', sporadic: true
