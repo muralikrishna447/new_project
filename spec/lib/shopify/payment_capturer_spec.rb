903c98f85fc40558aeb9e9c8ad117eb1b7fee148
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-03-02 17:02:35 -0800

    Fix bug where payment capture fails on partially paid orders

--- spec/lib/shopify/payment_capturer_spec.rb
+++ spec/lib/shopify/payment_capturer_spec.rb
@@ -53,6 +53,10 @@ describe Shopify::PaymentCapturer do
       transaction
     end
 
+    before :each do
+      capturer.stub(:build_capture_transaction).with(order).and_return(transaction)
+    end
+
     context 'payment capture raises error' do
       context 'payment was previously captured' do
         it 'checks order capture status and does not raise error' do
@@ -77,6 +81,104 @@ describe Shopify::PaymentCapturer do
       it 'saves capture transaction to shopify' do
         Shopify::Utils.should_receive(:send_assert_true).with(transaction, :save)
         capturer.capture_payment(order)
+      end
+    end
+  end
+
+  describe 'build_capture_transaction' do
+    let(:order_id) { 1234 }
+    let(:order) { ShopifyAPI::Order.new(id: 1234, financial_status: financial_status) }
+
+    context 'order has financial_status authorized' do
+      let(:financial_status) { 'authorized' }
+      it 'returns capture transaction with no amount' do
+        transaction = ShopifyAPI::Transaction.new(kind: 'capture')
+        transaction.prefix_options[:order_id] = order_id
+        expect(capturer.build_capture_transaction(order)).to eq(transaction)
+      end
+    end
+
+    context 'order has financial_status partially_paid' do
+      let(:financial_status) { 'partially_paid' }
+      let(:auth_amount_1) { 4.31 }
+      let(:auth_amount_2) { 6.45 }
+      let(:transaction_1) { ShopifyAPI::Transaction.new(amount: auth_amount_1) }
+      let(:transaction_2) { ShopifyAPI::Transaction.new(amount: auth_amount_2) }
+      let(:transactions) { [transaction_1, transaction_2] }
+
+      context 'order has successful non-gift-card authorizations' do
+        before :each do
+          order.stub(:transactions).and_return(transactions)
+          capturer.stub(:successful_cc_auth?).with(transaction_1).and_return(true)
+          capturer.stub(:successful_cc_auth?).with(transaction_2).and_return(true)
+        end
+
+        it 'returns capture transaction with sum of authorization amounts' do
+          transaction = ShopifyAPI::Transaction.new(
+            kind: 'capture',
+            amount: auth_amount_1 + auth_amount_2
+          )
+          transaction.prefix_options[:order_id] = order_id
+          expect(capturer.build_capture_transaction(order)).to eq(transaction)
+        end
+      end
+
+      context 'order has no successful non-gift-card authorizations' do
+        before :each do
+          capturer.stub(:successful_cc_auth?).with(transaction_1).and_return(false)
+          capturer.stub(:successful_cc_auth?).with(transaction_2).and_return(false)
+        end
+
+        it 'raises error' do
+          expect { capturer.build_capture_transaction(order) }.to raise_error
+        end
+      end
+    end
+  end
+
+  describe 'successful_cc_auth?' do
+    let(:transaction) do
+      ShopifyAPI::Transaction.new(
+        kind: kind,
+        gateway: gateway,
+        status: status
+      )
+    end
+
+    context 'transaction kind is not authorization' do
+      let(:kind) { 'capture' }
+      let(:gateway) { 'my_gateway' }
+      let(:status) { 'success' }
+      it 'returns false' do
+        expect(capturer.successful_cc_auth?(transaction)).to be_false
+      end
+    end
+
+    context 'transaction kind is authorization' do
+      let(:kind) { 'authorization' }
+      let(:status) { 'success' }
+      context 'gateway is gift_card' do
+        let(:gateway) { 'gift_card' }
+        it 'returns false' do
+          expect(capturer.successful_cc_auth?(transaction)).to be_false
+        end
+      end
+
+      context 'transaction gateway is not gift_card' do
+        let(:gateway) { 'my_gateway' }
+        context 'status is success' do
+          let(:status) { 'success' }
+          it 'returns true' do
+            expect(capturer.successful_cc_auth?(transaction)).to be_true
+          end
+        end
+
+        context 'transaction status is not success' do
+          let(:status) { 'failure' }
+          it 'returns false' do
+            expect(capturer.successful_cc_auth?(transaction)).to be_false
+          end
+        end
       end
     end
   end
