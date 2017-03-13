36cac2827c5d8ce4a1c17c35c1e90ca47aaf7870
tswetonic <swetonic@gmail.com>
2017-03-13 09:48:52 -0700

    catch nil user token

--- app/controllers/api/v0/content_controller.rb
+++ app/controllers/api/v0/content_controller.rb
@@ -4,11 +4,23 @@ module Api
       before_filter :ensure_authorized_or_anonymous
 
       def manifest
+        if @user_id_from_token.nil?
+          Rails.logger.error "ContentController: no user token"
+          return render_api_response 401
+        end
+
         manifest_endpoints = YAML.load_file(Rails.root.join('config', 'content_config.yml'))['manifest_endpoints']
-        response = {}
+
+        if BetaFeatureService.user_has_feature(current_api_user, 'beta_guides')
+          #always use the staging manifest for beta guides users
+          Rails.logger.info "ContentController: Beta user #{current_api_user.id} redirecting to staging manifest"
+          return redirect_to manifest_endpoints['staging'], status: 302
+        end
+
         if manifest_endpoints[params[:content_env]]
           redirect_to manifest_endpoints[params[:content_env]], status: 302
         else
+          Rails.logger.info "ContentController: unknown environment #{params[:content_env]}"
           render_api_response 404
         end
       end
