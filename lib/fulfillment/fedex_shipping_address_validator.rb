dd709b485b95cd64bb88a3346ea0c02735d67fdf
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-03-13 16:10:11 -0700

    Clean up validate method

--- lib/fulfillment/fedex_shipping_address_validator.rb
+++ lib/fulfillment/fedex_shipping_address_validator.rb
@@ -22,104 +22,18 @@ module Fulfillment
       )
       return validation unless validation[:is_valid]
 
-      validation = any_invalid?(
-        order: order,
-        validation_method: :nil_or_empty?,
-        message: 'nil or empty',
-        properties: [
-          :name,
-          :address1,
-          :city,
-          :province_code,
-          :country_code
-        ]
-      )
-      return validation unless validation[:is_valid]
+      multi_validations(order).each do |params|
+        validation = send(:any_invalid?, params)
+        return validation unless validation[:is_valid]
+      end
 
-      validation = any_invalid?(
-        order: order,
-        validation_method: :invalid_length?,
-        message: "invalid length, must be between #{MIN_LINE_LENGTH} and #{MAX_LINE_LENGTH} characters",
-        properties: [
-          :name,
-          :company,
-          :address1,
-          :city
-        ]
-      )
-      return validation unless validation[:is_valid]
-
-      validation = any_invalid?(
-        order: order,
-        validation_method: :exceeds_max_length?,
-        message: "exceeds max length, must be #{MAX_LINE_LENGTH} characters or less",
-        properties: [:address2]
-      )
-      return validation unless validation[:is_valid]
-
-      validation = any_invalid?(
-        order: order,
-        validation_method: :contains_invalid_char?,
-        message: 'contains invalid character',
-        properties: [
-          :name,
-          :company,
-          :address1,
-          :address2,
-          :city,
-          :phone
-        ]
-      )
-      return validation unless validation[:is_valid]
-
-      # State code is required and must be two characters in length
-      validation = log_validation(
-        order_id: order.id,
-        condition: order.shipping_address.province_code.length != 2,
-        message: "province_code has invalid length: #{order.shipping_address.province_code}"
-      )
-      return validation unless validation[:is_valid]
-
-      # Country code is required and must be two characters in length
-      validation = log_validation(
-        order_id: order.id,
-        condition: order.shipping_address.country_code.length != 2,
-        message: "country_code has invalid length: #{order.shipping_address.country_code}"
-      )
-      return validation unless validation[:is_valid]
-
-      # No PO boxes
-      validation = log_validation(
-        order_id: order.id,
-        condition: order.shipping_address.address1 =~ POBOX_REGEX,
-        message: "address1 looks like a PO Box: #{order.shipping_address.address1}"
-      )
-      return validation unless validation[:is_valid]
-      validation = log_validation(
-        order_id: order.id,
-        condition: order.shipping_address.address2 =~ POBOX_REGEX,
-        message: "address2 looks like a PO Box: #{order.shipping_address.address2}"
-      )
-      return validation unless validation[:is_valid]
-
-      # No APO/FPO/DPO military state codes
-      validation = log_validation(
-        order_id: order.id,
-        condition: US_MILITARY_STATES.include?(order.shipping_address.province_code),
-        message: "province_code is US military: #{order.shipping_address.province_code}"
-      )
-      return validation unless validation[:is_valid]
-
-      # No US territory state codes
-      validation = log_validation(
-        order_id: order.id,
-        condition: US_TERRITORY_STATES.include?(order.shipping_address.province_code),
-        message: "province_code is US military: #{order.shipping_address.province_code}"
-      )
-      return validation unless validation[:is_valid]
+      single_validations(order).each do |params|
+        validation = send(:log_validation, params)
+        return validation unless validation[:is_valid]
+      end
 
       Rails.logger.info("FedEx address validator order with id #{order.id} is valid")
-      validation
+      { is_valid: true, messages: [] }
     end
 
     private
@@ -174,7 +88,89 @@ module Fulfillment
         Rails.logger.warn("FedEx address validator order with id #{params[:order_id]} is invalid because #{params[:message]}")
         return { is_valid: false, messages: [params[:message]] }
       end
-      return { is_valid: true, messages: [] }
+      { is_valid: true, messages: [] }
+    end
+
+    def self.multi_validations(order)
+      [
+        {
+          order: order,
+          validation_method: :nil_or_empty?,
+          message: 'nil or empty',
+          properties: [
+            :name,
+            :address1,
+            :city,
+            :province_code,
+            :country_code
+          ]
+        },
+        {
+          order: order,
+          validation_method: :invalid_length?,
+          message: "invalid length, must be between #{MIN_LINE_LENGTH} and #{MAX_LINE_LENGTH} characters",
+          properties: [
+            :name,
+            :company,
+            :address1,
+            :city
+          ]
+        },
+        {
+          order: order,
+          validation_method: :exceeds_max_length?,
+          message: "exceeds max length, must be #{MAX_LINE_LENGTH} characters or less",
+          properties: [:address2]
+        },
+        {
+          order: order,
+          validation_method: :contains_invalid_char?,
+          message: 'contains invalid character',
+          properties: [
+            :name,
+            :company,
+            :address1,
+            :address2,
+            :city,
+            :phone
+          ]
+        }
+      ]
+    end
+
+    def self.single_validations(order)
+      [
+        {
+          order_id: order.id,
+          condition: order.shipping_address.province_code.length != 2,
+          message: "province_code has invalid length: #{order.shipping_address.province_code}"
+        },
+        {
+          order_id: order.id,
+          condition: order.shipping_address.country_code.length != 2,
+          message: "country_code has invalid length: #{order.shipping_address.country_code}"
+        },
+        {
+          order_id: order.id,
+          condition: order.shipping_address.address1 =~ POBOX_REGEX,
+          message: "address1 looks like a PO Box: #{order.shipping_address.address1}"
+        },
+        {
+          order_id: order.id,
+          condition: order.shipping_address.address2 =~ POBOX_REGEX,
+          message: "address2 looks like a PO Box: #{order.shipping_address.address2}"
+        },
+        {
+          order_id: order.id,
+          condition: US_MILITARY_STATES.include?(order.shipping_address.province_code),
+          message: "province_code is US military: #{order.shipping_address.province_code}"
+        },
+        {
+          order_id: order.id,
+          condition: US_TERRITORY_STATES.include?(order.shipping_address.province_code),
+          message: "province_code is US military: #{order.shipping_address.province_code}"
+        }
+      ]
     end
   end
 end
