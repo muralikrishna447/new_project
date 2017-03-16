f633cefb1bed38f3d201b857c73a9dd323089fe4
stujo <stujo@users.noreply.github.com>
2017-03-16 16:13:58 -0700

    Add dispatch to task_name class method

--- app/workers/fulfillment/aws_queue_worker.rb
+++ app/workers/fulfillment/aws_queue_worker.rb
@@ -1,18 +1,22 @@
 module Fulfillment
   class AwsQueueWorker
 
-    LOGGING_NAMESPACE = 'com.chefsteps.awsqueue.worker.'+ Rails.env + '.'
 
-    LOCK_QUEUE_NAME = (LOGGING_NAMESPACE + 'lock').to_sym
+    AWS_QUEUE_WORKER = 'aws-queue-worker'
 
-    LIBRATO_PREFIX = LOGGING_NAMESPACE
+    LOGGING_NAMESPACE = (AWS_QUEUE_WORKER + '-' + Rails.env + '-').downcase.gsub(/\s+/, '-')
+
+    QUEUE_LABEL = LOGGING_NAMESPACE
+
+    RESQUE_LOCK_NAME = (LOGGING_NAMESPACE + '-lock').to_sym
+
+    LIBRATO_PREFIX = LOGGING_NAMESPACE.tr('-', '.')
+
 
     extend Resque::Plugins::Lock
 
-    @queue = LOCK_QUEUE_NAME
-
     def self.lock(_params)
-      LOCK_QUEUE_NAME
+      RESQUE_LOCK_NAME
     end
 
     def self.perform(params = {})
@@ -22,26 +26,49 @@ module Fulfillment
         # Params hash keys are deserialized as strings coming out of Redis,
         # so we re-symbolize them here.
         sym_params = params.deep_symbolize_keys
+        task_name = sym_params[:task]
 
-        librato_increment('started', sym_params)
+        librato_increment('started', task_name)
 
-        Rails.logger.info { "AWSQueueWorker Called #{sym_params.inspect}" }
+        Rails.logger.info { "AWSQueueWorker Called #{task_name} : #{sym_params.inspect}" }
+
+        dispatch_task(task_name, sym_params[:message])
 
       rescue StandardError => error
         Rails.logger.error(error)
-        librato_increment('failure', sym_params)
+        librato_increment('failure', task_name)
+          raise # This is important because it will leave the message in the queue
       else
-        librato_increment('success', sym_params)
+        librato_increment('success', task_name)
       ensure
         Librato.tracker.flush
       end
     end
 
-    def self.librato_increment(subkey, sym_params)
-      task_name = sym_params ? sym_params[:task_name] : :unknown_task_name
+    def self.dispatch_task(task_name, message_body)
+      # Always perform tasks inline here, so if the task fails the messages
+      # will not be deleted from the SQS queue. Going ASYNC will break that
+
+      method_name = ('dispatch_' + task_name).to_sym
+
+      # This will throw and cancel the message delivery if the method does not exist
+      self.send(method_name, [message_body])
+
+    end
+
+    # Task Handlers - dispatch_TASK_NAME
+    def self.dispatch_submit_orders_to_rosti(message)
+      max_quantity = 1500 # Consider pull this from the message, but let's leave that for now
+      max_quantity = 5 if Rails.env.development?
+      inline = true
+      Fulfillment::RostiOrderSubmitter.submit_orders_to_rosti( max_quantity, inline )
+    end
+
+
+    def self.librato_increment(subkey, task_name)
       Rails.logger.info("#{LIBRATO_PREFIX} : #{subkey} : #{task_name}")
       begin
-        Librato.increment LIBRATO_PREFIX + subkey, sporadic: true
+        Librato.increment LIBRATO_PREFIX + task_name + '.' + subkey, sporadic: true
       rescue StandardError => error
         Rails.logger.error error
       end
--- lib/tasks/aws_queue.rake
+++ lib/tasks/aws_queue.rake
@@ -4,6 +4,11 @@ namespace :aws do
     args.with_defaults(inline: false)
     Rails.logger.info("AwsQueuePoller starting with params #{args}")
     if args[:inline].to_s == 'true'
+
+      if Rails.env.production?
+        Rails.logger.error "AWS:RUN_TASK_IF_QUEUED -> INLINE IS ONLY FOR DEV TESTING, ALWAYS RUN REAL JOBS with inline=false"
+      end
+
       Fulfillment::AwsQueuePoller.perform(args[:task_name].to_s)
     else
       Resque.enqueue(Fulfillment::AwsQueuePoller, args[:task_name].to_s)
