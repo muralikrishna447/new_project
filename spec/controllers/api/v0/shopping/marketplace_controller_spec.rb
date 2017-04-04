8c52fd4cf96985c0cf9587de75a3eff4c10c4b0c
tswetonic <swetonic@gmail.com>
2017-04-04 12:46:17 -0700

    added a test for the case: not logged in, feature name is set

--- spec/controllers/api/v0/shopping/marketplace_controller_spec.rb
+++ spec/controllers/api/v0/shopping/marketplace_controller_spec.rb
@@ -1,6 +1,5 @@
 describe Api::V0::Shopping::MarketplaceController do
   before :each do
-    @user = Fabricate :user, id: 10001, name: 'customer_1', email: 'customer_1@chefsteps.com', role: 'user'
     @marketplace_guide1 = Fabricate :marketplace_guide, id: 1, guide_id: '3N1qPSrcViOGEYCeaG6io4', url: 'https://test.myshopify.com/products/snake-river-farms-steak-selection?utm_source=App&utm_medium=post&utm_campaign=chefsteps_app_sales_srf', button_text: 'Shop steaks'
     @marketplace_guide2 = Fabricate :marketplace_guide, id: 2, guide_id: 'JIO8hrpTywMCSI40KswcY', url: 'https://store.chocolateyshatner.com/products/72-hour-short-ribs?utm_source=App&utm_medium=post&utm_campaign=short_ribs', button_text: 'Shop short ribs', button_text_line_2: '$84'
     @marketplace_guide3 = Fabricate :marketplace_guide, id: 3, guide_id: '2MH313EsysIOwGcMooSSkk', url: 'https://store.chocolateyshatner.com/products/double-r-ranch-steak-selection?utm_source=App&utm_medium=post&utm_campaign=chefsteps_app_sales_srf', button_text: 'Buy locally', button_text_line_2: '$20-$40', feature_name: 'steak_buy_button'
@@ -19,7 +18,14 @@ describe Api::V0::Shopping::MarketplaceController do
       expect(JSON.parse(response.body)['button']).to eq expected
     end
 
+    it 'returns no button when not logged in, when a feature name is set' do
+      get :guide_button, guide_id: '2MH313EsysIOwGcMooSSkk'
+      expected = {"request_id"=>nil, "status"=>200}
+      expect(JSON.parse(response.body)).to eq expected
+    end
+
     it 'supports beta-flagged guides' do
+      @user = Fabricate :user, id: 10001, name: 'customer_1', email: 'customer_1@chefsteps.com', role: 'user'
       BetaFeatureService.stub(:user_has_feature).with(anything(), 'steak_buy_button').and_return(true)
       request.env['HTTP_AUTHORIZATION'] = @user.valid_website_auth_token.to_jwt
       get :guide_button, guide_id: '2MH313EsysIOwGcMooSSkk'
@@ -36,6 +42,7 @@ describe Api::V0::Shopping::MarketplaceController do
     end
 
     it 'returns sso url when apprioriate' do
+      @user = Fabricate :user, id: 10001, name: 'customer_1', email: 'customer_1@chefsteps.com', role: 'user'
       request.env['HTTP_AUTHORIZATION'] = @user.valid_website_auth_token.to_jwt
       get :guide_button_redirect, guide_id: '3N1qPSrcViOGEYCeaG6io4'
 
