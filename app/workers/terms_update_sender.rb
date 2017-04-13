f98ef0b5faa704c325c5a82862c86d3017515d52
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-04-13 12:44:04 -0700

    Add utility methods to read email addresses from CSV file and queue a list of them

--- app/workers/terms_update_sender.rb
+++ app/workers/terms_update_sender.rb
@@ -1,3 +1,5 @@
+require 'aws-sdk'
+
 class TermsUpdateSender
   @queue = :TermsUpdateSender
 
@@ -6,5 +8,20 @@ class TermsUpdateSender
 
     Rails.logger.info "Starting TermsUpdateSender job for email address #{email_address} and terms version #{terms_version}"
     TermsUpdateMailer.prepare(email_address, terms_version).deliver
+  end
+
+  # Reads a text file containing email addresses from S3, one per line.
+  def self.email_addresses_from_s3(region, bucket, path)
+    s3 = Aws::S3::Resource.new(region: region)
+    body = s3.bucket(bucket).object(path).get.body.read
+    body.split("\n")
+  end
+
+  # Enqueues sender jobs for an array of email addresses and a specific
+  # version of the terms.
+  def self.enqueue_emails(email_addresses, terms_version)
+    email_addresses.each do |address|
+      Resque.enqueue(TermsUpdateSender, address, terms_version)
+    end
   end
 end
