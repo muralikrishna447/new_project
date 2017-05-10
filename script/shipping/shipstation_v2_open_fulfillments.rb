b10b43781f12ed71157e537552671ba5fe2004ee
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-05-09 17:40:53 -0700

    Update ShipStation stuff to use refactored fulfillment pipeline bits; remove Joule-specific SKU stuff

--- script/shipping/shipstation_v2_open_fulfillments.rb
+++ script/shipping/shipstation_v2_open_fulfillments.rb
@@ -6,6 +6,7 @@ require './rails_shim'
 #   --key: Your Shopify API key
 #   --password: Your Shopify API key password
 #   --store: The name of the Shopify store ('delve' for prod, or 'chefsteps-staging')
+#   --sku: The SKU to open fulfillments for
 #
 
 # Options parsing
@@ -30,26 +31,33 @@ option_parser = OptionParser.new do |option|
   option.on('-i', '--input INPUT', 'Input file') do |input|
     options[:input] = input
   end
+
+  option.on('-i', '--sku SKU', 'SKU to export') do |sku|
+    options[:sku] = sku
+  end
+
+  option.on('-q', '--quantity QUANTITY', 'Quantity to export') do |quantity|
+    options[:quantity] = quantity.to_i
+  end
 end
 option_parser.parse!
 raise '--key is required' unless options[:api_key]
 raise '--password is required' unless options[:password]
 raise '--store is required' unless options[:store]
 raise '--input is required' unless options[:input]
+raise '--sku is required' unless options[:sku]
+raise '--quantity is required' unless options[:quantity]
 
 # Configure shopify client
 ShopifyAPI::Base.site = "https://#{options[:api_key]}:#{options[:password]}@#{options[:store]}.myshopify.com/admin"
 
-orders = []
-CSV.foreach(options[:input], headers: true) do |input_row|
-  order_id = input_row['id']
-  STDERR.puts "Looking up order with id #{order_id}"
-  order = ShopifyAPI::Order.find(order_id)
-  raise "No order for id #{order_id}" unless order
-  orders << order
-end
-
-fulfillables = Fulfillment::ShipstationOrderExporter.fulfillables(orders, ['cs10001'])
-if options[:open_fulfillment]
-  Fulfillment::ShipstationOrderExporter.open_fulfillments(fulfillables)
-end
+Fulfillment::ShipstationOrderExporter.perform(
+  storage: 'stdout',
+  skus: [options[:sku]],
+  search_params: {
+    storage: 'file',
+    storage_filename: options[:input]
+  },
+  open_fulfillment: options[:open_fulfillment],
+  quantity: options[:quantity]
+)
