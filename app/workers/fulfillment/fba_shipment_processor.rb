530df6a2938c4c3b53d20d757424212cb18462bf
Jeremy Shaffer <jshaffer@chefsteps.com>
2017-06-07 15:50:25 -0700

    Add FBA shipment processor and spec

---
+++ app/workers/fulfillment/fba_shipment_processor.rb
@@ -0,0 +1,142 @@
+module Fulfillment
+  class FbaShipmentProcessor
+    # Scans Shopify for open orders looking for those with open fulfillments on
+    # FBA-fulfilled SKUs. Syncs fulfillment status from FBA to Shopify and
+    # updates tracking/completes fulfillment for each order.
+    def self.perform(params)
+      Rails.logger.info "FbaShipmentProcessor starting perform with params #{params}"
+      Shopify::Utils.search_orders_with_each(status: 'open') do |order|
+        process_order(order, params)
+      end
+    end
+
+    # Process a sync for the specified Shopify order. This is read only unless
+    # you explicitly specify { complete_fulfillment: true } in the params.
+    def self.process_order(order, params = {})
+      Rails.logger.info "FbaShipmentProcessor processing order with id #{order.id}"
+
+      order.line_items.each do |item|
+        next unless Fulfillment::FBA_FULFILLABLE_SKUS.include?(item.sku)
+
+        # Try to find a previously-opened fulfillment for the item.
+        fulfillable = Fulfillment::Fulfillable.new(
+          order: order, line_items: [item]
+        )
+        fulfillment = fulfillable.opened_fulfillment_for_line_item(item)
+        unless fulfillment
+          Rails.logger.info "FbaShipmentProcessor order with id #{order.id} " \
+                            "and line item with id #{item.id} has no opened fulfillment, skipping"
+          next
+        end
+
+        # Try to find a corresponding fulfillment order for the item in FBA.
+        seller_fulfillment_order_id = Fulfillment::Fba.seller_fulfillment_order_id(fulfillable, item)
+        response = Fulfillment::Fba.fulfillment_order(seller_fulfillment_order_id)
+        unless response
+          Rails.logger.info "FbaShipmentProcessor order with id #{order.id} " \
+                            "and line item with id #{item.id} has no FBA fulfillment order " \
+                            "with id #{seller_fulfillment_order_id}, skipping"
+          next
+        end
+
+        fulfillment_order = response.fetch('FulfillmentOrder')
+        status = fulfillment_order.fetch('FulfillmentOrderStatus')
+        Rails.logger.info "FbaShipmentProcessor order with id #{order.id} " \
+                          "and line item with id #{item.id} has FBA fulfillment order " \
+                          "with id #{seller_fulfillment_order_id} and status #{status}"
+
+        # Sync shipment status to Shopify as necessary.
+        case status
+        when 'RECEIVED'
+          handle_pending(fulfillment_order, order, status)
+        when 'INVALID'
+          handle_error(fulfillment_order, order, status)
+        when 'PLANNING'
+          handle_pending(fulfillment_order, order, status)
+        when 'PROCESSING'
+          handle_pending(fulfillment_order, order, status)
+        when 'CANCELLED'
+          Rails.logger.info "FbaShipmentProcessor fulfillment order with id #{seller_fulfillment_order_id} " \
+                            "was cancelled, not updating Shopify fulfillment for order with id #{order.id}"
+        when 'COMPLETE'
+          shipment = to_shipment(fulfillment_order, order, fulfillment)
+          shipment.complete! if shipment && params[:complete_fulfillment]
+        when 'COMPLETE_PARTIALLED'
+          handle_error(fulfillment_order, order, status)
+        when 'UNFULFILLABLE'
+          handle_error(fulfillment_order, order, status)
+        else
+          handle_error(fulfillment_order, order, status)
+        end
+      end
+    end
+
+    # Translates an FBA fulfillment order to a Fulfillment::Shipment object.
+    def self.to_shipment(fulfillment_order, order, fulfillment)
+      carrier_codes = []
+      tracking_numbers = []
+      all_shipped = true
+      fulfillment_order.fetch('FulfillmentShipment').each_value do |fba_shipment|
+        all_shipped = false unless fba_shipment.fetch('FulfillmentShipmentStatus') == 'SHIPPED'
+        fba_shipment.fetch('FulfillmentShipmentPackage').each_value do |fba_package|
+          carrier_codes << fba_package.fetch('CarrierCode')
+          tracking_numbers << fba_package.fetch('TrackingNumber')
+        end
+      end
+
+      # Wait until all shipments associated with the fulfillment order have
+      # shipped until we sync back to Shopfiy. It seems very unlikely we'll
+      # encounter any problems with this any time soon given that mostly
+      # there will just be a single shipment.
+      unless all_shipped
+        Rails.logger.info "FbaShipmentProcessor not all shipments have shipped for order with id #{order.id} " \
+                          "and fulfillment order with id #{fulfillment_order.fetch('SellerFulfillmentOrderId')}, " \
+                          'skipping'
+        return nil
+      end
+
+      # This seems unlikely, but we'd still want to complete fulfillment
+      # even if FBA doesn't give us tracking info.
+      if carrier_codes.empty?
+        Rails.logger.warn "FbaShipmentProcessor carrier codes is empty for order with id #{order.id} " \
+                          "and fulfillment order with id #{fulfillment_order.fetch('SellerFulfillmentOrderId')}"
+      end
+      if tracking_numbers.empty?
+        Rails.logger.warn "FbaShipmentProcessor tracking numbers is empty for order with id #{order.id} " \
+                          "and fulfillment order with id #{fulfillment_order.fetch('SellerFulfillmentOrderId')}"
+      end
+
+      Fulfillment::Shipment.new(
+        order: order,
+        fulfillments: [fulfillment],
+        tracking_company: reduce_carrier_codes(carrier_codes),
+        tracking_numbers: tracking_numbers
+      )
+    end
+
+    # This doesn't handle shipments with multiple carrier codes.
+    # This seems extremely unlikely for just clamps, but
+    # in the future we probably have to rejigger the fulfillments
+    # on the order to be able to reflect how FBA shipped it.
+    def self.reduce_carrier_codes(carrier_codes)
+      carrier_code = carrier_codes.first
+      # Need to match the carrier that Shopify expects to get links working, etc.
+      carrier_code = 'Amazon Logistics US' if carrier_code == 'Amazon Logistics'
+      carrier_code
+    end
+
+    private
+
+    def self.handle_pending(fulfillment_order, order, status)
+      Rails.logger.info "FbaShipmentProcessor fulfillment order with id #{fulfillment_order.fetch('SellerFulfillmentOrderId')} " \
+                        "has pending status #{status}, not updating Shopify fulfillment for order with id #{order.id}"
+    end
+
+    def self.handle_error(fulfillment_order, order, status)
+      Rails.logger.warn "FbaShipmentProcessor fulfillment order with id #{fulfillment_order.fetch('SellerFulfillmentOrderId')} " \
+                        "has error status #{status}, tagging Shopify order with id #{order.id} for follow up"
+      Shopify::Utils.add_to_order_tags(order, 'shipping-error')
+      Shopify::Utils.send_assert_true(order, :save)
+    end
+  end
+end
---
+++ spec/workers/fulfillment/fba_shipment_processor_spec.rb
@@ -0,0 +1,298 @@
+require 'spec_helper'
+
+describe Fulfillment::FbaShipmentProcessor do
+  describe 'perform' do
+    let(:order_1) { ShopifyAPI::Order.new(id: 1) }
+    let(:order_2) { ShopifyAPI::Order.new(id: 2) }
+    let(:params) { { my_param: true } }
+
+    it 'processes open orders' do
+      Shopify::Utils
+        .stub(:search_orders_with_each)
+        .with(status: 'open')
+        .and_yield(order_1)
+        .and_yield(order_2)
+      Fulfillment::FbaShipmentProcessor.should_receive(:process_order).with(order_1, params)
+      Fulfillment::FbaShipmentProcessor.should_receive(:process_order).with(order_2, params)
+      Fulfillment::FbaShipmentProcessor.perform(params)
+    end
+  end
+
+  describe 'process_order' do
+    let(:line_item) { ShopifyAPI::LineItem.new(id: 'my-line-item-id', sku: sku) }
+    let(:line_items) { [line_item] }
+    let(:fulfillment) do
+      ShopifyAPI::Fulfillment.new(
+        line_items: line_items,
+        status: fulfillment_status
+      )
+    end
+    let(:order) do
+      ShopifyAPI::Order.new(
+        id: 'my-order-id',
+        fulfillments: [fulfillment],
+        line_items: line_items
+      )
+    end
+    let(:fulfillable) { Fulfillment::Fulfillable.new(order: order, line_items: [line_item]) }
+
+    context 'item SKU is fulfillable by FBA' do
+      let(:sku) { 'cs30001' }
+      let(:seller_fulfillment_order_id) { 'my-fba-order-id' }
+
+      context 'item has opened fulfillment' do
+        let(:fulfillment_status) { 'open' }
+
+        before :each do
+          Fulfillment::Fba
+            .stub(:seller_fulfillment_order_id)
+            .with(fulfillable, line_item)
+            .and_return(seller_fulfillment_order_id)
+        end
+
+        context 'item has FBA fulfillment order' do
+          let(:fba_fulfillment_order) do
+            {
+              'SellerFulfillmentOrderId' => seller_fulfillment_order_id,
+              'FulfillmentOrderStatus' => fba_status
+            }
+          end
+          let(:fba_response) { { 'FulfillmentOrder' => fba_fulfillment_order } }
+
+          before :each do
+            Fulfillment::Fba
+              .stub(:fulfillment_order)
+              .with(seller_fulfillment_order_id)
+              .and_return(fba_response)
+          end
+
+          context 'FBA status is pending' do
+            shared_examples 'fba_shipment_pending' do
+              it 'does not call to_shipment' do
+                Fulfillment::FbaShipmentProcessor.should_not_receive(:to_shipment)
+                Fulfillment::FbaShipmentProcessor.process_order(order)
+              end
+            end
+
+            context 'FBA status is RECEIVED' do
+              let(:fba_status) { 'RECEIVED' }
+              include_examples 'fba_shipment_pending'
+            end
+            
+            context 'FBA status is PLANNING' do
+              let(:fba_status) { 'PLANNING' }
+              include_examples 'fba_shipment_pending'
+            end
+
+            context 'FBA status is PROCESSING' do
+              let(:fba_status) { 'PROCESSING' }
+            end
+          end
+
+          context 'FBA status is cancelled' do
+            let(:fba_status) { 'CANCELLED' }
+            it 'does not call to_shipment' do
+              Fulfillment::FbaShipmentProcessor.should_not_receive(:to_shipment)
+              Fulfillment::FbaShipmentProcessor.process_order(order)
+            end
+          end
+
+          context 'FBA status is complete' do
+            let(:fba_status) { 'COMPLETE' }
+            let(:params) { { complete_fulfillment: complete_fulfillment } }
+            let(:shipment) { double('shipment') }
+
+            context 'complete_fulfillment param is falsey' do
+              let(:complete_fulfillment) { false }
+
+              it 'calls to_shipment and does not complete fulfillment' do
+                Fulfillment::FbaShipmentProcessor
+                  .should_receive(:to_shipment)
+                  .with(fba_fulfillment_order, order, fulfillment)
+                  .and_return(shipment)
+                shipment.should_not_receive(:complete!)
+                Fulfillment::FbaShipmentProcessor.process_order(order, params)
+              end
+            end
+
+            context 'complete_fulfillment param is truthy' do
+              let(:complete_fulfillment) { true }
+              let(:shipment) { double('shipment') }
+
+              it 'calls to_shipment and completes fulfillment' do
+                Fulfillment::FbaShipmentProcessor
+                  .should_receive(:to_shipment)
+                  .with(fba_fulfillment_order, order, fulfillment)
+                  .and_return(shipment)
+                shipment.should_receive(:complete!)
+                Fulfillment::FbaShipmentProcessor.process_order(order, params)
+              end
+            end
+          end
+
+          context 'FBA status is error' do
+            shared_examples 'fba_shipment_error' do
+              it 'does not call to_shipment and adds shipment-error tag' do
+                Fulfillment::FbaShipmentProcessor.should_not_receive(:to_shipment)
+                Shopify::Utils.should_receive(:add_to_order_tags).with(order, 'shipping-error')
+                Shopify::Utils.should_receive(:send_assert_true).with(order, :save)
+                Fulfillment::FbaShipmentProcessor.process_order(order)
+              end
+            end
+
+            context 'FBA status is INVALID' do
+              let(:fba_status) { 'INVALID' }
+              include_examples 'fba_shipment_error'
+            end
+
+            context 'FBA status is COMPLETE_PARTIALLED' do
+              let(:fba_status) { 'COMPLETE_PARTIALLED' }
+              include_examples 'fba_shipment_error'
+            end
+
+            context 'FBA status is UNFULFILLABLE' do
+              let(:fba_status) { 'UNFULFILLABLE' }
+              include_examples 'fba_shipment_error'
+            end
+
+            context 'FBA status is unknown state' do
+              let(:fba_status) { 'unknown' }
+              include_examples 'fba_shipment_error'
+            end
+          end
+        end
+
+        context 'item has no FBA fulfillment order' do
+          before :each do
+            Fulfillment::Fba
+              .stub(:fulfillment_order)
+              .with(seller_fulfillment_order_id)
+              .and_return(nil)
+          end
+
+          it 'does not call to_shipment' do
+            Fulfillment::FbaShipmentProcessor.should_not_receive(:to_shipment)
+            Fulfillment::FbaShipmentProcessor.process_order(order)
+          end
+        end
+      end
+
+      context 'item does not have opened fulfillment' do
+        let(:fulfillment_status) { 'success' }
+        it 'does not look up fulfillment order' do
+          Fulfillment::Fba.should_not_receive(:fulfillment_order)
+          Fulfillment::FbaShipmentProcessor.process_order(order)
+        end
+      end
+    end
+
+    context 'item SKU is not fulfillable by FBA' do
+      let(:sku) { 'not fba sku' }
+      let(:fulfillment_status) { 'open' }
+      it 'does not look up fulfillment order' do
+        Fulfillment::Fba.should_not_receive(:fulfillment_order)
+        Fulfillment::FbaShipmentProcessor.process_order(order)
+      end
+    end
+  end
+
+  describe 'to_shipment' do
+    let(:fulfillment) { ShopifyAPI::Fulfillment.new(id: 1) }
+    let(:order) do
+      ShopifyAPI::Order.new(
+        id: 2,
+        fulfillments: [fulfillment]
+      )
+    end
+    let(:carrier_code) { 'USPS' }
+    let(:shipment_status_1) { 'SHIPPED' }
+    let(:tracking_number_1) { '111' }
+    let(:tracking_number_2) { '222' }
+    let(:tracking_number_3) { '333' }
+    let(:fba_fulfillment_order) do
+      {
+        'SellerFulfillmentOrderId' => 'my-fba-order-id',
+        'FulfillmentShipment' => {
+          '1' => {
+            'FulfillmentShipmentStatus' => shipment_status_1,
+            'FulfillmentShipmentPackage' => {
+              '1' => {
+                'CarrierCode' => carrier_code,
+                'TrackingNumber' => tracking_number_1
+              }
+            }
+          },
+          '2' => {
+            'FulfillmentShipmentStatus' => shipment_status_2,
+            'FulfillmentShipmentPackage' => {
+              '1' => {
+                'CarrierCode' => carrier_code,
+                'TrackingNumber' => tracking_number_2
+              },
+              '2' => {
+                'CarrierCode' => carrier_code,
+                'TrackingNumber' => tracking_number_3
+              }
+            }
+          }
+        }
+      }
+    end
+
+    before :each do
+      Fulfillment::FbaShipmentProcessor.stub(:reduce_carrier_codes).and_return(carrier_code)
+    end
+
+    context 'fulfillment shipment status for all shipments is shipped' do
+      let(:shipment_status_2) { 'SHIPPED' }
+      it 'returns shipment with tracking and the first carrier code' do
+        expect(Fulfillment::FbaShipmentProcessor.to_shipment(fba_fulfillment_order, order, fulfillment))
+          .to eq Fulfillment::Shipment.new(
+            order: order,
+            fulfillments: [fulfillment],
+            tracking_company: carrier_code,
+            tracking_numbers: [tracking_number_1, tracking_number_2, tracking_number_3]
+          )
+      end
+    end
+
+    context 'fulfillment shipment status is not shipped for all shipments' do
+      let(:shipment_status_2) { 'PENDING' }
+      it 'returns nil' do
+        expect(Fulfillment::FbaShipmentProcessor.to_shipment(fba_fulfillment_order, order, fulfillment)).to be_nil
+      end
+    end
+  end
+
+  describe 'reduce_carrier_codes' do
+    let(:carrier_code) { 'USPS' }
+
+    context 'multiple unique carrier codes' do
+      it 'returns first carrier code' do
+        expect(Fulfillment::FbaShipmentProcessor.reduce_carrier_codes([carrier_code, 'Other carrier']))
+          .to eq carrier_code
+      end
+    end
+
+    context 'single carrier code' do
+      it 'returns carrier code' do
+        expect(Fulfillment::FbaShipmentProcessor.reduce_carrier_codes([carrier_code, carrier_code]))
+          .to eq carrier_code
+      end
+
+      context 'carrier code is Amazon Logistics' do
+        let(:carrier_code) { 'Amazon Logistics' }
+        it 'returns Amazon Logistics US' do
+          expect(Fulfillment::FbaShipmentProcessor.reduce_carrier_codes([carrier_code]))
+            .to eq 'Amazon Logistics US'
+        end
+      end
+    end
+
+    context 'carrier codes is empty' do
+      it 'returns nil' do
+        expect(Fulfillment::FbaShipmentProcessor.reduce_carrier_codes([])).to be_nil
+      end
+    end
+  end
+end
