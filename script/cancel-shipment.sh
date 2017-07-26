3c572c823c7a8ee27d624223d69d9e1ccaed6980
Stujo <stujo@users.noreply.github.com>
2017-07-26 14:56:25 -0700

    Remove Temporary Files

--- script/cancel-shipment.sh
+++
@@ -1,4 +0,0 @@
-$ heroku run --app production-chefsteps rails c
-$ o = Shopify::Utils.order_by_name('#205107610417')
-$ f = o.fulfillments.select { |f| !f.line_items.select { |li| li.id == 10808846356 }.empty? && f.status == 'open' }.first
-$ f.cancel
