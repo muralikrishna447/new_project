ad9e6faf47ab54ca174c2014d835b2b195841930
stujo <stujo@users.noreply.github.com>
2017-09-20 16:27:17 -0700

    Add Fixture mock API response

--- spec/controllers/api/v0/shopping/products_groups_controller_spec.rb
+++ spec/controllers/api/v0/shopping/products_groups_controller_spec.rb
@@ -1,45 +1,35 @@
-def puts(value)
-  raise 'you found a puts'
-end
-
-def p(value)
-  raise 'you found a puts'
-end
-
-
 describe Api::V0::Shopping::ProductGroupsController do
+
+  ca_product_groups_data = {
+    iso2: 'CA',
+    currency: 'CAD',
+    product_groups: {
+      joule: {},
+      big_clamp: {},
+      premium: {},
+    }
+  }
+
+  us_product_groups_data = {
+    iso2: 'US',
+    currency: 'USD',
+    product_groups: {
+      joule: {},
+      big_clamp: {},
+      premium: {},
+    }
+  }
+
   before :each do
     Rails.cache.clear()
-    @non_premium_user = Fabricate :user, name: 'Non Premium User', email: 'non_premium_user@chefsteps.com', role: 'user', premium_member: false
-    @premium_user = Fabricate :user, name: 'Premium User', email: 'premium_user@chefsteps.com', role: 'user', premium_member: true, used_circulator_discount: false
-    @premium_user_used_discount = Fabricate :user, name: 'Premium User Used Discount', email: 'premium_user_used_discount@chefsteps.com', role: 'user', premium_member: true, used_circulator_discount: true
+    WebMock.stub_request(:get, 'https://spree-staging1.herokuapp.com/api/v1/cs_countries/US/cs_product_groups')
+      .to_return(status: 200, body: us_product_groups_data.to_json, headers: {})
 
-    product_groups_data = {}
-
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups.json/)
-    #   .to_return(status: 200, body: product_groups_data.to_json, headers: {})
-    #
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/count.json/)
-    #   .to_return(status: 200, body: "2", headers: {})
-    #
-    # product_groups_1_data = JSON.parse(ShopifyAPI::Mock::Fixture.find('product_groups').data)['product_groups'][0]
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/123.json/).to_return(status: 200, body: product_groups_1_data.to_json)
-    #
-    # product_groups_2_data = JSON.parse(ShopifyAPI::Mock::Fixture.find('product_groups').data)['product_groups'][1]
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/345.json/).to_return(status: 200, body: product_groups_2_data.to_json)
-    #
-    # product_groups_3_data = JSON.parse(ShopifyAPI::Mock::Fixture.find('product_groups').data)['product_groups'][2]
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/567.json/).to_return(status: 200, body: product_groups_3_data.to_json)
-    #
-    # product_groups_4_data = JSON.parse(ShopifyAPI::Mock::Fixture.find('product_groups').data)['product_groups'][3]
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/789.json/).to_return(status: 200, body: product_groups_4_data.to_json)
-    #
-    # product_groups_1_metafields_data = JSON.parse(ShopifyAPI::Mock::Fixture.find('product_groups').data)['product_groups'][0]['metafields']
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/123\/metafields.json/).to_return(status: 200, body: product_groups_1_metafields_data.to_json)
-    #
-    # product_groups_2_metafields_data = JSON.parse(ShopifyAPI::Mock::Fixture.find('product_groups').data)['product_groups'][1]['metafields']
-    # WebMock.stub_request(:get, /test.myshopify.com\/admin\/product_groups\/345\/metafields.json/).to_return(status: 200, body: product_groups_2_metafields_data.to_json)
+    WebMock.stub_request(:get, 'https://spree-staging1.herokuapp.com/api/v1/cs_countries/CA/cs_product_groups')
+      .to_return(status: 200, body: ca_product_groups_data.to_json, headers: {})
   end
+
+
 
   describe 'GET /product_groups' do
     it "US should respond with an array of product_groups" do
