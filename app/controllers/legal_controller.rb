491eb08a95dfb90ce415a98388dce8891fa17092
Huy Nguyen <hueezer@gmail.com>
2017-09-29 14:12:35 -0700

    intl legal wip

--- app/controllers/legal_controller.rb
+++ app/controllers/legal_controller.rb
@@ -5,5 +5,86 @@ class LegalController < ApplicationController
   def load_vars
     @eula_terms_url = terms_url
     @eula_privacy_url = privacy_url
+    @location = JSON.parse(cookies['cs_geo'])
+    puts "LOCATION HERE: #{@location}"
+    @countries = [
+      {
+        code: "US",
+        name: "United States",
+        languages: ["English"]
+      },
+      {
+        code: "CA",
+        name: "Canada",
+        languages: ["English", "French"]
+      },
+      {
+        code: "BE",
+        name: "Belgium",
+        languages: ["Dutch", "French", "German"]
+      },
+      {
+        code: "DK",
+        name: "Denmark",
+        languages: ["Danish","German"]
+      },
+      {
+        code: "FI",
+        name: "Finland",
+        languages: ["Finnish", "Swedish"]
+      },
+      {
+        code: "FR",
+        name: "France",
+        languages: ["French"]
+      },
+      {
+        code: "DE",
+        name: "Germany",
+        languages: ["German"]
+      },
+      {
+        code: "IE",
+        name: "Ireland",
+        languages: ["English"]
+      },
+      {
+        code: "IT",
+        name: "Italy",
+        languages: ["Italian"]
+      },
+      {
+        code: "NL",
+        name: "Netherlands",
+        languages: ["Dutch", "English"]
+      },
+      {
+        code: "NO",
+        name: "Norway",
+        languages: ["Norwegian"]
+      },
+      {
+        code: "ES",
+        name: "Spain",
+        languages: ["Spanish"]
+      },
+      {
+        code: "SE",
+        name: "Sweden",
+        languages: ["Swedish"]
+      },
+      {
+        code: "CH",
+        name: "Switzerland",
+        languages: ["German", "French", "English"]
+      },
+      {
+        code: "GB",
+        name: "United Kingdom",
+        languages: ["English"]
+      }
+    ]
+    @country = @countries.find { |c| c[:code] == @location['country'] }
+
   end
 end
