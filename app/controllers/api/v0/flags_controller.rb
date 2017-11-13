59fcec565f78c6881fddd8e003bfba5cd41db757
Huy Nguyen <hueezer@gmail.com>
2017-11-13 01:18:07 -0800

    update flags controller

--- app/controllers/api/v0/flags_controller.rb
+++ app/controllers/api/v0/flags_controller.rb
@@ -5,7 +5,9 @@ module Api
       # Used to display temporary flags (alerts) at the top of the website for things like sales or announcement messages
       def index
         flags = {
-          US: ENV["CS_FLAGS_US"] || ""
+          US: {
+            bannerText: ENV["CS_FLAGS_US"] || ""
+          }
         }
         render_api_response 200, { flags: flags }
       end
