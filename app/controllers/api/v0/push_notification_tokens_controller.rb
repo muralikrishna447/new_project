5e3ff4d2e0d3a5bb6a679008111f6b99310dc57c
Kenshi Kawaguchi <kenshi@recurse.ca>
2018-02-05 14:17:51 -0800

    log full token

--- app/controllers/api/v0/push_notification_tokens_controller.rb
+++ app/controllers/api/v0/push_notification_tokens_controller.rb
@@ -5,8 +5,7 @@ module Api
       before_filter :ensure_authorized_or_anonymous, only: [:destroy]
 
       def create
-        token_snippet = params[:device_token][0..10]
-        logger.info "Creating token for user #{@user_id_from_token} #{token_snippet}"
+        logger.info "Creating token for user #{@user_id_from_token} #{params[:device_token]}"
 
         return render_api_response 200, {} if find_and_clean_existing(params[:device_token])
 
