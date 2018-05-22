cfa92ea67b0bdc083e84f34c93a89f6ae57b7684
tswetonic <swetonic@gmail.com>
2018-05-21 17:08:31 -0700

    possible fix for stripe API change

--- app/controllers/api/v0/products_controller.rb
+++ app/controllers/api/v0/products_controller.rb
@@ -2,11 +2,11 @@ module Api
   module V0
     class ProductsController < BaseController
       def index
-        circulator, premium = Rails.cache.fetch("stripe_products", expires_in: 1.hour){
-          StripeOrder.stripe_products
-        }
-        result = {products: {circulator[:sku] => circulator, premium[:sku] => premium}}
-        result = result.merge(location: geolocate_ip())
+        # circulator, premium = Rails.cache.fetch("stripe_products", expires_in: 1.hour){
+        #   StripeOrder.stripe_products
+        # }
+        # result = {products: {circulator[:sku] => circulator, premium[:sku] => premium}}
+        result = {location: geolocate_ip()}
         render(json: result)
         # render(json: {}) # When things go 'oh shit' uncomment this line
       end
