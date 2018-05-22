0de30fcef0944ecb9a32d4c4b06b5be531bcaa1e
tswetonic <swetonic@gmail.com>
2018-05-21 17:21:39 -0700

    added the hash

--- app/controllers/api/v0/products_controller.rb
+++ app/controllers/api/v0/products_controller.rb
@@ -5,7 +5,8 @@ module Api
         # circulator, premium = Rails.cache.fetch("stripe_products", expires_in: 1.hour){
         #   StripeOrder.stripe_products
         # }
-        result = {products: {'cs10001' => '', 'cs10002' => ''}}
+        result = {products: {'cs10001' => {sku: "cs10001", title: "Joule", price: 0, msrp: 0, tax_code: "null", shippable: true},
+                             'cs10002' => {sku: "cs10002", title: "Premium", price: 0, msrp: 0, tax_code: "null", shippable: false}}}
         result.merge({location: geolocate_ip()})
         render(json: result)
         # render(json: {}) # When things go 'oh shit' uncomment this line
