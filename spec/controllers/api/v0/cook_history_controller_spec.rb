0570e12ea29540b620a61e0e00a1bc1ae0a150b3
John Rivera <jrivera@chefsteps.com>
2018-07-18 14:21:51 -0700

    Throw in some cook history validation tests

--- spec/controllers/api/v0/cook_history_controller_spec.rb
+++ spec/controllers/api/v0/cook_history_controller_spec.rb
@@ -2,19 +2,32 @@ require 'spec_helper'
 
 describe Api::V0::CookHistoryController do
 
+  
 
-  cook_history_params = {
-    cook_id: "eb10428ed5f24c55988cbd1bf84523d4",
-    cook_time: 64800,
-    guide_id: "3kXWJim8FqKsC6AOy62M8C",
-    idempotency_id: "7095f553-f68e-4e33-9de6-08c6369e0db6",
-    program_id: "6yrX59W7oQ82sUE0woWgUS",
-    program_type: "AUTOMATIC",
-    set_point: 58,
-    start_time: 1483999425792,
-    started_from: "jouleApp",
-    timer_id: "6cVAqvnbbymQOOOKWUGWgS"
-  }
+  
+  def create_cook_entry (program_type: 'AUTOMATIC', is_guided: true)
+    
+    entry = {
+      cook_id: "eb10428ed5f24c55988cbd1bf84523d4",
+      cook_time: 64800,
+      idempotency_id: 'ab10428ed5f24d55988cbd1bf84523d3',
+      program_type: program_type,
+      set_point: 58,
+      start_time: 1483999425792,
+      started_from: "jouleApp",
+      program_id: nil,
+      guide_id: nil,
+      timer_id: nil
+    }
+    
+    if is_guided
+      entry[:guide_id] = "3kXWJim8FqKsC6AOy62M8C"
+      entry[:program_id] = "6yrX59W7oQ82sUE0woWgUS"
+      entry[:timer_id] = "6cVAqvnbbymQOOOKWUGWgS"
+    end
+      
+    return entry
+  end
 
   before :each do
     @user = Fabricate :user, name: 'Test User', email: 'admin@chefsteps.com'
@@ -164,21 +177,49 @@ describe Api::V0::CookHistoryController do
   end
 
   # POST /api/v0/cook_history
-  it 'should create a Cook History Item that belongs to authenticated user' do
-    post :create, { cook_history: cook_history_params }
-    response.should be_success
-    parsed = JSON.parse response.body
-    JouleCookHistoryItem.find_by_external_id(parsed["externalId"]).user.should == @user
-  end
-
-  # POST /api/v0/cook_history
-  it 'should not create item with identical user/idempotency_id as existing entry' do
-    fabricate_unique_cook_history_item
-    5.times do
-      post :create, { cook_history: cook_history_params }
+  describe 'create' do
+    it 'should create a Cook History Item that belongs to authenticated user' do
+      post :create, { cook_history: create_cook_entry() }
       response.should be_success
+      parsed = JSON.parse response.body
+      JouleCookHistoryItem.find_by_external_id(parsed["externalId"]).user.should == @user
     end
-    @user_entries.length.should == 2
+
+    it 'should not create item with identical user/idempotency_id as existing entry' do
+      fabricate_unique_cook_history_item
+      5.times do
+        post :create, { cook_history: create_cook_entry() }
+        response.should be_success
+      end
+      @user_entries.length.should == 2
+    end
+    
+    describe 'validation' do
+      UNPROCESSABLE_ENTITY_STATUS = 422
+      it 'should succeed when valid: guided and AUTOMATIC' do
+        create_params = create_cook_entry(program_type: 'AUTOMATIC', is_guided: true)
+        post :create, { cook_history: create_params }
+        response.should be_success
+      end
+      
+      it 'should fail when missing program_id or timer_id: guided' do
+        create_params = create_cook_entry(program_type: 'AUTOMATIC', is_guided: true)
+        create_params[:program_id] = nil
+        post :create, { cook_history: create_params }
+        response.status.should == UNPROCESSABLE_ENTITY_STATUS
+        
+        create_params = create_cook_entry(program_type: 'AUTOMATIC', is_guided: true)
+        create_params[:timer_id] = nil
+        post :create, { cook_history: create_params }
+        response.status.should == UNPROCESSABLE_ENTITY_STATUS
+      end
+      
+      it 'should succeed when valid: non-guided and AUTOMATIC' do
+        create_params = create_cook_entry(program_type: 'AUTOMATIC', is_guided: false)
+        post :create, { cook_history: create_params }
+        response.should be_success
+      end
+    end
   end
   
   # DELETE /api/v0/cook_history
