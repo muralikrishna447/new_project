3c53a5293ff23c8af2a2241401d7d5995d0412bf
stujo <stujo@users.noreply.github.com>
2019-01-09 15:39:26 -0800

    Remove space

--- db/structure.sql
+++ db/structure.sql
@@ -4042,7 +4042,7 @@ CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (v
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO "$user", public;
+SET search_path TO "$user",public;
 
 INSERT INTO schema_migrations (version) VALUES ('20120906183600');
 
