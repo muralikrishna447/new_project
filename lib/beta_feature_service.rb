52d8effc7eea1078a59ec605d0c233ad67260220
stujo <stujo@users.noreply.github.com>
2019-01-24 12:29:41 -0800

    Cache the values in the cluster cache

--- lib/beta_feature_service.rb
+++ lib/beta_feature_service.rb
@@ -7,7 +7,7 @@ module BetaFeature
     end
 
     def user_has_feature(user, feature_name)
-      groups = user.beta_feature_groups
+      groups = get_groups_for_user(user)
       Rails.logger.info "User #{user.id} belongs to these groups: #{groups}"
 
       if groups.length ==  0
@@ -71,37 +71,42 @@ module BetaFeature
     # Fetches all the feature/group info objects for a given
     # feature_name.
     def get_feature_groups_by_feature_name(feature_name)
-      response = @client.query(
-        table_name: @table_config[:group_features_table],
-        select: "ALL_ATTRIBUTES",
-        key_condition_expression: 'feature_name = :feature_name',
-        expression_attribute_values: {
-          ':feature_name' => feature_name
+      cache_key = "beta_features_get_feature_groups_by_feature_name(#{feature_name})"
+      Rails.cache.fetch(cache_key, expires_in: 60.minutes) do
+        response = @client.query(
+          table_name: @table_config[:group_features_table],
+          select: "ALL_ATTRIBUTES",
+          key_condition_expression: 'feature_name = :feature_name',
+          expression_attribute_values: {
+            ':feature_name' => feature_name
+          }
+        )
+        items = response.items.map {|i|
+          {
+            'group_name' => i['group_name'],
+            'feature_name' => i['feature_name'],
+            'is_enabled' => i['is_enabled'],
+          }
         }
-      )
-      items = response.items.map {|i|
-        {
-          'group_name' => i['group_name'],
-          'feature_name' => i['feature_name'],
-          'is_enabled' => i['is_enabled'],
-        }
-      }
-      return items
+       items
+      end
     end
 
     # Fetches and returns a list of groups for a given user.  Returns
     # an empty list if a user is not associated to any groups
     def get_groups_for_user(user)
-      response = @client.query(
-        table_name: @table_config[:group_associations_table],
-        select: "ALL_ATTRIBUTES",
-        key_condition_expression: 'user_id = :user_id',
-        expression_attribute_values: {
-          ':user_id' => user.id
-        }
-      )
-      groups = response.items.map {|i| i['group_name']}
-      return groups
+      cache_key = "beta_features_get_groups_for_user(#{user.id})"
+      Rails.cache.fetch(cache_key, expires_in: 60.minutes) do
+        response = @client.query(
+          table_name: @table_config[:group_associations_table],
+          select: "ALL_ATTRIBUTES",
+          key_condition_expression: 'user_id = :user_id',
+          expression_attribute_values: {
+            ':user_id' => user.id
+          }
+        )
+        response.items.map {|i| i['group_name']}
+      end
     end
 
   end
