02858365aa78e862154d0dfb2694c95e4cec6c40
stujo <stujo@users.noreply.github.com>
2019-01-28 16:24:43 -0800

    Don't join trackable, users who have Course trackables will get an error The Course model has been removed from the codebase

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -21,7 +21,7 @@ class UserProfilesController < ApplicationController
     @total_recipes = @user_pubbed_recipes.count + @user_unpubbed_recipes.count
     @can_add_recipes = (can? :create, Activity) && @is_current_user
     @show_recipes_tab = (@total_recipes > 0) || (@can_add_recipes)
-    @timeline_events =  @user.events.includes([:trackable, :user]).limit(TIMELINE_EVENT_LIMIT).timeline.find_all { |e| e.trackable.published rescue true }.reject { |e| e.trackable_type == 'Vote'}
+    @timeline_events =  @user.events.includes([:user]).limit(TIMELINE_EVENT_LIMIT).timeline.find_all { |e| e.trackable.published rescue true }.reject { |e| e.trackable_type == 'Vote'}
 
     @user.events.timeline.unviewed.each do |event|
       event.viewed = true
