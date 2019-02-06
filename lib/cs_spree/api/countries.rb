3e9e4115c5c2012a24f6712c9befe043fbb51a85
stujo <stujo@users.noreply.github.com>
2019-02-06 11:34:08 -0800

    Catch Exceptions and throw transient fetch errors

--- lib/cs_spree/api/countries.rb
+++ lib/cs_spree/api/countries.rb
@@ -2,28 +2,24 @@ module CsSpree::Api::Countries
 
   def self.enabled_countries
     url_path = '/api/v1/cs_countries/enabled_countries'
-    begin
-      CacheExtensions::fetch_with_rescue('enabled_countries', 1.hour, 1.minute) do
+    CacheExtensions::fetch_with_rescue('enabled_countries', 1.hour, 1.minute) do
+      begin
         CsSpree.get_api(url_path)
-      end
-    rescue StandardError => e
-      Rails.logger.error "Error in  CsSpree.get_api('#{url_path}') #{e}"
-      DEFAULT_PRODUCT_GROUPS_BY_COUNTRY.fetch(up_iso2) do
-        Rails.logger.error "No default Product Groups configured for #{up_iso2}"
+      rescue StandardError => e
+        Rails.logger.error "Error in  CsSpree.get_api('#{url_path}') #{e}"
+        raise CacheExtensions::TransientFetchError,  "Error in CsSpree.get_api('#{url_path}') #{e.class.name} #{e.message}"
       end
     end
   end
 
   def self.intl_enabled
     url_path = '/api/v1/cs_countries/intl_enabled'
-    begin
-      CacheExtensions::fetch_with_rescue('intl_enabled', 1.hour, 1.minute) do
+    CacheExtensions::fetch_with_rescue('intl_enabled', 1.hour, 1.minute) do
+      begin
         CsSpree.get_api(url_path)
-      end
-    rescue StandardError => e
-      Rails.logger.error "Error in  CsSpree.get_api('#{url_path}') #{e}"
-      DEFAULT_PRODUCT_GROUPS_BY_COUNTRY.fetch(up_iso2) do
-        Rails.logger.error "No default Product Groups configured for #{up_iso2}"
+      rescue StandardError => e
+        Rails.logger.error "Error in  CsSpree.get_api('#{url_path}') #{e}"
+        raise CacheExtensions::TransientFetchError,  "Error in CsSpree.get_api('#{url_path}') #{e.class.name} #{e.message}"
       end
     end
   end
