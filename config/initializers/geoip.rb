d7c77a2b24d4f4eea2a85bd16501bce1f5c4a5e3
Jeremy Shaffer <jshaffer@chefsteps.com>
2019-05-06 17:43:15 -0700

    Don't call geo API if no config set

--- config/initializers/geoip.rb
+++ config/initializers/geoip.rb
@@ -1,14 +1,22 @@
-
-geoip_config = OpenStruct.new
-
-Rails.configuration.geoip = geoip_config
-
 if Rails.env.development? || Rails.env.test?
-  geoip_config.license = 'v6DjqPqzPsNL'
-  geoip_config.user = '106517'
-  geoip_config.cache_expiry = 10.seconds
+  Rails.configuration.geoip = OpenStruct.new(
+    is_configured: true,
+    license: 'v6DjqPqzPsNL',
+    user: '106517',
+    cache_expiry: 10.seconds
+  )
+elsif ENV["GEOIP_LICENSE"].present? && ENV["GEOIP_USER"].present?
+  Rails.configuration.geoip = OpenStruct.new(
+    is_configured: true,
+    license: ENV["GEOIP_LICENSE"],
+    user: ENV["GEOIP_USER"],
+    cache_expiry: 7.days
+  )
 else
-  geoip_config.license = ENV["GEOIP_LICENSE"]
-  geoip_config.user = ENV["GEOIP_USER"]
-  geoip_config.cache_expiry = 7.days
+  Rails.configuration.geoip = OpenStruct.new(
+    is_configured: false,
+    license: nil,
+    user: nil,
+    cache_expiry: 7.days
+  )
 end
