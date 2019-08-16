fabfd20d2658ca342ff8b3161cc7cf8cdcfc93b8
Chris <cwalstad@gmail.com>
2019-08-16 09:08:14 -0700

    works with login

--- app/controllers/api/v0/chargebee_controller.rb
+++ app/controllers/api/v0/chargebee_controller.rb
@@ -1,26 +1,35 @@
 module Api
   module V0
-    class ChargebeeController < ::Api::BaseController
+    class ChargebeeController < BaseController
       before_filter :ensure_authorized
+
+      rescue_from ChargeBee::InvalidRequestError, with: :render_invalid_chargebee_request
 
       def generate_premium_url
         params = {
           :subscription => {
-            :plan_id => ENV['PREMIUM_PLAN_ID']
+            :plan_id => ENV['PREMIUM_PLAN_ID'] || 'cbdemo_nuts'
           },
           :customer => {
-            :id => current_user.id,
-            :email => current_user.email
+            :id => current_api_user.id,
+            :email => current_api_user.email
           }
         }
 
         result = ChargeBee::HostedPage.checkout_new(params)
-        render :json => result.hosted_page.to_s
+        render_api_response(200, result.hosted_page)
       end
 
       def create_portal_session
-        result = ChargeBee::PortalSession.create({ :customer => { :id => current_user.id } })
-        render :json => result.portal_session.to_s
+        result = ChargeBee::PortalSession.create({ :customer => { :id => current_api_user.id } })
+        render_api_response(200, result.portal_session)
+      end
+
+      private
+
+      def render_invalid_chargebee_request(exception = nil)
+        Rails.logger.error("ChargeBee::InvalidRequestError.  current_api_user.id=#{current_api_user.id} exception=#{exception}")
+        render_api_response(400, {message: 'Failed to make ChargeBee request'})
       end
 
     end
