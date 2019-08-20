c6518def72e3d6a430e01df071eef61feaf0a686
Chris <cwalstad@gmail.com>
2019-08-20 12:36:54 -0700

    add webhook route

--- config/routes.rb
+++ config/routes.rb
@@ -315,9 +315,12 @@ Delve::Application.routes.draw do
 
       get 'content_config/manifest', to: 'content#manifest'
 
-      post '/create_portal_session', to: 'chargebee#create_portal_session'
-      post '/generate_premium_url', to: 'chargebee#generate_premium_url'
-      post '/sync_subscriptions', to: 'chargebee#sync_subscriptions'
+      namespace :subscription do
+        post '/create_portal_session', to: 'chargebee#create_portal_session'
+        post '/generate_premium_url', to: 'chargebee#generate_premium_url'
+        post '/sync_subscriptions', to: 'chargebee#sync_subscriptions'
+        post '/webhook', to: 'chargebee#webhook'
+      end
 
       namespace :shopping do
 
