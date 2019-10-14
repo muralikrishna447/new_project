ca62b0656cd0e77892cac385d144ade746499e4c
Chris <cwalstad@gmail.com>
2019-10-14 16:13:24 -0700

    minor changes

--- app/workers/circulator_athena_sync.rb
+++ app/workers/circulator_athena_sync.rb
@@ -7,7 +7,7 @@ class CirculatorAthenaSync
     region = options[:region] || 'us-east-1'
     database = options[:database] || "extracted_logs"
     work_group = options[:work_group] || "primary"
-    output_location = options[:output_location] || "todo-newbucket"
+    output_location = options[:output_location] || "s3://circulator-athena-sync-query-results"
     limit = options[:limit] || 1000
 
     @client = Aws::Athena::Client.new(
@@ -15,16 +15,10 @@ class CirculatorAthenaSync
     )
 
     serial_numbers = Circulator.where(:athena_sync_at => nil).limit(limit).pluck(:serial_number)
-
     query = "select distinct(serial_number), hardware_version, hardware_options "\
               "from identify_circulator_reply "\
               "where serial_number in (#{serial_numbers.join(',')}) "\
               "limit #{limit}"
-
-
-    Rails.logger.info("CirculatorAthenaSync - serial_numbers.count=#{serial_numbers.count} ")
-    Rails.logger.info("CirculatorAthenaSync - start_query_execution params=#{params.inspect}")
-
     params = {
         query_string: query,
         query_execution_context: {
@@ -38,6 +32,10 @@ class CirculatorAthenaSync
         },
         work_group: work_group,
     }
+
+    Rails.logger.info("CirculatorAthenaSync - serial_numbers.count=#{serial_numbers.count} ")
+    Rails.logger.info("CirculatorAthenaSync - start_query_execution params=#{params.inspect}")
+
     @query_execution_id = client.start_query_execution(params)
 
     wait_for_query
@@ -69,9 +67,7 @@ class CirculatorAthenaSync
                                            })
       results = response.result_set.rows
       more_results = response.next_token.present?
-
       Rails.logger.info("CirculatorAthenaSync - get_query_results - results.count=#{results.count} more_results=#{more_results}")
-
       update_results(results)
     end
   end
@@ -80,13 +76,13 @@ class CirculatorAthenaSync
   # results => Array<serial_number, hardware_version, hardware_options>
   def self.update_results(results)
     results.each do |row|
-      circulator = Circulator.find_by_serial_number(row.data[0].var_char_value)
+      circulator = Circulator.find_by_serial_number!(row.data[0].var_char_value)
       circulator.update_attributes!({
                                         :hardware_version => row.data[1].var_char_value,
                                         :hardware_options => row.data[2].var_char_value,
                                         :athena_sync_at => Time.now
                                     })
-      
+
       if circulator.premium_offer_eligible?
         user = circulator.circulator_users.first
         Rails.logger.info("CirculatorAthenaSync - make_premium_member - user.id=#{user.id}")
