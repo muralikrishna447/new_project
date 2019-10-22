331d5b06769b06b7dafca4e3e957ee60676f2e85
Chris <cwalstad@gmail.com>
2019-10-22 16:13:21 -0700

    patch chargebee client

--- config/initializers/chargebee.rb
+++ config/initializers/chargebee.rb
@@ -2,3 +2,13 @@ ChargeBee.configure(
   site: ENV['CHARGEBEE_SITE'],
   api_key: ENV['CHARGEBEE_API_KEY']
 )
+
+
+# Patching in the checkout_gift method which is not available in the version of Chargebee we are using
+# The later version of Chargebee requires json_pure 2.0+ which is not compatible
+# with our (very old) version of ActiveRecord's json serialization
+ChargeBee::HostedPage.class_eval do
+  def self.checkout_gift(params, env=nil, headers={})
+    ChargeBee::Request.send('post', uri_path("hosted_pages","checkout_gift"), params, env, headers)
+  end
+end
