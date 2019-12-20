ab3020a2f68d045e33ec09e0b5e337ba32ec9c0a
Chris <cwalstad@gmail.com>
2019-12-20 14:43:18 -0800

    set origin explicitly in allowed case

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -14,13 +14,15 @@ module Api
     end
 
     def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Origin, Content-Type, Accept, Authorization, Token, cs-referer, X-Application-Version'
       headers['Access-Control-Max-Age'] = "1728000"
       if allowed_origin?(origin_hostname)
+        headers['Access-Control-Allow-Origin'] = request.headers['origin']
         headers['Access-Control-Allow-Credentials'] = 'true'
       else
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Credentials'] = 'false'
         Rails.logger.info "[cors] Not setting Access-Control-Allow-Credentials because origin #{origin_hostname} is not allowed"
       end
 
