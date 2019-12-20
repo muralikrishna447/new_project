58f5904f15f323703e5f39efb9c2feded288ad31
Chris <cwalstad@gmail.com>
2019-12-20 14:39:58 -0800

    query allowed origins for credentials

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -18,7 +18,12 @@ module Api
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Origin, Content-Type, Accept, Authorization, Token, cs-referer, X-Application-Version'
       headers['Access-Control-Max-Age'] = "1728000"
-      headers['Access-Control-Allow-Credentials'] = 'false'
+      if allowed_origin?(origin_hostname)
+        headers['Access-Control-Allow-Credentials'] = 'true'
+      else
+        Rails.logger.info "[cors] Not setting Access-Control-Allow-Credentials because origin #{origin_hostname} is not allowed"
+      end
+
       if request.method == 'OPTIONS'
         render :text => '', :content_type => 'text/plain'
       end
