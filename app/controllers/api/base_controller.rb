5dcfbea03ca29ee03f9b8ec976b50c7b6bb63458
Chris <cwalstad@gmail.com>
2019-12-21 10:08:27 -0800

    undo base_controller change

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -14,6 +14,10 @@ module Api
     end
 
     def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Origin, Content-Type, Accept, Authorization, Token, cs-referer, X-Application-Version'
+      headers['Access-Control-Max-Age'] = "1728000"
       if request.method == 'OPTIONS'
         render :text => '', :content_type => 'text/plain'
       end
