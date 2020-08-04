20a0a5e3928140094d9845ec814fd29bb8a95172
Dheena <dheenadhayalan.g@breville.com>
2020-08-04 15:49:42 +0530

    Multiple-subscription-issue

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,9 @@
 class Subscription < ApplicationRecord
-  STUDIO_PLAN_ID = ENV['STUDIO_PLAN_ID']
+  STUDIO_PLAN_ID = 'chefsteps_studio_pass'#ENV['STUDIO_PLAN_ID']
   EXISTING_PREMIUM_COUPON = ENV['EXISTING_PREMIUM_COUPON']
   ACTIVE_PLAN_STATUSES = ['active', 'in_trial', 'non_renewing']
   ACTIVE_OR_CANCELLED_PLAN_STATUSES = Array.new(ACTIVE_PLAN_STATUSES).concat(['cancelled'])
+  CANCELLED_STATUS = ['cancelled']
 
   IS_STUDIO_LIVE = ENV['CS_IS_STUDIO_LIVE'] == 'true'
 
@@ -11,13 +12,22 @@ class Subscription < ApplicationRecord
   validates :status, inclusion: { in: %w(future in_trial active non_renewing paused cancelled) }
 
   scope :active, -> { where(:status => ACTIVE_PLAN_STATUSES) }
+  scope :cancelled, -> { where(:status => CANCELLED_STATUS) }
 
   def self.user_has_subscription?(user, plan_id)
     self.where(:user_id => user.id).where(:plan_id => plan_id).active.exists?
   end
 
+  def self.user_has_cancelled_subscription?(user, plan_id)
+    self.where(user_id: user.id, plan_id: plan_id).cancelled.exists?
+  end
+
   def self.user_has_studio?(user)
     self.user_has_subscription?(user, STUDIO_PLAN_ID)
+  end
+
+  def self.user_has_cancelled_studio?(user)
+    self.user_has_cancelled_subscription?(user, STUDIO_PLAN_ID)
   end
 
   def self.create_or_update_by_params(params, user_id)
@@ -35,6 +45,9 @@ class Subscription < ApplicationRecord
     end
 
     if params[:resource_version].to_i > subscription.resource_version
+      #resource_version will be uniq for each request from chargebee.
+      #It should be updated everytime.
+      subscription.resource_version = params[:resource_version]
       subscription.status = params[:status]
     end
 
