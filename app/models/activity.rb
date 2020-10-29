6e38da396959593ff9d3a4715aa020c6478592a0
Jayaprakash <jayaprakash.asokan@nirman.io>
2020-10-29 11:13:44 +0530

    CHEFWEB-392 | Activity slug cloumn ignored while restoring the revision record

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -46,7 +46,7 @@ class Activity < ApplicationRecord
   belongs_to :currently_editing_user, class_name: 'User', foreign_key: 'currently_editing_user'
 
   validates :title, presence: true
-  validates :slug, presence: true, uniqueness: true, on: :update
+  validates :slug, presence: true, uniqueness: true, on: :update, if: -> { slug_modified? }
   validates :promote_order, :numericality => { greater_than_or_equal_to: 1, message: "Order should be greater than or equal to 1"}, if: -> { promoted? }
 
   scope :with_video, -> { where("youtube_id <> '' OR vimeo_id <> ''") }
@@ -224,7 +224,7 @@ class Activity < ApplicationRecord
 
   after_commit :create_or_update_as_ingredient, :if => :persisted?
 
-  before_save :check_slug, on: :update, if: :slug_changed?
+  before_save :check_slug, on: :update, if: :slug_modified?
 
   def check_slug
     self.slug = slug.parameterize
@@ -546,6 +546,15 @@ class Activity < ApplicationRecord
 
   def chefsteps_generated
     user.blank?
+  end
+
+  def self.non_restore_columns
+    %w[slug]
+  end
+
+  # slug_changed? method will not work for revision_restore process
+  def slug_modified?
+    slug_change && slug_change.compact.uniq.length > 1
   end
 
   private
