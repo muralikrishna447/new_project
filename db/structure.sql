ef1355dcf490446d74a0150f381375036e49f69b
Jayaprakash <jayaprakash.asokan@nirman.io>
2020-11-09 12:58:21 +0530

    CHEFWEB-109 | structure SQL file issue

--- db/structure.sql
+++ db/structure.sql
@@ -2593,6 +2593,7 @@ CREATE TABLE public.users (
     remember_token character varying,
     opt_in boolean,
     country_code text,
+    apple_user_id character varying,
     is_consent_displayed boolean DEFAULT false,
     marketing_mail_status integer DEFAULT 0
 );
