07912b7458079ceb5295bc720b86810dc3cc7fc1
BharathRam <bharath.ram@breville.com>
2020-11-09 11:16:58 +0530

    CHEFWEB-406 | remove activity from algolia after activity deleted

--- app/workers/algolia_sync.rb
+++ app/workers/algolia_sync.rb
@@ -3,12 +3,17 @@ class AlgoliaSync
   def self.perform(activity_id)
     Rails.logger.info "Syncing activity [#{activity_id}] to Algolia"
     begin
-      activity = Activity.find(activity_id)
-      activity.title = CGI.unescapeHTML(activity.title.to_s)
-      activity.description = CGI.unescapeHTML(activity.description.to_s)
-      activity.short_description = CGI.unescapeHTML(activity.short_description.to_s)
-      activity.byline = CGI.unescapeHTML(activity.byline.to_s)
-      activity.index!
+      activity = Activity.find_by_id(activity_id)
+      if activity
+        activity.title = CGI.unescapeHTML(activity.title.to_s)
+        activity.description = CGI.unescapeHTML(activity.description.to_s)
+        activity.short_description = CGI.unescapeHTML(activity.short_description.to_s)
+        activity.byline = CGI.unescapeHTML(activity.byline.to_s)
+        activity.index!
+      else
+        index = Algolia::Index.new(Activity.index_name)
+        index.delete_object(activity_id)
+      end
     rescue Exception => e
       Librato.increment("algolia.sync_failed.activity")
       msg = "Exception #{e.message} while syncing activity [#{activity_id}] to Algolia"
