f3ee29bf485d02d9319b4ca50e180af3c4618efd
Mani M <mani.madalaimuthu@breville.com>
2021-03-16 13:44:05 +0530

    CHEFWEB-533 | Added remove_column with original column options

--- db/migrate/20210315180837_remove_is_consent_displayed_to_users.rb
+++ db/migrate/20210315180837_remove_is_consent_displayed_to_users.rb
@@ -1,5 +1,5 @@
 class RemoveIsConsentDisplayedToUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :is_consent_displayed, :boolean
+    remove_column :users, :is_consent_displayed, :boolean, default: false
   end
 end
