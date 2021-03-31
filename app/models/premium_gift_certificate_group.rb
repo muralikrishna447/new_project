0cd93063e52a92d9cbf9912ad21036d97b2e92d0
BharathRam <bharath.ram@breville.com>
2021-03-31 12:42:41 +0530

    CHEFWEB-556 | PremiumGroupCertificate coupon count increased to 10000

--- app/models/premium_gift_certificate_group.rb
+++ app/models/premium_gift_certificate_group.rb
@@ -1,7 +1,7 @@
 class PremiumGiftCertificateGroup < ApplicationRecord
   validates :title, :coupon_count, :created_by_id, presence: true
   validates_uniqueness_of :title
-  validates_numericality_of :coupon_count, greater_than: 0, less_than: 5000
+  validates_numericality_of :coupon_count, greater_than: 0, less_than: 10001
 
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
 
