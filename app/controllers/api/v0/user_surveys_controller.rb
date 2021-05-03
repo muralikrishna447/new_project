18c664de5d0418be6eda0865557574b1a5b87d69
Jayaprakash <jayaprakash.asokan@nirman.io>
2021-05-03 10:51:37 +0530

    CHEFWEB-594 | Mailchimp user survey group ID added in environment files

--- app/controllers/api/v0/user_surveys_controller.rb
+++ app/controllers/api/v0/user_surveys_controller.rb
@@ -7,7 +7,9 @@ module Api
         if current_api_user.update(survey_results: params[:survey_results].to_unsafe_h)
           interests = current_api_user.survey_results['interests']
           if interests
-            email_list_add_to_group(current_api_user.email, '4751714', interests)
+            email_list_add_to_group(current_api_user.email,
+                                    Rails.configuration.mailchimp[:survey_group_id],
+                                    interests)
           end
           render json: current_api_user.survey_results
         end
