4eab8333b6084e9cc333bf934ac8e58ea0f1515c
Mani M <mani.madalaimuthu@breville.com>
2021-05-04 17:10:34 +0530

    CHEFWEB-604 | Fixed send userPlan only for loggedIn user

--- public/analytics_user_plan.js
+++ public/analytics_user_plan.js
@@ -1,13 +1,15 @@
 (function(){
   var analyticsUserPlan = window.analyticsUserPlan = window.analyticsUserPlan || {};
 
-  analyticsUserPlan.load = function(is_studio, is_premium) {
-    if (is_studio) {
-      return ({chefstepsUserPlan: 'studio'});
-    } else if (is_premium) {
-      return ({chefstepsUserPlan: 'premium'});
-    } else {
-      return ({chefstepsUserPlan: 'free'});
+  analyticsUserPlan.load = function(loggedIn, isStudio, isPremium) {
+    if (loggedIn) {
+      if (isStudio) {
+        return ({chefstepsUserPlan: 'studio'});
+      } else if (isPremium) {
+        return ({chefstepsUserPlan: 'premium'});
+      } else {
+        return ({chefstepsUserPlan: 'free'});
+      }
     }
   }
 })();
